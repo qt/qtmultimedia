940f14c3b6da270cdf3e7574f06accb82fe3ed5e
Thiago Macieira <thiago.macieira@intel.com>
2012-06-22 11:08:07 +0200

    Update the export macros in qtmultimedia.git
    Use the new, simpler QT_STATIC macro.
    
    Change-Id: Ia3271a437ed76f54971444f6e73e54dbc416d483
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@nokia.com>

--- src/multimedia/qtmultimediadefs.h
+++ src/multimedia/qtmultimediadefs.h
@@ -57,30 +57,14 @@
 
 QT_BEGIN_HEADER
 
-#if defined(Q_OS_WIN)
-#  if defined(QT_NODLL)
-#    undef QT_MAKEDLL
-#    undef QT_DLL
-#  elif defined(QT_MAKEDLL)
-#    if defined(QT_DLL)
-#      undef QT_DLL
-#    endif
+#ifndef QT_STATIC
 #    if defined(QT_BUILD_MULTIMEDIA_LIB)
 #        define Q_MULTIMEDIA_EXPORT Q_DECL_EXPORT
 #    else
 #        define Q_MULTIMEDIA_EXPORT Q_DECL_IMPORT
 #    endif
-#  elif defined(QT_DLL) /* use a Qt DLL library */
-#    define Q_MULTIMEDIA_EXPORT Q_DECL_IMPORT
-#  endif
-#endif
-
-#if !defined(Q_MULTIMEDIA_EXPORT)
-#  if defined(QT_SHARED)
-#    define Q_MULTIMEDIA_EXPORT Q_DECL_EXPORT
-#  else
+#else
 #    define Q_MULTIMEDIA_EXPORT
-#  endif
 #endif
 
 QT_END_HEADER
