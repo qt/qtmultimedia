adenexter/qtmultimedia-1

Fix incorrect/missing application of recording settings in camerabin.
Don't set profiles if no settings are specified.
Apply all settings before starting a pipeline as the mode can switch
without being restarted and incompatible video recording settings can
prevent the pipeline starting even in image capture mode.
Set audio encoding settings and encoder profiles if they are supplied.

Change-Id: I06febf977c2cae306383f9dbaae0f81f531b4757

src/plugins/gstreamer/camerabin/camerabinaudioencoder.cpp
@@ -106,18 +106,19 @@ void CameraBinAudioEncoder::resetActualSettings()
 GstEncodingProfile *CameraBinAudioEncoder::createProfile()
 {
     QString codec = m_actualAudioSettings.codec();
+    QString preset = m_actualAudioSettings.encodingOption(QStringLiteral("preset")).toString();
     GstCaps *caps;
 
     if (codec.isEmpty())
-        caps = gst_caps_new_any();
+        return 0;
     else
         caps = gst_caps_from_string(codec.toLatin1());
 
     return (GstEncodingProfile *)gst_encoding_audio_profile_new(
                                         caps,
-                                        NULL, //preset
-                                        NULL, //restriction
-                                        0); //presence
+                                        !preset.isEmpty() ? preset.toUtf8().constData() : NULL, //preset
+                                        NULL,   //restriction
+                                        0);     //presence
 }
 
 QT_END_NAMESPACE

src/plugins/gstreamer/camerabin/camerabinvideoencoder.cpp
@@ -160,18 +160,25 @@ QPair<int,int> CameraBinVideoEncoder::rateAsRational(qreal frameRate) const
 GstEncodingProfile *CameraBinVideoEncoder::createProfile()
 {
     QString codec = m_actualVideoSettings.codec();
+    QString preset = m_actualVideoSettings.encodingOption(QStringLiteral("preset")).toString();
+
     GstCaps *caps;
 
     if (codec.isEmpty())
         caps = gst_caps_new_any();
     else
-        caps = gst_caps_from_string(codec.toLatin1());
+        caps = gst_caps_from_string(codec.toUtf8());
 
-    return (GstEncodingProfile *)gst_encoding_video_profile_new(
+    GstEncodingVideoProfile *profile = gst_encoding_video_profile_new(
                 caps,
-                NULL, //preset
+                !preset.isEmpty() ? preset.toUtf8().constData() : NULL, //preset
                 NULL, //restriction
-                0); //presence
+                1); //presence
+
+    gst_encoding_video_profile_set_pass(profile, 0);
+    gst_encoding_video_profile_set_variableframerate(profile, TRUE);
+
+    return (GstEncodingProfile *)profile;
 }
 
 QT_END_NAMESPACE

qt/qtmultimedia

Reviewed-by: Yoann Lopes <yoann.lopes@digia.com>

src/plugins/gstreamer/camerabin/camerabinaudioencoder.cpp
@@ -106,18 +106,19 @@ void CameraBinAudioEncoder::resetActualSettings()
 GstEncodingProfile *CameraBinAudioEncoder::createProfile()
 {
     QString codec = m_actualAudioSettings.codec();
+    QString preset = m_actualAudioSettings.encodingOption(QStringLiteral("preset")).toString();
     GstCaps *caps;
 
     if (codec.isEmpty())
-        caps = gst_caps_new_any();
+        return 0;
     else
         caps = gst_caps_from_string(codec.toLatin1());
 
     return (GstEncodingProfile *)gst_encoding_audio_profile_new(
                                         caps,
-                                        NULL, //preset
-                                        NULL, //restriction
-                                        0); //presence
+                                        !preset.isEmpty() ? preset.toLatin1().constData() : NULL, //preset
+                                        NULL,   //restriction
+                                        0);     //presence
 }
 
 QT_END_NAMESPACE

src/plugins/gstreamer/camerabin/camerabinvideoencoder.cpp
@@ -160,18 +160,25 @@ QPair<int,int> CameraBinVideoEncoder::rateAsRational(qreal frameRate) const
 GstEncodingProfile *CameraBinVideoEncoder::createProfile()
 {
     QString codec = m_actualVideoSettings.codec();
+    QString preset = m_actualVideoSettings.encodingOption(QStringLiteral("preset")).toString();
+
     GstCaps *caps;
 
     if (codec.isEmpty())
-        caps = gst_caps_new_any();
+        caps = 0;
     else
         caps = gst_caps_from_string(codec.toLatin1());
 
-    return (GstEncodingProfile *)gst_encoding_video_profile_new(
+    GstEncodingVideoProfile *profile = gst_encoding_video_profile_new(
                 caps,
-                NULL, //preset
+                !preset.isEmpty() ? preset.toLatin1().constData() : NULL, //preset
                 NULL, //restriction
-                0); //presence
+                1); //presence
+
+    gst_encoding_video_profile_set_pass(profile, 0);
+    gst_encoding_video_profile_set_variableframerate(profile, TRUE);
+
+    return (GstEncodingProfile *)profile;
 }
 
 QT_END_NAMESPACE