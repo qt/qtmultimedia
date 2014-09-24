2d911d4e0926969528be8e170a02f74f4761ee25
Bernd Weimer <bweimer@blackberry.com>
2014-09-24 15:43:46 +0200

    QNX: Fix resetting video surface
    If video surface object is set to 0, it must not be accessed to set a
    property on it.
    
    Task-number: QTBUG-40746
    Change-Id: I1de0e5495918d3ea06706412fab15bf1af012f36
    Reviewed-by: Fabian Bumberger <fbumberger@rim.com>

--- src/plugins/qnx/mediaplayer/mmrendererplayervideorenderercontrol.cpp
+++ src/plugins/qnx/mediaplayer/mmrendererplayervideorenderercontrol.cpp
@@ -78,7 +78,7 @@ void MmRendererPlayerVideoRendererControl::setSurface(QAbstractVideoSurface *sur
     m_surface = QPointer<QAbstractVideoSurface>(surface);
     if (QOpenGLContext::currentContext())
         m_windowGrabber->checkForEglImageExtension();
-    else
+    else if (m_surface)
         m_surface->setProperty("_q_GLThreadCallback", QVariant::fromValue<QObject*>(this));
 }
 
