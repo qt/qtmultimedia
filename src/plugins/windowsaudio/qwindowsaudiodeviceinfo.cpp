5f11c33fd54bfdc51e918d8976ee0405e8835481
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-07-13 15:14:48 +0200

    WindowsAudio: Fix compiler warnings by g++/MinGW.
    Replace NULL by 0 for parameters of type DWORD_PTR.
    
    qwindowsaudiodeviceinfo.cpp: In member function 'bool QWindowsAudioDeviceInfo::testSettings(const QAudioFormat&) const':
    qwindowsaudiodeviceinfo.cpp:220:50: warning: passing NULL to non-pointer argument 4 of 'MMRESULT waveOutOpen(LPHWAVEOUT, UINT, LPCWAVEFORMATEX, DWORD_PTR, DWORD_PTR, DWORD)' [-Wconversion-null]
                                     WAVE_FORMAT_QUERY) == MMSYSERR_NOERROR);                                                  ^
    qwindowsaudiodeviceinfo.cpp:220:50: warning: passing NULL to non-pointer argument 5 of 'MMRESULT waveOutOpen(LPHWAVEOUT, UINT, LPCWAVEFORMATEX, DWORD_PTR, DWORD_PTR, DWORD)' [-Wconversion-null]
    qwindowsaudiodeviceinfo.cpp:223:50: warning: passing NULL to non-pointer argument 4 of 'MMRESULT waveInOpen(LPHWAVEIN, UINT, LPCWAVEFORMATEX, DWORD_PTR, DWORD_PTR, DWORD)' [-Wconversion-null]
                                     WAVE_FORMAT_QUERY) == MMSYSERR_NOERROR);                                                  ^
    qwindowsaudiodeviceinfo.cpp:223:50: warning: passing NULL to non-pointer argument 5 of 'MMRESULT waveInOpen(LPHWAVEIN, UINT, LPCWAVEFORMATEX, DWORD_PTR, DWORD_PTR, DWORD)' [-Wconversion-null]# ==[ Please wrap at 72 characters ]===================================|
    
    Change-Id: Ie8f65e0e1f4f1274a1b634f3701ae0103e716d97
    Reviewed-by: Yoann Lopes <yoann.lopes@theqtcompany.com>

--- src/plugins/windowsaudio/qwindowsaudiodeviceinfo.cpp
+++ src/plugins/windowsaudio/qwindowsaudiodeviceinfo.cpp
@@ -216,10 +216,10 @@ bool QWindowsAudioDeviceInfo::testSettings(const QAudioFormat& format) const
     if (qt_convertFormat(format, &wfx)) {
         // query only, do not open device
         if (mode == QAudio::AudioOutput) {
-            return (waveOutOpen(NULL, UINT_PTR(devId), &wfx.Format, NULL, NULL,
+            return (waveOutOpen(NULL, UINT_PTR(devId), &wfx.Format, 0, 0,
                                 WAVE_FORMAT_QUERY) == MMSYSERR_NOERROR);
         } else { // AudioInput
-            return (waveInOpen(NULL, UINT_PTR(devId), &wfx.Format, NULL, NULL,
+            return (waveInOpen(NULL, UINT_PTR(devId), &wfx.Format, 0, 0,
                                 WAVE_FORMAT_QUERY) == MMSYSERR_NOERROR);
         }
     }
