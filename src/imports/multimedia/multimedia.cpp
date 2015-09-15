99f6cf5f282c0d3d7192d5e87b745873e159c93d
Marco Benelli <marco.benelli@theqtcompany.com>
2015-09-15 13:37:57 +0200

    Fixed revisions of camera-related meta objects.
    Change-Id: I91fe90122d53ce12322cecac8282ef9622869b71
    Reviewed-by: Yoann Lopes <yoann.lopes@theqtcompany.com>

--- src/imports/multimedia/multimedia.cpp
+++ src/imports/multimedia/multimedia.cpp
@@ -97,7 +97,7 @@ public:
                                 trUtf8("CameraImageProcessing is provided by Camera"));
 
         // 5.2 types
-        qmlRegisterRevision<QDeclarativeVideoOutput, 2>(uri, 5, 2);
+        qmlRegisterType<QDeclarativeVideoOutput, 2>(uri, 5, 2, "VideoOutput");
 
         // 5.3 types
         // Nothing changed, but adding "import QtMultimedia 5.3" in QML will fail unless at
@@ -106,13 +106,13 @@ public:
 
         // 5.4 types
         qmlRegisterSingletonType<QDeclarativeMultimediaGlobal>(uri, 5, 4, "QtMultimedia", multimedia_global_object);
-        qmlRegisterRevision<QDeclarativeCamera, 1>(uri, 5, 4);
+        qmlRegisterType<QDeclarativeCamera, 1>(uri, 5, 4, "Camera");
         qmlRegisterUncreatableType<QDeclarativeCameraViewfinder>(uri, 5, 4, "CameraViewfinder",
                                 trUtf8("CameraViewfinder is provided by Camera"));
 
         // 5.5 types
         qmlRegisterUncreatableType<QDeclarativeCameraImageProcessing, 1>(uri, 5, 5, "CameraImageProcessing", trUtf8("CameraImageProcessing is provided by Camera"));
-        qmlRegisterRevision<QDeclarativeCamera, 2>(uri, 5, 5);
+        qmlRegisterType<QDeclarativeCamera, 2>(uri, 5, 5, "Camera");
 
         qmlRegisterType<QDeclarativeMediaMetaData>();
         qmlRegisterType<QAbstractVideoFilter>();
