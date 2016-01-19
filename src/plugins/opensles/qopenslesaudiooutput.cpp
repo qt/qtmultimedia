e126dbb6fb7440882b49fb2fcbde53f4b0a5e180
Yoann Lopes <yoann.lopes@theqtcompany.com>
2016-01-19 14:15:40 +0100

    OpenSL ES: fix QAudioOutput state after resume().
    In push mode, the state must be IdleState after resume(), and only
    change to ActiveState once it receives some data.
    
    Task-number: QTBUG-50390
    Change-Id: Iaf47363196ee94b80ac4ebe58a588929af8d3fad
    Reviewed-by: Pasi Petäjäjärvi <pasi.petajajarvi@theqtcompany.com>

--- src/plugins/opensles/qopenslesaudiooutput.cpp
+++ src/plugins/opensles/qopenslesaudiooutput.cpp
@@ -238,7 +238,7 @@ void QOpenSLESAudioOutput::resume()
         return;
     }
 
-    setState(QAudio::ActiveState);
+    setState(m_pullMode ? QAudio::ActiveState : QAudio::IdleState);
     setError(QAudio::NoError);
 }
 
