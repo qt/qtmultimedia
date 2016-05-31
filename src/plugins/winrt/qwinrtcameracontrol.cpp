qt/qtmultimedia

commit c84bdf63e4de16586eda3c45d5c3c3b2dc4fe089

winrt: Fix crash when initializing certain cameras
Some cameras return video properties when querying for
MediaStreamType_Photo, ie. Surface Book. This caused an assert.

Instead when asking for photo resolutions, only query the available
image resolutions and skip results not of type image.

Change-Id: Ia1886a11f47676d6713eec86f3a80c664871a968
Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

src/plugins/winrt/qwinrtcameracontrol.cpp
@@ -101,15 +101,30 @@ HRESULT getMediaStreamResolutions(IMediaDeviceController *device,
         ComPtr<IMediaEncodingProperties> properties;
         hr = (*propertiesList)->GetAt(index, &properties);
         Q_ASSERT_SUCCEEDED(hr);
-        ComPtr<IVideoEncodingProperties> videoProperties;
-        hr = properties.As(&videoProperties);
-        Q_ASSERT_SUCCEEDED(hr);
-        UINT32 width, height;
-        hr = videoProperties->get_Width(&width);
-        Q_ASSERT_SUCCEEDED(hr);
-        hr = videoProperties->get_Height(&height);
-        Q_ASSERT_SUCCEEDED(hr);
-        resolutions->append(QSize(width, height));
+        if (type == MediaStreamType_VideoRecord || type == MediaStreamType_VideoPreview) {
+            ComPtr<IVideoEncodingProperties> videoProperties;
+            hr = properties.As(&videoProperties);
+            Q_ASSERT_SUCCEEDED(hr);
+            UINT32 width, height;
+            hr = videoProperties->get_Width(&width);
+            Q_ASSERT_SUCCEEDED(hr);
+            hr = videoProperties->get_Height(&height);
+            Q_ASSERT_SUCCEEDED(hr);
+            resolutions->append(QSize(width, height));
+        } else if (type == MediaStreamType_Photo) {
+            ComPtr<IImageEncodingProperties> imageProperties;
+            hr = properties.As(&imageProperties);
+            // Asking for Photo also returns video resolutions in addition
+            // We skip those, as we are only interested in image Type
+            if (FAILED(hr) || !imageProperties)
+                continue;
+            UINT32 width, height;
+            hr = imageProperties->get_Width(&width);
+            Q_ASSERT_SUCCEEDED(hr);
+            hr = imageProperties->get_Height(&height);
+            Q_ASSERT_SUCCEEDED(hr);
+            resolutions->append(QSize(width, height));
+        }
     }
     return resolutions->isEmpty() ? MF_E_INVALID_FORMAT : hr;
 }

commit 711efc552d65f22b262e049c5a046198377519dc

winrt: Fix potential crash when querying for resolutions
c84bdf63e4de16586eda3c45d5c3c3b2dc4fe089 introduced a potential crash on
Windows Phone as querying for photo resolutions still might return video
properties. As a result, querying for ImageEncodingProperties will fail
and the function returns no resolution at all causing an assert later
on.

Instead of checking for the stream type passed to
getMediaStreamResolutions we have to check for the property type of the
list itself.

Task-number: QTBUG-53714
Change-Id: I7bfc48ebf1fef4f9165135af604ce6c91a418438
Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

src/plugins/winrt/qwinrtcameracontrol.cpp
@@ -101,7 +101,13 @@ HRESULT getMediaStreamResolutions(IMediaDeviceController *device,
         ComPtr<IMediaEncodingProperties> properties;
         hr = (*propertiesList)->GetAt(index, &properties);
         Q_ASSERT_SUCCEEDED(hr);
-        if (type == MediaStreamType_VideoRecord || type == MediaStreamType_VideoPreview) {
+        HString propertyType;
+        hr = properties->get_Type(propertyType.GetAddressOf());
+        Q_ASSERT_SUCCEEDED(hr);
+
+        const HStringReference videoRef = HString::MakeReference(L"Video");
+        const HStringReference imageRef = HString::MakeReference(L"Image");
+        if (propertyType == videoRef) {
             ComPtr<IVideoEncodingProperties> videoProperties;
             hr = properties.As(&videoProperties);
             Q_ASSERT_SUCCEEDED(hr);
@@ -111,13 +117,10 @@ HRESULT getMediaStreamResolutions(IMediaDeviceController *device,
             hr = videoProperties->get_Height(&height);
             Q_ASSERT_SUCCEEDED(hr);
             resolutions->append(QSize(width, height));
-        } else if (type == MediaStreamType_Photo) {
+        } else if (propertyType == imageRef) {
             ComPtr<IImageEncodingProperties> imageProperties;
             hr = properties.As(&imageProperties);
-            // Asking for Photo also returns video resolutions in addition
-            // We skip those, as we are only interested in image Type
-            if (FAILED(hr) || !imageProperties)
-                continue;
+            Q_ASSERT_SUCCEEDED(hr);
             UINT32 width, height;
             hr = imageProperties->get_Width(&width);
             Q_ASSERT_SUCCEEDED(hr);