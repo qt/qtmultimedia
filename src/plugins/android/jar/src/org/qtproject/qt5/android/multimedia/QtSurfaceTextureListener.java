qt/qtmultimedia

commit ba8127639857232d8a37e953c5cda84203360d97

Android: improve texture rendering on API level >= 16.
Android API level 16 added SurfaceTexture::attachToGLContext(). This
allows to create the OpenGL texture when the first video frame is
available, rather than at initialization. This means we can do without
the ugly hack that makes the render thread call us back through some
custom property.
Additionally, it allows to recreate a new OpenGL texture every time
the SurfaceTexture is reset.

Task-number: QTBUG-51911
Change-Id: I17b04524d426c42ef8aa0288b0731597bc9eba62
Reviewed-by: Christian Stromme <christian.stromme@qt.io>

src/plugins/android/src/wrappers/jni/androidsurfacetexture.cpp
@@ -146,6 +146,22 @@ jobject AndroidSurfaceTexture::surfaceHolder()
     return m_surfaceHolder.object();
 }
 
+void AndroidSurfaceTexture::attachToGLContext(int texName)
+{
+    if (QtAndroidPrivate::androidSdkVersion() < 16 || !m_surfaceTexture.isValid())
+        return;
+
+    m_surfaceTexture.callMethod<void>("attachToGLContext", "(I)V", texName);
+}
+
+void AndroidSurfaceTexture::detachFromGLContext()
+{
+    if (QtAndroidPrivate::androidSdkVersion() < 16 || !m_surfaceTexture.isValid())
+        return;
+
+    m_surfaceTexture.callMethod<void>("detachFromGLContext");
+}
+
 bool AndroidSurfaceTexture::initJNI(JNIEnv *env)
 {
     // SurfaceTexture is available since API 11.

src/plugins/android/src/wrappers/jni/androidsurfacetexture.h
@@ -58,6 +58,9 @@ public:
     void release(); // API level 14
     void updateTexImage();
 
+    void attachToGLContext(int texName); // API level 16
+    void detachFromGLContext(); // API level 16
+
     static bool initJNI(JNIEnv *env);
 
 Q_SIGNALS:

src/plugins/android/src/common/qandroidvideooutput.cpp
@@ -42,6 +42,7 @@
 #include <qopenglfunctions.h>
 #include <qopenglshaderprogram.h>
 #include <qopenglframebufferobject.h>
+#include <QtCore/private/qjnihelpers_p.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -159,6 +160,7 @@ QAndroidTextureVideoOutput::QAndroidTextureVideoOutput(QObject *parent)
     , m_fbo(0)
     , m_program(0)
     , m_glDeleter(0)
+    , m_surfaceTextureCanAttachToContext(QtAndroidPrivate::androidSdkVersion() >= 16)
 {
 
 }
@@ -184,12 +186,14 @@ void QAndroidTextureVideoOutput::setSurface(QAbstractVideoSurface *surface)
     if (m_surface) {
         if (m_surface->isActive())
             m_surface->stop();
-        m_surface->setProperty("_q_GLThreadCallback", QVariant());
+
+        if (!m_surfaceTextureCanAttachToContext)
+            m_surface->setProperty("_q_GLThreadCallback", QVariant());
     }
 
     m_surface = surface;
 
-    if (m_surface) {
+    if (m_surface && !m_surfaceTextureCanAttachToContext) {
         m_surface->setProperty("_q_GLThreadCallback",
                                QVariant::fromValue<QObject*>(this));
     }
@@ -197,7 +201,7 @@ void QAndroidTextureVideoOutput::setSurface(QAbstractVideoSurface *surface)
 
 bool QAndroidTextureVideoOutput::isReady()
 {
-    return QOpenGLContext::currentContext() || m_externalTex;
+    return m_surfaceTextureCanAttachToContext || QOpenGLContext::currentContext() || m_externalTex;
 }
 
 bool QAndroidTextureVideoOutput::initSurfaceTexture()
@@ -208,14 +212,16 @@ bool QAndroidTextureVideoOutput::initSurfaceTexture()
     if (!m_surface)
         return false;
 
-    // if we have an OpenGL context in the current thread, create a texture. Otherwise, wait
-    // for the GL render thread to call us back to do it.
-    if (QOpenGLContext::currentContext()) {
-        glGenTextures(1, &m_externalTex);
-        m_glDeleter = new OpenGLResourcesDeleter;
-        m_glDeleter->setTexture(m_externalTex);
-    } else if (!m_externalTex) {
-        return false;
+    if (!m_surfaceTextureCanAttachToContext) {
+        // if we have an OpenGL context in the current thread, create a texture. Otherwise, wait
+        // for the GL render thread to call us back to do it.
+        if (QOpenGLContext::currentContext()) {
+            glGenTextures(1, &m_externalTex);
+            m_glDeleter = new OpenGLResourcesDeleter;
+            m_glDeleter->setTexture(m_externalTex);
+        } else if (!m_externalTex) {
+            return false;
+        }
     }
 
     QMutexLocker locker(&m_mutex);
@@ -227,7 +233,8 @@ bool QAndroidTextureVideoOutput::initSurfaceTexture()
     } else {
         delete m_surfaceTexture;
         m_surfaceTexture = 0;
-        m_glDeleter->deleteLater();
+        if (m_glDeleter)
+            m_glDeleter->deleteLater();
         m_externalTex = 0;
         m_glDeleter = 0;
     }
@@ -242,6 +249,10 @@ void QAndroidTextureVideoOutput::clearSurfaceTexture()
         delete m_surfaceTexture;
         m_surfaceTexture = 0;
     }
+
+    // Also reset the attached OpenGL texture
+    if (m_surfaceTextureCanAttachToContext)
+        m_externalTex = 0;
 }
 
 AndroidSurfaceTexture *QAndroidTextureVideoOutput::surfaceTexture()
@@ -364,6 +375,18 @@ void QAndroidTextureVideoOutput::renderFrameToFbo()
 
 void QAndroidTextureVideoOutput::createGLResources()
 {
+    Q_ASSERT(QOpenGLContext::currentContext() != NULL);
+
+    if (!m_glDeleter)
+        m_glDeleter = new OpenGLResourcesDeleter;
+
+    if (m_surfaceTextureCanAttachToContext && !m_externalTex) {
+        m_surfaceTexture->detachFromGLContext();
+        glGenTextures(1, &m_externalTex);
+        m_surfaceTexture->attachToGLContext(m_externalTex);
+        m_glDeleter->setTexture(m_externalTex);
+    }
+
     if (!m_fbo || m_fbo->size() != m_nativeSize) {
         delete m_fbo;
         m_fbo = new QOpenGLFramebufferObject(m_nativeSize);
@@ -407,7 +430,7 @@ void QAndroidTextureVideoOutput::customEvent(QEvent *e)
 {
     if (e->type() == QEvent::User) {
         // This is running in the render thread (OpenGL enabled)
-        if (!m_externalTex) {
+        if (!m_surfaceTextureCanAttachToContext && !m_externalTex) {
             glGenTextures(1, &m_externalTex);
             m_glDeleter = new OpenGLResourcesDeleter; // will cleanup GL resources in the correct thread
             m_glDeleter->setTexture(m_externalTex);

src/plugins/android/src/common/qandroidvideooutput.h
@@ -110,6 +110,8 @@ private:
     QOpenGLShaderProgram *m_program;
     OpenGLResourcesDeleter *m_glDeleter;
 
+    bool m_surfaceTextureCanAttachToContext;
+
     friend class AndroidTextureVideoBuffer;
 };
 

commit 443ed0be093b2d8e78d111a59eca97f2eba5b0fe

Android: Fix attach- and detachFromGLContext()
This fixes a regression caused by ba8127639857232d8a. The change
assumed that the AndroidSurfaceTexture class could cope with changing
textures, but since the callback from the SurfaceTextureListener was
tied to the initial, invalid, texture handle, it would only work as
long as there were only one texture; all textures would register
the callback to the same invalid handle.
This change maps the callback directly to the android texture object,
instead of the texture handle.

Task-number: QTBUG-54340
Change-Id: I39568d0f97fa6b9cb1182efaca568b16a26f0d09
Reviewed-by: Yoann Lopes <yoann.lopes@qt.io>

src/plugins/android/jar/src/org/qtproject/qt5/android/multimedia/QtSurfaceTextureListener.java
@@ -37,18 +37,18 @@ import android.graphics.SurfaceTexture;
 
 public class QtSurfaceTextureListener implements SurfaceTexture.OnFrameAvailableListener
 {
-    private final int texID;
+    private final long m_id;
 
-    public QtSurfaceTextureListener(int texName)
+    public QtSurfaceTextureListener(long id)
     {
-        texID = texName;
+        m_id = id;
     }
 
     @Override
     public void onFrameAvailable(SurfaceTexture surfaceTexture)
     {
-        notifyFrameAvailable(texID);
+        notifyFrameAvailable(m_id);
     }
 
-    private static native void notifyFrameAvailable(int id);
+    private static native void notifyFrameAvailable(long id);
 }

src/plugins/android/src/wrappers/jni/androidsurfacetexture.cpp
@@ -34,24 +34,32 @@
 #include "androidsurfacetexture.h"
 #include <QtCore/private/qjni_p.h>
 #include <QtCore/private/qjnihelpers_p.h>
+#include <QtCore/qmutex.h>
 
 QT_BEGIN_NAMESPACE
 
 static const char QtSurfaceTextureListenerClassName[] = "org/qtproject/qt5/android/multimedia/QtSurfaceTextureListener";
-static QMap<int, AndroidSurfaceTexture*> g_objectMap;
+typedef QVector<jlong> SurfaceTextures;
+Q_GLOBAL_STATIC(SurfaceTextures, g_surfaceTextures);
+Q_GLOBAL_STATIC(QMutex, g_textureMutex);
 
 // native method for QtSurfaceTexture.java
-static void notifyFrameAvailable(JNIEnv* , jobject, int id)
+static void notifyFrameAvailable(JNIEnv* , jobject, jlong id)
 {
-    AndroidSurfaceTexture *obj = g_objectMap.value(id, 0);
+    const QMutexLocker lock(g_textureMutex);
+    const int idx = g_surfaceTextures->indexOf(id);
+    if (idx == -1)
+        return;
+
+    AndroidSurfaceTexture *obj = reinterpret_cast<AndroidSurfaceTexture *>(g_surfaceTextures->at(idx));
     if (obj)
         Q_EMIT obj->frameAvailable();
 }
 
 AndroidSurfaceTexture::AndroidSurfaceTexture(unsigned int texName)
     : QObject()
-    , m_texID(int(texName))
 {
+    Q_STATIC_ASSERT(sizeof (jlong) >= sizeof (void *));
     // API level 11 or higher is required
     if (QtAndroidPrivate::androidSdkVersion() < 11) {
         qWarning("Camera preview and video playback require Android 3.0 (API level 11) or later.");
@@ -67,13 +75,13 @@ AndroidSurfaceTexture::AndroidSurfaceTexture(unsigned int texName)
         env->ExceptionClear();
     }
 
-    if (m_surfaceTexture.isValid())
-        g_objectMap.insert(int(texName), this);
+    if (!m_surfaceTexture.isValid())
+        return;
 
-    QJNIObjectPrivate listener(QtSurfaceTextureListenerClassName, "(I)V", jint(texName));
-    m_surfaceTexture.callMethod<void>("setOnFrameAvailableListener",
-                                      "(Landroid/graphics/SurfaceTexture$OnFrameAvailableListener;)V",
-                                      listener.object());
+    const QMutexLocker lock(g_textureMutex);
+    g_surfaceTextures->append(jlong(this));
+    QJNIObjectPrivate listener(QtSurfaceTextureListenerClassName, "(J)V", jlong(this));
+    setOnFrameAvailableListener(listener);
 }
 
 AndroidSurfaceTexture::~AndroidSurfaceTexture()
@@ -83,7 +91,10 @@ AndroidSurfaceTexture::~AndroidSurfaceTexture()
 
     if (m_surfaceTexture.isValid()) {
         release();
-        g_objectMap.remove(m_texID);
+        const QMutexLocker lock(g_textureMutex);
+        const int idx = g_surfaceTextures->indexOf(jlong(this));
+        if (idx != -1)
+            g_surfaceTextures->remove(idx);
     }
 }
 
@@ -172,7 +183,7 @@ bool AndroidSurfaceTexture::initJNI(JNIEnv *env)
                                                      env);
 
     static const JNINativeMethod methods[] = {
-        {"notifyFrameAvailable", "(I)V", (void *)notifyFrameAvailable}
+        {"notifyFrameAvailable", "(J)V", (void *)notifyFrameAvailable}
     };
 
     if (clazz && env->RegisterNatives(clazz,
@@ -182,6 +193,13 @@ bool AndroidSurfaceTexture::initJNI(JNIEnv *env)
     }
 
     return true;
+}
+
+void AndroidSurfaceTexture::setOnFrameAvailableListener(const QJNIObjectPrivate &listener)
+{
+    m_surfaceTexture.callMethod<void>("setOnFrameAvailableListener",
+                                      "(Landroid/graphics/SurfaceTexture$OnFrameAvailableListener;)V",
+                                      listener.object());
 }
 
 QT_END_NAMESPACE

src/plugins/android/src/wrappers/jni/androidsurfacetexture.h
@@ -48,7 +48,6 @@ public:
     explicit AndroidSurfaceTexture(unsigned int texName);
     ~AndroidSurfaceTexture();
 
-    int textureID() const { return m_texID; }
     jobject surfaceTexture();
     jobject surface();
     jobject surfaceHolder();
@@ -67,7 +66,8 @@ Q_SIGNALS:
     void frameAvailable();
 
 private:
-    int m_texID;
+    void setOnFrameAvailableListener(const QJNIObjectPrivate &listener);
+
     QJNIObjectPrivate m_surfaceTexture;
     QJNIObjectPrivate m_surface;
     QJNIObjectPrivate m_surfaceHolder;