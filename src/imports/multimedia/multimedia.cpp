eecd98d19211383ef197ecedc28ba57dee316944
VaL Doroshchuk <valentyn.doroshchuk@qt.io>
2019-02-25 15:50:21 +0100

    Add a revision of QML VideoOutput flushMode property to 5.13
    Change-Id: Ie7982232af45fd09a46854c96d7c7d6672857a97
    Reviewed-by: Kai Koehne <kai.koehne@qt.io>

--- src/imports/multimedia/multimedia.cpp
+++ src/imports/multimedia/multimedia.cpp
@@ -157,6 +157,9 @@ public:
         qmlRegisterType<QDeclarativeMediaMetaData>();
         qmlRegisterType<QAbstractVideoFilter>();
 
+        // 5.13 types
+        qmlRegisterType<QDeclarativeVideoOutput, 13>(uri, 5, 13, "VideoOutput");
+
         // Auto-increment the import to stay in sync with ALL future QtQuick minor versions from 5.11 onward
         qmlRegisterModule(uri, 5, QT_VERSION_MINOR);
     }
