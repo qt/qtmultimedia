abab792c4f0977a4599e5b7b2e97a5ee586fe388
Pablo Luis Garcia <pablo@skandal.tech>
2019-08-19 13:58:42 +0200

    macOS: Fix video rendering on with OpenGL Core profile
    CoreAnimation OpenGL renderer only has support for pre 3.0 profiles.
    Changed CoreAnimation renderer to use a Metal texture for rendering
    and use a pixel buffer to share the texture data with an OpenGL
    texture, making it possible to render with 3.2+ profiles.
    
    Fixes: QTBUG-51064
    Fixes: QTBUG-62694
    Change-Id: I48a4a6e0d8fbc48170dfe82d1e71cd265d70179a
    Pick-to: 5.15
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

--- src/plugins/avfoundation/mediaplayer/avfvideoframerenderer.h
+++ src/plugins/avfoundation/mediaplayer/avfvideoframerenderer.h
@@ -54,6 +54,7 @@
 QT_BEGIN_NAMESPACE
 
 class QOpenGLFramebufferObject;
+class QOpenGLShaderProgram;
 class QWindow;
 class QOpenGLContext;
 class QAbstractVideoSurface;
@@ -69,16 +70,49 @@ public:
     quint64 renderLayerToMTLTexture(AVPlayerLayer *layer);
     QImage renderLayerToImage(AVPlayerLayer *layer);
 
+    static GLuint createGLTexture(CGLContextObj cglContextObj, CGLPixelFormatObj cglPixelFormtObj,
+                                  CVOpenGLTextureCacheRef cvglTextureCache,
+                                  CVPixelBufferRef cvPixelBufferRef,
+                                  CVOpenGLTextureRef cvOpenGLTextureRef);
+
+    static id<MTLTexture> createMetalTexture(id<MTLDevice> mtlDevice,
+                                             CVMetalTextureCacheRef cvMetalTextureCacheRef,
+                                             CVPixelBufferRef cvPixelBufferRef,
+                                             MTLPixelFormat pixelFormat, size_t width, size_t height,
+                                             CVMetalTextureRef cvMetalTextureRef);
+
 private:
     QOpenGLFramebufferObject* initRenderer(AVPlayerLayer *layer);
     void renderLayerToFBO(AVPlayerLayer *layer, QOpenGLFramebufferObject *fbo);
+    void renderLayerToFBOCoreOpenGL(AVPlayerLayer *layer, QOpenGLFramebufferObject *fbo);
 
     CARenderer *m_videoLayerRenderer;
     QAbstractVideoSurface *m_surface;
     QOpenGLFramebufferObject *m_fbo[2];
+    QOpenGLShaderProgram *m_shader = nullptr;
     QWindow *m_offscreenSurface;
     QOpenGLContext *m_glContext;
     QSize m_targetSize;
+
+    bool m_useCoreProfile = false;
+
+    // Shared pixel buffer
+    CVPixelBufferRef m_CVPixelBuffer;
+
+    // OpenGL Texture
+    CVOpenGLTextureCacheRef m_CVGLTextureCache;
+    CVOpenGLTextureRef m_CVGLTexture;
+    CGLPixelFormatObj m_CGLPixelFormat;
+    GLuint m_textureName = 0;
+
+    // Metal Texture
+    CVMetalTextureRef m_CVMTLTexture;
+    CVMetalTextureCacheRef m_CVMTLTextureCache;
+
+    NSOpenGLContext *m_NSGLContext = nullptr;
+
+    GLuint m_quadVao = 0;
+    GLuint m_quadVbos[2];
 
     uint m_currentBuffer;
     bool m_isContextShared;
