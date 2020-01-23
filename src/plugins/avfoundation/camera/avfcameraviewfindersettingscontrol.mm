1e50366ea7cd8909f049f161a41afbd152a94579
Val Doroshchuk <valentyn.doroshchuk@qt.io>
2020-01-23 13:29:37 +0100

    AVF: Always lock the capture device when start the camera
    If the viewfinder settings are valid, need to lock the capture device
    to avoid reseting settings to default values.
    
    See also a8123e737140719549252806e0e6a1c121359f79
    
    Fixes: QTBUG-81048
    Change-Id: I834815ef6c5ef28d8017d04bdb9d5256c02d1de7
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>
    (cherry picked from commit 265bfb22e7f0f5333e584f6dd91926b8516e394e)

--- src/plugins/avfoundation/camera/avfcameraviewfindersettingscontrol.mm
+++ src/plugins/avfoundation/camera/avfcameraviewfindersettingscontrol.mm
@@ -185,6 +185,7 @@ void AVFCameraViewfinderSettingsControl2::setViewfinderSettings(const QCameraVie
         return;
 
     m_settings = settings;
+    applySettings(m_settings);
 }
 
 QVideoFrame::PixelFormat AVFCameraViewfinderSettingsControl2::QtPixelFormatFromCVFormat(unsigned avPixelFormat)
