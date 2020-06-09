qt/qtmultimedia

commit d8d072417b08dd75734b3f0aa86c3a49db934770

Gstreamer: fix stream buffering
Gstreamer client should handle GST_MESSAGE_BUFFERING to play and pause
streams based on buffer-percent value and Qt does it properly.
However `updateSessionState` of QGstreamerPlayerControl was implemented
to go back into playing state each time session state becomes paused.
That behavior resulted into choppy stream playback.
Fix with condition to un-pause playback only if it's already buffered.

Fixes: QTBUG-83417
Change-Id: Ida4a9e2e196de00050bdc64725fa818c7e939785
Reviewed-by: VaL Doroshchuk <valentyn.doroshchuk@qt.io>

src/gsttools/qgstreamerplayercontrol.cpp
@@ -439,8 +439,10 @@ void QGstreamerPlayerControl::updateSessionState(QMediaPlayer::State state)
         }
         m_pendingSeekPosition = -1;
 
-        if (m_currentState == QMediaPlayer::PlayingState)
-            m_session->play();
+        if (m_currentState == QMediaPlayer::PlayingState) {
+            if (m_mediaStatus == QMediaPlayer::BufferedMedia)
+                m_session->play();
+        }
     }
 
     updateMediaStatus();

commit 19cc3a018f2442221d566ec56bda2bdbdce7ec2e

GStreamer: Start playback when buffered
Regression since d8d072417b08dd75734b3f0aa86c3a49db934770

Pick-to: 5.15
Fixes: QTBUG-84556
Change-Id: I557ac7eb38ebdbb6c52e5348902ee8c3f1a91f07
Reviewed-by: <roman.valov@gmail.com>
Reviewed-by: Andy Shaw <andy.shaw@qt.io>

src/gsttools/qgstreamerplayercontrol.cpp
@@ -440,7 +440,7 @@ void QGstreamerPlayerControl::updateSessionState(QMediaPlayer::State state)
         m_pendingSeekPosition = -1;
 
         if (m_currentState == QMediaPlayer::PlayingState) {
-            if (m_mediaStatus == QMediaPlayer::BufferedMedia)
+            if (m_bufferProgress == -1 || m_bufferProgress == 100)
                 m_session->play();
         }
     }