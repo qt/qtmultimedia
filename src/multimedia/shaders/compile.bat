e6710a5290fbac8ea0545755f411db58f42e3ee5
Lars Knoll <lars.knoll@qt.io>
2021-03-26 11:23:25 +0100

    Provide a shader for the AYUV444 pixel format
    Change-Id: I073155bf052eb93eeb5c7b371cf58d791f894e3c
    Reviewed-by: Doris Verria <doris.verria@qt.io>
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- src/multimedia/shaders/compile.bat
+++ src/multimedia/shaders/compile.bat
@@ -48,5 +48,6 @@ qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o nv12.frag.qsb nv12.frag
 qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o nv21.frag.qsb nv21.frag
 qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o uyvy.frag.qsb uyvy.frag
 qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o yuyv.frag.qsb yuyv.frag
-qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o p010le.frag.qsb p010le.frag
+qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o ayuv.frag.qsb ayuv.frag
+qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o p010be.frag.qsb p010be.frag
 qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o p010be.frag.qsb p010be.frag
