qt/qtmultimedia

commit 3e913c0814464bd9b09307d2e3d7c6763a1481b3

Get rid of QMediaFormat::Mode
This is not needed as we can determine the mode from the encoder
settings' codecs

Change-Id: I1850494cad8fef1ce78f056bcbf8e8b6b0e98112
Reviewed-by: André de la Rocha <andre.rocha@qt.io>
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/multimedia/platform/gstreamer/mediacapture/qgstreamermediaencoder.cpp
@@ -263,7 +263,7 @@ static GstEncodingContainerProfile *createEncodingProfile(const QMediaEncoderSet
     auto *containerProfile = createContainerProfile(settings);
     if (containerProfile) {
         GstEncodingProfile *audioProfile = createAudioProfile(settings);
-        GstEncodingProfile *videoProfile = settings.mode() & QMediaFormat::AudioAndVideo ? createVideoProfile(settings) : nullptr;
+        GstEncodingProfile *videoProfile = settings.videoCodec() != QMediaFormat::VideoCodec::Unspecified ? createVideoProfile(settings) : nullptr;
         qDebug() << "audio profile" << gst_caps_to_string(gst_encoding_profile_get_format(audioProfile));
         qDebug() << "video profile" << gst_caps_to_string(gst_encoding_profile_get_format(videoProfile));
         qDebug() << "conta profile" << gst_caps_to_string(gst_encoding_profile_get_format((GstEncodingProfile *)containerProfile));
@@ -331,7 +331,7 @@ void QGstreamerMediaEncoder::record()
     QGstPad audioPad = gstEncoder.getRequestPad("audio_%u");
     audioSrcPad.link(audioPad);
 
-    if (m_settings.mode() == QMediaFormat::AudioAndVideo) {
+    if (m_settings.videoCodec() != QMediaFormat::VideoCodec::Unspecified) {
         videoSrcPad = m_session->getVideoPad();
         if (!videoSrcPad.isNull()) {
             QGstPad videoPad = gstEncoder.getRequestPad("video_%u");
@@ -400,20 +400,27 @@ void QGstreamerMediaEncoder::finalize()
     updateStatus();
 }
 
-void QGstreamerMediaEncoder::applySettings()
+void QGstreamerMediaEncoder::updateSettings()
 {
+    applySettings();
 }
 
-void QGstreamerMediaEncoder::setEncoderSettings(const QMediaEncoderSettings &settings)
+void QGstreamerMediaEncoder::applySettings()
 {
     if (!m_session)
         return;
-    m_settings = settings;
-    m_settings.resolveFormat();
+    const auto flag = m_session->camera() ? QMediaEncoderSettings::RequiresVideo
+                                          : QMediaEncoderSettings::NoFlags;
+    m_settings.resolveFormat(flag);
 
     auto *encodingProfile = createEncodingProfile(m_settings);
     g_object_set (gstEncoder.object(), "profile", encodingProfile, nullptr);
     gst_encoding_profile_unref(encodingProfile);
+}
+
+void QGstreamerMediaEncoder::setEncoderSettings(const QMediaEncoderSettings &settings)
+{
+    m_settings = settings;
 }
 
 void QGstreamerMediaEncoder::setMetaData(const QMediaMetaData &metaData)
@@ -463,13 +470,15 @@ void QGstreamerMediaEncoder::setCaptureSession(QPlatformMediaCaptureSession *ses
 
     // ensure we have a usable format
     setEncoderSettings(QMediaEncoderSettings());
+    connect(m_session, &QGstreamerMediaCapture::cameraChanged,
+            this, &QGstreamerMediaEncoder::updateSettings);
 }
 
 QDir QGstreamerMediaEncoder::defaultDir() const
 {
     QStringList dirCandidates;
 
-    if (m_settings.mode() == QMediaFormat::AudioAndVideo)
+    if (m_settings.videoCodec() != QMediaFormat::VideoCodec::Unspecified)
         dirCandidates << QStandardPaths::writableLocation(QStandardPaths::MoviesLocation);
     else
         dirCandidates << QStandardPaths::writableLocation(QStandardPaths::MusicLocation);

src/multimedia/platform/gstreamer/mediacapture/qgstreamermediaencoder_p.h
@@ -105,6 +105,7 @@ private slots:
     void handleSessionError(int code, const QString &description);
     void updateDuration();
     void finalize();
+    void updateSettings();
 
 private:
     QDir defaultDir() const;

src/multimedia/platform/android/mediacapture/qandroidcapturesession.cpp
@@ -358,8 +358,10 @@ qint64 QAndroidCaptureSession::duration() const
 
 void QAndroidCaptureSession::setEncoderSettings(const QMediaEncoderSettings &settings)
 {
+    const auto flag = (m_cameraSession ? QMediaEncoderSettings::ResolveFlags::RequiresVideo
+                                       : QMediaEncoderSettings::ResolveFlags::NoFlags;
     m_encoderSettings = settings;
-    m_encoderSettings.resolveFormat();
+    m_encoderSettings.resolveFormat(flag);
     m_encoderSettingsDirty = true;
 }
 

src/multimedia/platform/darwin/camera/avfmediaencoder.mm
@@ -370,11 +370,14 @@ void AVFMediaEncoder::applySettings()
     if (m_state != QMediaEncoder::StoppedState)
         return;
 
-    auto encoderSettings = m_settings;
-    encoderSettings.resolveFormat();
+    const auto flag = (session->activeCameraInfo().isNull())
+                              ? QMediaEncoderSettings::ResolveFlags::NoFlags
+                              : QMediaEncoderSettings::ResolveFlags::RequiresVideo;
+
+    m_settings.resolveFormat(flag);
 
     // audio settings
-    m_audioSettings = avfAudioSettings(encoderSettings);
+    m_audioSettings = avfAudioSettings(m_settings);
     if (m_audioSettings)
         [m_audioSettings retain];
 
@@ -384,7 +387,7 @@ void AVFMediaEncoder::applySettings()
         return;
     const AVFConfigurationLock lock(device); // prevents activeFormat from being overridden
     AVCaptureConnection *conn = [session->videoOutput()->videoDataOutput() connectionWithMediaType:AVMediaTypeVideo];
-    m_videoSettings = avfVideoSettings(encoderSettings, device, conn);
+    m_videoSettings = avfVideoSettings(m_settings, device, conn);
     if (m_videoSettings)
         [m_videoSettings retain];
 }
@@ -404,6 +407,16 @@ void AVFMediaEncoder::unapplySettings()
 void AVFMediaEncoder::setEncoderSettings(const QMediaEncoderSettings &settings)
 {
     m_settings = settings;
+}
+
+QMediaEncoderSettings AVFMediaEncoder::encoderSettings() const
+{
+    QMediaEncoderSettings s = m_settings;
+    const auto flag = (m_service->session()->activeCameraInfo().isNull())
+                            ? QMediaEncoderSettings::ResolveFlags::NoFlags
+                            : QMediaEncoderSettings::ResolveFlags::RequiresVideo;
+    s.resolveFormat(flag);
+    return s;
 }
 
 void AVFMediaEncoder::setCaptureSession(QPlatformMediaCaptureSession *session)
@@ -454,7 +467,7 @@ void AVFMediaEncoder::setState(QMediaEncoder::State state)
 
 void AVFMediaEncoder::record()
 {
-    const bool audioOnly = m_settings.mode() == QMediaFormat::AudioOnly;
+    const bool audioOnly = m_settings.videoCodec() == QMediaFormat::VideoCodec::Unspecified;
     AVCaptureSession *session = m_service->session()->captureSession();
     float rotation = 0;
 

src/multimedia/platform/darwin/camera/avfmediaencoder_p.h
@@ -89,11 +89,7 @@ public:
     void unapplySettings();
 
     void setEncoderSettings(const QMediaEncoderSettings &settings) override;
-    QMediaEncoderSettings encoderSettings() const {
-        QMediaEncoderSettings s = m_settings;
-        s.resolveFormat();
-        return s;
-    }
+    QMediaEncoderSettings encoderSettings() const;
 
     AVFCameraService *cameraService() const { return m_service; }
 

src/multimedia/platform/qplatformmediaformatinfo.cpp
@@ -52,8 +52,6 @@ QList<QMediaFormat::FileFormat> QPlatformMediaFormatInfo::supportedFileFormats(c
 
     const auto &codecMap = (m == QMediaFormat::Encode) ? encoders : decoders;
     for (const auto &m : codecMap) {
-        if (constraints.mode() == QMediaFormat::AudioAndVideo && m.video.isEmpty())
-            continue;
         if (constraints.audioCodec() != QMediaFormat::AudioCodec::Unspecified && !m.audio.contains(constraints.audioCodec()))
             continue;
         if (constraints.videoCodec() != QMediaFormat::VideoCodec::Unspecified && !m.video.contains(constraints.videoCodec()))
@@ -81,9 +79,6 @@ QList<QMediaFormat::AudioCodec> QPlatformMediaFormatInfo::supportedAudioCodecs(c
 
 QList<QMediaFormat::VideoCodec> QPlatformMediaFormatInfo::supportedVideoCodecs(const QMediaFormat &constraints, QMediaFormat::ConversionMode m) const
 {
-    if (constraints.mode() == QMediaFormat::AudioOnly)
-        return {};
-
     QSet<QMediaFormat::VideoCodec> codecs;
 
     const auto &codecMap = (m == QMediaFormat::Encode) ? encoders : decoders;
@@ -107,7 +102,7 @@ bool QPlatformMediaFormatInfo::isSupported(const QMediaFormat &format, QMediaFor
             continue;
         if (!m.audio.contains(format.audioCodec()))
             continue;
-        if (format.mode() == QMediaFormat::AudioAndVideo && !m.video.contains(format.videoCodec()))
+        if (format.videoCodec() != QMediaFormat::VideoCodec::Unspecified && !m.video.contains(format.videoCodec()))
             continue;
         return true;
     }

src/multimedia/qmediaformat.cpp
@@ -91,9 +91,8 @@ const char *mimeTypeForFormat[QMediaFormat::LastFileFormat + 2] =
 */
 
 // these are non inline to make a possible future addition of a d pointer binary compatible
-QMediaFormat::QMediaFormat(FileFormat format, Mode mode)
-    : fmtMode(mode),
-      fmt(format)
+QMediaFormat::QMediaFormat(FileFormat format)
+    : fmt(format)
 {
     Q_UNUSED(d);
 }
@@ -329,8 +328,7 @@ QString QMediaFormat::videoCodecDescription(QMediaFormat::VideoCodec c)
 bool QMediaFormat::operator==(const QMediaFormat &other) const
 {
     Q_ASSERT(!d);
-    return fmtMode == other.fmtMode &&
-            fmt == other.fmt &&
+    return fmt == other.fmt &&
             audio == other.audio &&
             video == other.video;
 }

src/multimedia/qmediaformat.h
@@ -108,12 +108,7 @@ public:
         Decode
     };
 
-    enum Mode {
-        AudioOnly,
-        AudioAndVideo
-    };
-
-    QMediaFormat(FileFormat format = UnspecifiedFormat, Mode = AudioAndVideo);
+    QMediaFormat(FileFormat format = UnspecifiedFormat);
     ~QMediaFormat();
     QMediaFormat(const QMediaFormat &other);
     QMediaFormat &operator=(const QMediaFormat &other);
@@ -128,9 +123,6 @@ public:
     AudioCodec audioCodec() const { return audio; }
 
     bool isSupported(ConversionMode mode) const;
-
-    void setMode(Mode m) { fmtMode = m; }
-    Mode mode() const { return fmtMode; }
 
     QMimeType mimeType() const;
 
@@ -152,7 +144,6 @@ public:
 
 protected:
     friend class QMediaFormatPrivate;
-    Mode fmtMode;
     FileFormat fmt;
     AudioCodec audio = AudioCodec::Unspecified;
     VideoCodec video = VideoCodec::Unspecified;

commit 63a073533f1fe283d9f7804a2c4bdff54f6d9522

Fix camera not showing up on gstreamer
Apparently, the gstreamer encodebin needs an encoding to
be set up very early on. Fixes a regression after change
3e913c0814464bd9b09307d2e3d7c6763a1481b3.

Change-Id: I7cb223a46ff2934dbffe30cc2a803eb52cb297a8
Reviewed-by: Doris Verria <doris.verria@qt.io>
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/multimedia/platform/gstreamer/mediacapture/qgstreamermediaencoder.cpp
@@ -296,7 +296,7 @@ void QGstreamerMediaEncoder::record()
 
     // create new encoder
     if (m_requestedOutputLocation.isEmpty()) {
-        QString container = m_settings.mimeType().preferredSuffix();
+        QString container = m_resolvedSettings.mimeType().preferredSuffix();
         m_outputLocation = QUrl(generateFileName(defaultDir(), container));
     }
     QUrl actualSink = QUrl::fromLocalFile(QDir::currentPath()).resolved(m_outputLocation);
@@ -314,7 +314,7 @@ void QGstreamerMediaEncoder::record()
     QGstPad audioPad = gstEncoder.getRequestPad("audio_%u");
     audioSrcPad.link(audioPad);
 
-    if (m_settings.videoCodec() != QMediaFormat::VideoCodec::Unspecified) {
+    if (m_resolvedSettings.videoCodec() != QMediaFormat::VideoCodec::Unspecified) {
         videoSrcPad = m_session->getVideoPad();
         if (!videoSrcPad.isNull()) {
             QGstPad videoPad = gstEncoder.getRequestPad("video_%u");
@@ -393,9 +393,10 @@ void QGstreamerMediaEncoder::applySettings()
         return;
     const auto flag = m_session->camera() ? QMediaEncoderSettings::RequiresVideo
                                           : QMediaEncoderSettings::NoFlags;
-    m_settings.resolveFormat(flag);
+    m_resolvedSettings = m_settings;
+    m_resolvedSettings.resolveFormat(flag);
 
-    auto *encodingProfile = createEncodingProfile(m_settings);
+    auto *encodingProfile = createEncodingProfile(m_resolvedSettings);
     g_object_set (gstEncoder.object(), "profile", encodingProfile, nullptr);
     gst_encoding_profile_unref(encodingProfile);
 }
@@ -403,6 +404,7 @@ void QGstreamerMediaEncoder::applySettings()
 void QGstreamerMediaEncoder::setEncoderSettings(const QMediaEncoderSettings &settings)
 {
     m_settings = settings;
+    applySettings();
 }
 
 void QGstreamerMediaEncoder::setMetaData(const QMediaMetaData &metaData)
@@ -460,7 +462,7 @@ QDir QGstreamerMediaEncoder::defaultDir() const
 {
     QStringList dirCandidates;
 
-    if (m_settings.videoCodec() != QMediaFormat::VideoCodec::Unspecified)
+    if (m_resolvedSettings.videoCodec() != QMediaFormat::VideoCodec::Unspecified)
         dirCandidates << QStandardPaths::writableLocation(QStandardPaths::MoviesLocation);
     else
         dirCandidates << QStandardPaths::writableLocation(QStandardPaths::MusicLocation);

src/multimedia/platform/gstreamer/mediacapture/qgstreamermediaencoder_p.h
@@ -106,6 +106,7 @@ private:
     QUrl m_requestedOutputLocation;
     QUrl m_outputLocation;
     QMediaEncoderSettings m_settings;
+    QMediaEncoderSettings m_resolvedSettings;
     QGstreamerMediaCapture *m_session = nullptr;
     QGstreamerMetaData m_metaData;
     QElapsedTimer m_duration;