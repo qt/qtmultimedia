dd49b60f636476dad8946ed91364dbe7d26e8527
Iikka Eklund <iikka.eklund@qt.io>
2021-06-28 10:15:33 +0300

    Support builds with Conan
    The recipe uses Conan's supported way to inherit from a given
    base class via 'python_requires_extend'.
    
    The qt-conan-common package implements a base class for Qt leaf
    module recipes. The build steps in leaf modules are mostly identical
    so it makes sense to put those in the base class.
    
    Dependencies are read by the base class from the 'dependencies.yaml'
    which is the same file the CI system uses.
    
    Task-number: QTBUG-94814
    Change-Id: I8796766dbad4ff9061d78eb56711b24149e21c09
    Reviewed-by: Ievgenii Meshcheriakov <ievgenii.meshcheriakov@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: Toni Saario <toni.saario@qt.io>
    (cherry picked from commit c4f2536b5fa5808b18a6e05897d1a6c5153e9d53)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ conanfile.py
@@ -0,0 +1,61 @@
+#############################################################################
+##
+## Copyright (C) 2021 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the release tools of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+from conans import ConanFile
+import re
+from pathlib import Path
+
+
+def _parse_qt_version_by_key(key: str) -> str:
+    with open(Path(__file__).parent.resolve() / ".cmake.conf") as f:
+        m = re.search(fr'{key} .*"(.*)"', f.read())
+    return m.group(1) if m else ""
+
+
+def _get_qt_minor_version() -> str:
+    return ".".join(_parse_qt_version_by_key("QT_REPO_MODULE_VERSION").split(".")[:2])
+
+
+class QtMultimedia(ConanFile):
+    name = "qtmultimedia"
+    license = "LGPL-3.0-only"
+    author = "The Qt Company <https://www.qt.io/contact-us>"
+    url = "https://code.qt.io/cgit/qt/qtmultimedia.git"
+    description = (
+        "Qt Multimedia is an essential module that provides a rich set of QML types and C++ "
+        "classes to handle multimedia content. It also provides necessary APIs to access "
+        "camera and radio functionality. The included Qt Audio Engine provides types for 3D "
+        "positional audio playback and content management."
+    )
+    topics = "qt", "qt6", "qtmultimedia", "multimedia"
+    settings = "os", "compiler", "arch", "build_type"
+    # for referencing the version number and prerelease tag and dependencies info
+    exports = ".cmake.conf", "dependencies.yaml"
+    exports_sources = "*", "!conan*.*"
+    python_requires = f"qt-conan-common/{_get_qt_minor_version()}@qt/everywhere"
+    python_requires_extend = "qt-conan-common.QtLeafModule"
