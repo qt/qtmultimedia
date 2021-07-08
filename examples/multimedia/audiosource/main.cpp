1ce3c811d9b3d871be878fcf1e9fcb4bab5b962e
Kai Köhne <kai.koehne@qt.io>
2021-07-08 15:25:17 +0200

    Rename example to audiosource
    Amends 1c4dd0c23c0a
    
    Pick-to: 6.2
    Change-Id: Id84a6b12b5e15b20be265858446a4fca1517f77e
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- examples/multimedia/audiosource/main.cpp
+++ examples/multimedia/audiosource/main.cpp
@@ -55,7 +55,7 @@
 int main(int argv, char **args)
 {
     QApplication app(argv, args);
-    app.setApplicationName("Audio Input Test");
+    app.setApplicationName("Audio Source Test");
 
     InputTest input;
     input.show();
