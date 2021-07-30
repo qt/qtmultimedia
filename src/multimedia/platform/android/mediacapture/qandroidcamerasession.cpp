64ce7f9c63ca891425656a29fd59db869c91efd1
Bartlomiej Moskal <bartlomiej.moskal@qt.io>
2021-07-30 14:51:14 +0200

    Auto test fix after run on Android: qcamerabackend
    Commit contains two fixes:
    1. QCameraDevice is set to default since commit:
    e045ee08c84fbc6d1c6837e34c30f88c09cd2cdb. That is why check for not
    starting up camera (without setting device) need to be removed;
    2. After commit 2f018aad29cebe4b4db3f69ff2d5e11f8ba2cad2 there was an
    assertion in android cameraSession for starting preview (for not set
    video output). Assertion is removed from code. Before testing for camera
    start - video output is set;
    
    Change-Id: I9931d49c8388c92f7159b302afe5e1f5e629e764
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- src/multimedia/platform/android/mediacapture/qandroidcamerasession.cpp
+++ src/multimedia/platform/android/mediacapture/qandroidcamerasession.cpp
@@ -341,13 +341,11 @@ QList<AndroidCamera::FpsRange> QAndroidCameraSession::getSupportedPreviewFpsRang
 
 bool QAndroidCameraSession::startPreview()
 {
-    if (!m_camera)
+    if (!m_camera || !m_videoOutput)
         return false;
 
     if (m_previewStarted)
         return true;
-
-    Q_ASSERT(m_videoOutput);
 
     if (!m_videoOutput->isReady())
         return true; // delay starting until the video output is ready
