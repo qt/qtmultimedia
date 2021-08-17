qt/qtmultimedia

commit c2e3377dd8d3c041d2777f2c4a785f6ff8866556

Fix video output in the capture session on macOS
Do not add the AVFCameraRenderer only until a camera is added to
the capture session. A videoOutput may have been set to the
session before that. Instead, add it when the latter is set to
the session. Also, make sure to set the renderer's videoSink to
null when removed.

Change-Id: I450c66909240ae95ae3f1378dc11b2b5c67aa992
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/multimedia/platform/darwin/camera/avfcamerasession.mm
@@ -186,8 +186,6 @@ void AVFCameraSession::setActiveCamera(const QCameraDevice &info)
     if (m_activeCameraDevice != info) {
         m_activeCameraDevice = info;
         attachVideoInputDevice();
-        if (!m_activeCameraDevice.isNull() && !m_videoOutput)
-            setVideoOutput(new AVFCameraRenderer(this));
     }
 }
 
@@ -474,10 +472,15 @@ void AVFCameraSession::setVideoSink(QVideoSink *sink)
 
     m_videoSink = videoSink;
 
-    if (m_videoSink && m_videoOutput) {
+    if (m_videoSink && !m_videoOutput)
+        setVideoOutput(new AVFCameraRenderer(this));
+
+    if (m_videoOutput) {
         m_videoOutput->setVideoSink(videoSink);
-        AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:m_captureSession];
-        m_videoOutput->setLayer(previewLayer);
+        if (m_videoSink) {
+            AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:m_captureSession];
+            m_videoOutput->setLayer(previewLayer);
+        }
     }
 }
 

commit da46561373d0731b479606405109fe7c34eec1da

macOS: Fix image capture part 2
Change c2e3377dd8d3c041d2777f2c4a785f6ff8866556 introduced a
regression with image capture where camera times out waiting for a
preview frame. Partly fix this by connecting AVFImageCapture's
slot triggered on a new viewfinder's frame, to the AVFCameraSession::
newVideoFrame signal instead of the AVFCameraRenderer, as the image
capture doesn't know when the video output is added to the session.

In addition, add AVFCameraRenderer to the session when a camera is
first attached and update accordingly when a video sink is set.

Pick-to: 6.2
Change-Id: I32645081e9c1d032691ac23f5ef658e252106c68
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/multimedia/platform/darwin/camera/avfcamerasession.mm
@@ -186,6 +186,13 @@ void AVFCameraSession::setActiveCamera(const QCameraDevice &info)
     if (m_activeCameraDevice != info) {
         m_activeCameraDevice = info;
         attachVideoInputDevice();
+
+        if (!m_activeCameraDevice.isNull() && !m_videoOutput) {
+            setVideoOutput(new AVFCameraRenderer(this));
+            connect(m_videoOutput, &AVFCameraRenderer::newViewfinderFrame,
+                     this, &AVFCameraSession::newViewfinderFrame);
+        }
+        updateVideoOutput();
     }
 }
 
@@ -479,16 +486,7 @@ void AVFCameraSession::setVideoSink(QVideoSink *sink)
 
     m_videoSink = videoSink;
 
-    if (m_videoSink && !m_videoOutput)
-        setVideoOutput(new AVFCameraRenderer(this));
-
-    if (m_videoOutput) {
-        m_videoOutput->setVideoSink(videoSink);
-        if (m_videoSink) {
-            AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:m_captureSession];
-            m_videoOutput->setLayer(previewLayer);
-        }
-    }
+    updateVideoOutput();
 }
 
 void AVFCameraSession::updateAudioInput()
@@ -515,6 +513,17 @@ void AVFCameraSession::updateAudioOutput()
         m_audioRenderer.audioOutputDeviceUniqueID = [NSString stringWithUTF8String:
                                                              deviceId.constData()];
 #endif
+    }
+}
+
+void AVFCameraSession::updateVideoOutput()
+{
+    if (m_videoOutput) {
+        m_videoOutput->setVideoSink(m_videoSink);
+        if (m_videoSink) {
+            AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:m_captureSession];
+            m_videoOutput->setLayer(previewLayer);
+        }
     }
 }
 

src/multimedia/platform/darwin/camera/avfcamerasession_p.h
@@ -114,10 +114,13 @@ public Q_SLOTS:
     void processSessionStarted();
     void processSessionStopped();
 
+    void updateVideoOutput();
+
 Q_SIGNALS:
     void readyToConfigureConnections();
     void activeChanged(bool);
     void error(int error, const QString &errorString);
+    void newViewfinderFrame(const QVideoFrame &frame);
 
 private:
     void setVideoOutput(AVFCameraRenderer *output);

src/multimedia/platform/darwin/camera/avfimagecapture.mm
@@ -232,10 +232,6 @@ void AVFImageCapture::onCameraChanged()
     m_cameraControl = static_cast<AVFCamera *>(m_service->camera());
     if (m_cameraControl)
         connect(m_cameraControl, SIGNAL(activeChanged(bool)), this, SLOT(updateReadyStatus()));
-    if (m_session->videoOutput())
-        connect(m_session->videoOutput(), &AVFCameraRenderer::newViewfinderFrame,
-                this, &AVFImageCapture::onNewViewfinderFrame,
-                Qt::DirectConnection);
 }
 
 void AVFImageCapture::makeCapturePreview(CaptureRequest request,
@@ -397,6 +393,8 @@ void AVFImageCapture::setCaptureSession(QPlatformMediaCaptureSession *session)
 
     connect(m_service, &AVFCameraService::cameraChanged, this, &AVFImageCapture::onCameraChanged);
     connect(m_session, SIGNAL(readyToConfigureConnections()), SLOT(updateCaptureConnection()));
+    connect(m_session, &AVFCameraSession::newViewfinderFrame,
+                 this, &AVFImageCapture::onNewViewfinderFrame);
 
     updateCaptureConnection();
     updateReadyStatus();