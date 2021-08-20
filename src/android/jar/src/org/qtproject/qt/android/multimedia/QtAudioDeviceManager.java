f5261a41adcdf7a681212769c3ac5883e93bde73
Assam Boudjelthia <assam.boudjelthia@qt.io>
2021-08-20 11:01:40 +0300

    Android: use correct flag in QtAudioDeviceManager.setAudioInput()
    Use GET_DEVICES_INPUTS instead GET_DEVICES_ALL when getting devices
    in setAudioInput().
    
    Change-Id: I1626fb9610d05e483dfb102aaf6b3d1e46a11dd3
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>
    (cherry picked from commit 73ca28f731aaa1287642991efe19d7080558456e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/android/jar/src/org/qtproject/qt/android/multimedia/QtAudioDeviceManager.java
+++ src/android/jar/src/org/qtproject/qt/android/multimedia/QtAudioDeviceManager.java
@@ -106,8 +106,8 @@ public class QtAudioDeviceManager
     private static boolean setAudioInput(MediaRecorder recorder, int id)
     {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
-            final AudioDeviceInfo[] audioDevices =
-                                            m_audioManager.getDevices(AudioManager.GET_DEVICES_ALL);
+            final AudioDeviceInfo[] audioDevices;
+            audioDevices = m_audioManager.getDevices(AudioManager.GET_DEVICES_INPUTS);
             for (AudioDeviceInfo deviceInfo : audioDevices) {
                if (deviceInfo.getId() ==  id)
                    return recorder.setPreferredDevice(deviceInfo);
