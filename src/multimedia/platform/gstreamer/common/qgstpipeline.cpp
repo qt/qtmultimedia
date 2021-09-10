qt/qtmultimedia

commit 8d5182d03c1967d77ca8da531b004e85bb0892f0

Move gstreamer over to the new subtitle handling
Implement a custom sink for subtitles that notifies the video
sink about changes in the subtitles. Remove the subtitleoverlay
we have been using so far.

Pick-to: 6.2
Change-Id: I6ecfb1ab8538169d7365f21e0c0648b4aa2cd769
Reviewed-by: Piotr Srebrny <piotr.srebrny@qt.io>
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/multimedia/platform/gstreamer/common/qgstpipeline.cpp
@@ -66,6 +66,7 @@ public:
     mutable qint64 m_position = 0;
     double m_rate = 1.;
     bool m_flushOnConfigChanges = false;
+    bool m_pendingFlush = false;
 
     int m_configCounter = 0;
     GstState m_savedState = GST_STATE_NULL;
@@ -271,6 +272,16 @@ void QGstPipeline::removeMessageFilter(QGstreamerBusMessageFilter *filter)
     d->removeMessageFilter(filter);
 }
 
+GstStateChangeReturn QGstPipeline::setState(GstState state)
+{
+    auto retval = gst_element_set_state(element(), state);
+    if (d->m_pendingFlush) {
+        d->m_pendingFlush = false;
+        flush();
+    }
+    return retval;
+}
+
 void QGstPipeline::beginConfig()
 {
     if (!d)
@@ -296,8 +307,7 @@ void QGstPipeline::endConfig()
     if (d->m_configCounter)
         return;
 
-    if (d->m_savedState != GST_STATE_NULL && d->m_flushOnConfigChanges)
-        flush();
+    d->m_pendingFlush = true;
     if (d->m_savedState == GST_STATE_PLAYING)
         setState(GST_STATE_PLAYING);
     d->m_savedState = GST_STATE_NULL;

src/multimedia/CMakeLists.txt
@@ -212,6 +212,7 @@ qt_internal_extend_target(Multimedia CONDITION QT_FEATURE_gstreamer
         platform/gstreamer/common/qgstutils.cpp platform/gstreamer/common/qgstutils_p.h
         platform/gstreamer/common/qgstvideobuffer.cpp platform/gstreamer/common/qgstvideobuffer_p.h
         platform/gstreamer/common/qgstvideorenderersink.cpp platform/gstreamer/common/qgstvideorenderersink_p.h
+        platform/gstreamer/common/qgstsubtitlesink.cpp platform/gstreamer/common/qgstsubtitlesink_p.h
         platform/gstreamer/qgstreamermediadevices.cpp platform/gstreamer/qgstreamermediadevices_p.h
         platform/gstreamer/qgstreamerformatinfo.cpp platform/gstreamer/qgstreamerformatinfo_p.h
         platform/gstreamer/qgstreamerintegration.cpp platform/gstreamer/qgstreamerintegration_p.h

src/multimedia/platform/gstreamer/common/qgst_p.h
@@ -377,6 +377,9 @@ public:
 
     GstPad *pad() const { return GST_PAD_CAST(object()); }
 
+    GstEvent *stickyEvent(GstEventType type) { return gst_pad_get_sticky_event(pad(), type, 0); }
+    bool sendEvent(GstEvent *event) { return gst_pad_send_event (pad(), event); }
+
     template<auto Member, typename T>
     void addProbe(T *instance, GstPadProbeType type) {
         struct Impl {

src/multimedia/platform/gstreamer/common/qgstpipeline_p.h
@@ -100,6 +100,8 @@ public:
     void installMessageFilter(QGstreamerBusMessageFilter *filter);
     void removeMessageFilter(QGstreamerBusMessageFilter *filter);
 
+    GstStateChangeReturn setState(GstState state);
+
     GstPipeline *pipeline() const { return GST_PIPELINE_CAST(m_object); }
 
     void dumpGraph(const char *fileName)

src/multimedia/platform/gstreamer/common/qgstreamermediaplayer.cpp
@@ -78,6 +78,11 @@ QGstreamerMediaPlayer::QGstreamerMediaPlayer(QMediaPlayer *parent)
     inputSelector[AudioStream] = QGstElement("input-selector", "audioInputSelector");
     inputSelector[VideoStream] = QGstElement("input-selector", "videoInputSelector");
     inputSelector[SubtitleStream] = QGstElement("input-selector", "subTitleInputSelector");
+    for (int i = 0; i < 3; ++i)
+        inputSelector[i].set ("sync-streams", true);
+
+    inputSelector[SubtitleStream].set("sync-mode", 1 /*clock*/);
+    inputSelector[SubtitleStream].set("cache-buffers", true);
 
     playerPipeline.add(inputSelector[AudioStream], inputSelector[VideoStream], inputSelector[SubtitleStream]);
 
@@ -795,6 +800,10 @@ void QGstreamerMediaPlayer::setActiveTrack(QPlatformMediaPlayer::TrackType type,
     auto &selector = inputSelector[type];
     if (selector.isNull())
         return;
+
+    if (type == QPlatformMediaPlayer::SubtitleStream)
+        gstVideoOutput->flushSubtitles();
+
     selector.set("active-pad", streams.at(index));
     // seek to force an immediate change of the stream
     if (playerPipeline.state() == GST_STATE_PLAYING)

src/multimedia/platform/gstreamer/common/qgstreamermediaplayer_p.h
@@ -115,6 +115,7 @@ public:
 
     bool processBusMessage(const QGstreamerMessage& message) override;
     bool processSyncMessage(const QGstreamerMessage& message) override;
+
 public Q_SLOTS:
     void updatePosition() { positionChanged(position()); }
 

src/multimedia/platform/gstreamer/common/qgstreamervideooutput.cpp
@@ -39,6 +39,7 @@
 
 #include <private/qgstreamervideooutput_p.h>
 #include <private/qgstreamervideosink_p.h>
+#include <private/qgstsubtitlesink_p.h>
 #include <qvideosink.h>
 
 #include <QtCore/qloggingcategory.h>
@@ -55,10 +56,8 @@ QGstreamerVideoOutput::QGstreamerVideoOutput(QObject *parent)
     videoQueue = QGstElement("queue", "videoQueue");
     videoConvert = QGstElement("videoconvert", "videoConvert");
     videoSink = QGstElement("fakesink", "fakeVideoSink");
-    subTitleQueue = QGstElement("queue", "subtitleQueue");
-    subtitleOverlay = QGstElement("subtitleoverlay", "subtitleoverlay");
-    gstVideoOutput.add(videoQueue, subtitleOverlay, videoConvert, videoSink, subTitleQueue);
-    if (!videoQueue.link(subtitleOverlay, videoConvert, videoSink))
+    gstVideoOutput.add(videoQueue, videoConvert, videoSink);
+    if (!videoQueue.link(videoConvert, videoSink))
         qCDebug(qLcMediaVideoOutput) << ">>>>>> linking failed";
 
     gstVideoOutput.addGhostPad(videoQueue, "sink");
@@ -72,19 +71,19 @@ QGstreamerVideoOutput::~QGstreamerVideoOutput()
 void QGstreamerVideoOutput::setVideoSink(QVideoSink *sink)
 {
     auto *gstVideoSink = sink ? static_cast<QGstreamerVideoSink *>(sink->platformVideoSink()) : nullptr;
-    if (gstVideoSink == m_videoWindow)
+    if (gstVideoSink == m_videoSink)
         return;
 
-    if (m_videoWindow)
-        m_videoWindow->setPipeline({});
+    if (m_videoSink)
+        m_videoSink->setPipeline({});
 
-    m_videoWindow = gstVideoSink;
-    if (m_videoWindow)
-        m_videoWindow->setPipeline(gstPipeline);
+    m_videoSink = gstVideoSink;
+    if (m_videoSink)
+        m_videoSink->setPipeline(gstPipeline);
 
     QGstElement gstSink;
-    if (m_videoWindow) {
-        gstSink = m_videoWindow->gstSink();
+    if (m_videoSink) {
+        gstSink = m_videoSink->gstSink();
         isFakeSink = false;
     } else {
         gstSink = QGstElement("fakesink", "fakevideosink");
@@ -107,6 +106,8 @@ void QGstreamerVideoOutput::setVideoSink(QVideoSink *sink)
     gst_element_send_event(videoSink.element(), event);
     videoSink.setState(GST_STATE_PAUSED);
 
+    doLinkSubtitleStream();
+
     gstPipeline.endConfig();
 
     qCDebug(qLcMediaVideoOutput) << "sinkChanged" << gstSink.name();
@@ -121,8 +122,8 @@ void QGstreamerVideoOutput::setVideoSink(QVideoSink *sink)
 void QGstreamerVideoOutput::setPipeline(const QGstPipeline &pipeline)
 {
     gstPipeline = pipeline;
-    if (m_videoWindow)
-        m_videoWindow->setPipeline(gstPipeline);
+    if (m_videoSink)
+        m_videoSink->setPipeline(gstPipeline);
 }
 
 void QGstreamerVideoOutput::linkSubtitleStream(QGstElement src)
@@ -132,20 +133,26 @@ void QGstreamerVideoOutput::linkSubtitleStream(QGstElement src)
         return;
 
     gstPipeline.beginConfig();
-
-    if (!subtitleSrc.isNull()) {
-        subtitleSrc.unlink(subTitleQueue);
-        subTitleQueue.unlink(subtitleOverlay);
-    }
     subtitleSrc = src;
-    if (!subtitleSrc.isNull()) {
-        if (!subtitleSrc.link(subTitleQueue))
-            qCDebug(qLcMediaVideoOutput) << "link subtitle stream 1 failed";
-        if (!subTitleQueue.link(subtitleOverlay))
-            qCDebug(qLcMediaVideoOutput) << "link subtitle stream 1 failed";
-    }
-
+    doLinkSubtitleStream();
     gstPipeline.endConfig();
+}
+
+void QGstreamerVideoOutput::doLinkSubtitleStream()
+{
+    if (!subtitleSink.isNull()) {
+        subtitleSink.setStateSync(GST_STATE_NULL);
+        gstPipeline.remove(subtitleSink);
+        subtitleSink = {};
+    }
+    if (!m_videoSink || subtitleSrc.isNull())
+        return;
+    if (subtitleSink.isNull()) {
+        subtitleSink = m_videoSink->subtitleSink();
+        gstPipeline.add(subtitleSink);
+    }
+    if (!subtitleSrc.link(subtitleSink))
+        qCDebug(qLcMediaVideoOutput) << "link subtitle stream failed";
 }
 
 void QGstreamerVideoOutput::setIsPreview()
@@ -157,6 +164,17 @@ void QGstreamerVideoOutput::setIsPreview()
     videoQueue.set("max-size-buffers", 1);
     videoQueue.set("max-size-bytes", 0);
     videoQueue.set("max-size-time", 0);
+}
+
+void QGstreamerVideoOutput::flushSubtitles()
+{
+    if (!subtitleSink.isNull()) {
+        auto pad = subtitleSink.staticPad("sink");
+        auto *event = gst_event_new_flush_start();
+        pad.sendEvent(event);
+        event = gst_event_new_flush_stop(false);
+        pad.sendEvent(event);
+    }
 }
 
 QT_END_NAMESPACE

src/multimedia/platform/gstreamer/common/qgstreamervideosink.cpp
@@ -39,6 +39,7 @@
 
 #include "qgstreamervideosink_p.h"
 #include "private/qgstvideorenderersink_p.h"
+#include "private/qgstsubtitlesink_p.h"
 #include <private/qgstutils_p.h>
 #include <QtGui/private/qrhi_p.h>
 
@@ -91,6 +92,8 @@ QGstreamerVideoSink::QGstreamerVideoSink(QVideoSink *parent)
     sinkBin.add(gstQueue, gstPreprocess);
     gstQueue.link(gstPreprocess);
     sinkBin.addGhostPad(gstQueue, "sink");
+
+    gstSubtitleSink = GST_ELEMENT(QGstSubtitleSink::createSink(this));
 }
 
 QGstreamerVideoSink::~QGstreamerVideoSink()

commit 6e96999bb1f61ec4bfdf93ad4438bfdcfdbd9ad7

Don't try to flush a live pipeline
When using a pipeline in a capture session where the input source
are live, we can't do a seeking flush.

Fixes a regression introduced in 8d5182d03c1967d77ca8da531b004e85bb0892f0

Pick-to: 6.2 6.2.0
Change-Id: Id3e3d3ff3b0ac996ea6065f24ade0a0920f2b44c
Reviewed-by: Piotr Srebrny <piotr.srebrny@qt.io>
Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>

src/multimedia/platform/gstreamer/common/qgstpipeline.cpp
@@ -307,7 +307,8 @@ void QGstPipeline::endConfig()
     if (d->m_configCounter)
         return;
 
-    d->m_pendingFlush = true;
+    if (d->m_flushOnConfigChanges)
+        d->m_pendingFlush = true;
     if (d->m_savedState == GST_STATE_PLAYING)
         setState(GST_STATE_PLAYING);
     d->m_savedState = GST_STATE_NULL;