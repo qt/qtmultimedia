qt/qtmultimedia

commit 7a177613086393e001b00e33002b3f914f942da1

Fix QSoundEffect::play()
play() is documented to restart the sound effect when it is
already playing. So enforce that behavior, by calling stop()
unconditionally and then restart the playback. But also make
sure we don't emit the playingChanged() signal in this case.

Task-number: QTBUG-96202
Pick-to: 6.2
Change-Id: Ie0006a50d2d05d12713883fa9a1410ef4c40c817
Reviewed-by: Piotr Srebrny <piotr.srebrny@qt.io>
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/multimedia/audio/qsoundeffect.cpp
@@ -214,15 +214,18 @@ void QSoundEffectPrivate::setStatus(QSoundEffect::Status status)
 void QSoundEffectPrivate::setPlaying(bool playing)
 {
     qCDebug(qLcSoundEffect) << this << "setPlaying(" << playing << ")" << m_playing;
+    if (m_audioOutput) {
+        m_audioOutput->stop();
+        if (playing) {
+            if (!m_sampleReady)
+                return;
+            m_audioOutput->start(this);
+        }
+    }
+
     if (m_playing == playing)
         return;
     m_playing = playing;
-    if (m_audioOutput) {
-        if (!m_playing)
-            m_audioOutput->stop();
-        else if (m_audioOutput->state() == QAudio::StoppedState && m_sampleReady)
-            m_audioOutput->start(this);
-    }
 
     emit q_ptr->playingChanged();
 }

commit 432986bf3db9c530c6cbdb991a7376d122004a53

Fix regression in QSoundEffect::play() on Android
Commit 7a177613086393e001b00e33002b3f914f942da1 broke the
QSoundEffect::play() on Android.

The QSoundEffect::playing must be true before the output is started,
because otherwise the QSoundEffectPrivate::readData returns 0 and
drains the output. This can cause onEOSEvent before the playing is
set to true. At the time ::setPlaying finally sets playing to true,
the audio output has already drained and the playback has stopped.

Fix the issue by setting QSoundEffect::playing true before starting
the output.

Task-number: QTBUG-96202
Pick-to: 6.2
Change-Id: I22d763052f36682f46134c3f62bf404f19e79673
Reviewed-by: Piotr Srebrny <piotr.srebrny@qt.io>
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/multimedia/audio/qsoundeffect.cpp
@@ -216,16 +216,16 @@ void QSoundEffectPrivate::setPlaying(bool playing)
     qCDebug(qLcSoundEffect) << this << "setPlaying(" << playing << ")" << m_playing;
     if (m_audioOutput) {
         m_audioOutput->stop();
-        if (playing) {
-            if (!m_sampleReady)
-                return;
-            m_audioOutput->start(this);
-        }
+        if (playing && !m_sampleReady)
+            return;
     }
 
     if (m_playing == playing)
         return;
     m_playing = playing;
+
+    if (m_audioOutput && playing)
+        m_audioOutput->start(this);
 
     emit q_ptr->playingChanged();
 }