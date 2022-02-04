21996f927ed87d25d1988a9e94b24985adff5786
Timur Pocheptsov <timur.pocheptsov@qt.io>
2022-02-04 11:40:53 +0100

    AVFoundation plugin: protect the use of codec constant
    It's not defined in older SDKs, so fails to build on macOS 10.15.
    
    Change-Id: I9518db080660d15eb0ef8af0c509e1062ada7113
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>
    (cherry picked from commit f1babcc79a1693e134b9b12df3b4a5f922d42f89)

--- src/multimedia/platform/darwin/qdarwinintegration.mm
+++ src/multimedia/platform/darwin/qdarwinintegration.mm
@@ -54,7 +54,7 @@ QT_BEGIN_NAMESPACE
 
 QDarwinIntegration::QDarwinIntegration()
 {
-#ifdef Q_OS_MACOS
+#if defined(Q_OS_MACOS) && QT_MACOS_PLATFORM_SDK_EQUAL_OR_ABOVE(__MAC_11_0)
     if (__builtin_available(macOS 11.0, *))
         VTRegisterSupplementalVideoDecoderIfAvailable(kCMVideoCodecType_VP9);
 #endif
