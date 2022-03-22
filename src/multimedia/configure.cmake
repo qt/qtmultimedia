149c17c25a6bb273c1693507ad0eb3442bdc5fa9
Rafael Roquetto <rafael.roquetto@qt.io>
2022-03-22 14:55:28 +1000

    Enable ffmpeg build for QNX
    Change-Id: I511fe8d12c0321c0752432803e7e3df071124a5f
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- src/multimedia/configure.cmake
+++ src/multimedia/configure.cmake
@@ -73,7 +73,7 @@ qt_feature("ffmpeg" PRIVATE
     LABEL "FFmpeg"
     ENABLE INPUT_ffmpeg STREQUAL 'yes'
     DISABLE INPUT_ffmpeg STREQUAL 'no'
-    CONDITION FFmpeg_FOUND AND (APPLE OR WIN32 OR QT_FEATURE_pulseaudio)
+    CONDITION FFmpeg_FOUND AND (APPLE OR WIN32 OR QNX OR QT_FEATURE_pulseaudio)
 )
 qt_feature("alsa" PUBLIC PRIVATE
     LABEL "ALSA"
