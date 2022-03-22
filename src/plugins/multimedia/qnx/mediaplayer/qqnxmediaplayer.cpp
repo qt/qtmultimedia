e2a4d131087646d28f8d9eb91f23a369e24c95de
Rafael Roquetto <rafael.roquetto@qt.io>
2022-03-22 14:30:57 +1000

    QQnxMediaPlayer: fix default sound device
    According to the QNX documentation, "snd:" is the string to be used to
    refer to the preferred audio device.
    
    Change-Id: Icc8609a9e7fd7d46847b63adbf5b963ccb2298f0
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- src/plugins/multimedia/qnx/mediaplayer/qqnxmediaplayer.cpp
+++ src/plugins/multimedia/qnx/mediaplayer/qqnxmediaplayer.cpp
@@ -226,7 +226,7 @@ void QQnxMediaPlayer::attach()
         m_videoRenderer->attachDisplay(m_context);
 
     const QByteArray defaultAudioDevice = qgetenv("QQNX_RENDERER_DEFAULT_AUDIO_SINK");
-    m_audioId = mmr_output_attach(m_context, defaultAudioDevice.isEmpty() ? "audio:default" : defaultAudioDevice.constData(), "audio");
+    m_audioId = mmr_output_attach(m_context, defaultAudioDevice.isEmpty() ? "snd:" : defaultAudioDevice.constData(), "audio");
     if (m_audioId == -1) {
         emitMmError("mmr_output_attach() for audio failed");
         return;
