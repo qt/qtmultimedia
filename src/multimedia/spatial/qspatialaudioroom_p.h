2d475bc61c68afa2fea3f156d20eb337ed9feb58
Moody Liu <mooodyhunter@outlook.com>
2022-05-12 10:25:21 +0100

    Add missing "We mean it" disclaimer
    Change-Id: I117649de26664b02536309ed6fab838c027676c1
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- src/multimedia/spatial/qspatialaudioroom_p.h
+++ src/multimedia/spatial/qspatialaudioroom_p.h
@@ -37,6 +37,16 @@
 #ifndef QSPATIALAUDIOROOM_P_H
 #define QSPATIALAUDIOROOM_P_H
 
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <qspatialaudioroom.h>
 #include <qspatialaudioengine_p.h>
 #include <QtGui/qquaternion.h>
