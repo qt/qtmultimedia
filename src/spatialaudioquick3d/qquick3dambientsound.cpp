399a538ff7601022e716e17fe2bd70fe0b256f48
Paul Olav Tvete <paul.tvete@qt.io>
2022-06-30 14:23:41 +0200

    Support sound files in qrc
    This is required for QML examples that include audio files.
    
    This has two parts. One is letting relative urls map to qrc:// for
    spatial audio when the QML file is in qrc. The other is to support
    qrc:// URLs in QAudioEngine.
    
    Pick-to: 6.4
    Change-Id: Ide3fdee1503eb6782029306bfa4399946f27b130
    Reviewed-by: Lars Knoll <lars.knoll@gmail.com>

--- src/spatialaudioquick3d/qquick3dambientsound.cpp
+++ src/spatialaudioquick3d/qquick3dambientsound.cpp
@@ -5,6 +5,8 @@
 #include "qambientsound.h"
 #include <QAudioFormat>
 #include <qdir.h>
+#include <QQmlContext>
+#include <QQmlFile>
 
 QT_BEGIN_NAMESPACE
 
@@ -47,9 +49,14 @@ QUrl QQuick3DAmbientSound::source() const
 
 void QQuick3DAmbientSound::setSource(QUrl source)
 {
-    QUrl url = QUrl::fromLocalFile(QDir::currentPath() + u"/");
-    url = url.resolved(source);
-
+    const QQmlContext *context = qmlContext(this);
+    QUrl url;
+    if (context) {
+        url = context->resolvedUrl(source);
+    } else {
+        url = QUrl::fromLocalFile(QDir::currentPath() + u"/");
+        url = url.resolved(source);
+    }
     m_sound->setSource(url);
 }
 
