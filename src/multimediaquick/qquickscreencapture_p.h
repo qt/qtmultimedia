dd2ba6ebfe09a515be9c5783f6ea4f86d42f5246
Piotr Srebrny <piotr.srebrny@qt.io>
2022-07-04 18:16:45 +0200

    Add support for QML screens that are wrapped in QQuickScreenInfo object
    Setting screen via setScreen(QScreen *) does not work for QML as
    QML screen has QQuickScreenInfo type. This patch overrides
    QScreenCapture screen property with the proper QML screen type.
    
    Change-Id: I85ba134c2d306d92f35dd8d52ed6040b8b9b2817
    Reviewed-by: Lars Knoll <lars.knoll@gmail.com>

---
+++ src/multimediaquick/qquickscreencapture_p.h
@@ -0,0 +1,52 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QQUICKSCREENCAPTURE_H
+#define QQUICKSCREENCAPTURE_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of other Qt classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QScreenCapture>
+#include <qtmultimediaquickexports.h>
+#include <private/qglobal_p.h>
+#include <private/qquickscreen_p.h>
+
+QT_BEGIN_NAMESPACE
+
+class Q_MULTIMEDIAQUICK_EXPORT QQuickScreenCatpure : public QScreenCapture
+{
+    Q_OBJECT
+    Q_PROPERTY(QQuickScreenInfo *screen READ qmlScreen WRITE qmlSetScreen NOTIFY screenChanged)
+    QML_NAMED_ELEMENT(ScreenCapture)
+
+public:
+    QQuickScreenCatpure(QObject *parent = nullptr) : QScreenCapture(parent)
+    {
+        connect(this, &QScreenCapture::screenChanged, [&](QScreen *screen) {
+            emit QQuickScreenCatpure::screenChanged(new QQuickScreenInfo(this, screen));
+        });
+    }
+
+    void qmlSetScreen(const QQuickScreenInfo *info)
+    {
+        setScreen(info ? info->wrappedScreen() : nullptr);
+    }
+
+    QQuickScreenInfo *qmlScreen() { return new QQuickScreenInfo(this, screen()); }
+
+Q_SIGNALS:
+    void screenChanged(QQuickScreenInfo *);
+};
+
+QT_END_NAMESPACE
+
+#endif
--- src/multimediaquick/qtmultimediaquicktypes_p.h
+++ src/multimediaquick/qtmultimediaquicktypes_p.h
@@ -126,13 +126,6 @@ struct QCameraFormatForeign
     QML_NAMED_ELEMENT(cameraFormat)
 };
 
-struct QScreenCaptureForeign
-{
-    Q_GADGET
-    QML_FOREIGN(QScreenCapture)
-    QML_NAMED_ELEMENT(ScreenCapture)
-};
-
 QT_END_NAMESPACE
 
 #endif
