f2af3a1eff4921f0a8b0074de7207a7c278c91a3
Lucie Gérard <lucie.gerard@qt.io>
2022-07-05 14:50:28 +0200

    Add license headers to cmake files
    CMakeLists.txt and .cmake files of significant size
    (more than 2 lines according to our check in tst_license.pl)
    now have the copyright and license header.
    
    Existing copyright statements remain intact
    
    Task-number: QTBUG-88621
    Change-Id: If5c61e7ebea0452847d411ea230438095111e030
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>

--- cmake/FindFFmpeg.cmake
+++ cmake/FindFFmpeg.cmake
@@ -1,3 +1,5 @@
+# Copyright (C) 2022 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 #.rst:
 # FindFFmpeg
 # ----------
--- cmake/FindMMRendererCore.cmake
+++ cmake/FindMMRendererCore.cmake
@@ -1,3 +1,6 @@
+# Copyright (C) 2022 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
 find_library(MMRendererCore_LIBRARY NAMES mmrndcore)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MMRendererCore DEFAULT_MSG MMRendererCore_LIBRARY)
