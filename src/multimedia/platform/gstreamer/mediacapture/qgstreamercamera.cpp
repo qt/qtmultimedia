f7328dfff9f3a661dcdbbcad50117e04ff4ef661
Piotr Srebrny <piotr.srebrny@qt.io>
2022-10-12 17:03:46 +0200

    Fix memleak
    Change-Id: Id974afa69e4423163c5c96ba812aa7ca017474fc
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>
    (cherry picked from commit 8d0e5dfe93536a682849cb49fa2276ceba58ca60)

--- src/multimedia/platform/gstreamer/mediacapture/qgstreamercamera.cpp
+++ src/multimedia/platform/gstreamer/mediacapture/qgstreamercamera.cpp
@@ -107,9 +107,11 @@ void QGstreamerCamera::setCamera(const QCameraDevice &camera)
         auto *devices = static_cast<QGstreamerMediaDevices *>(QGstreamerIntegration::instance()->devices());
         auto *device = devices->videoDevice(camera.id());
         gstNewCamera = gst_device_create_element(device, "camerasrc");
-        QGstStructure properties = gst_device_get_properties(device);
-        if (properties.name() == "v4l2deviceprovider")
-            m_v4l2Device = QString::fromUtf8(properties["device.path"].toString());
+        if (QGstStructure properties = gst_device_get_properties(device); !properties.isNull()) {
+            if (properties.name() == "v4l2deviceprovider")
+                m_v4l2Device = QString::fromUtf8(properties["device.path"].toString());
+            properties.free();
+        }
     }
 
     QCameraFormat f = findBestCameraFormat(camera);
