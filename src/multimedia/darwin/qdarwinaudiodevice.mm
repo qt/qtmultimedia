23e4f614b3ab22300b2ce58c50b1e9a6d4ab5011
Artem Dyomin <artem.dyomin@qt.io>
2022-12-28 17:16:56 +0100

    Improve macos audio data warnings
    Functions AudioObjectGetPropertyDataSize and AudioObjectGetPropertyData
    have been wrapped into util functions that can print warnings with
    info that might help us.
    
    Task-number: QTBUG-108176
    Pick-to: 6.5 6.4
    Change-Id: I787d5a93ad395d2156df6b542c290949c61daaa3
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Doris Verria <doris.verria@qt.io>

--- src/multimedia/darwin/qdarwinaudiodevice.mm
+++ src/multimedia/darwin/qdarwinaudiodevice.mm
@@ -5,8 +5,10 @@
 #include "qcoreaudioutils_p.h"
 #include <private/qcore_mac_p.h>
 
-#if defined(Q_OS_IOS) || defined(Q_OS_TVOS)
-# include "qcoreaudiosessionmanager_p.h"
+#if defined(Q_OS_IOS)
+#include "qcoreaudiosessionmanager_p.h"
+#else
+#include "qmacosaudiodatautils_p.h"
 #endif
 
 #include <QtCore/QDataStream>
@@ -36,50 +38,29 @@ QCoreAudioDeviceInfo::QCoreAudioDeviceInfo(const QByteArray &device, QAudioDevic
 
 }
 
-
 QAudioFormat QCoreAudioDeviceInfo::determinePreferredFormat() const
 {
     QAudioFormat format;
 
 #if defined(Q_OS_MACOS)
-    UInt32  propSize = 0;
-    AudioObjectPropertyScope audioDevicePropertyScope = mode == QAudioDevice::Input ? kAudioDevicePropertyScopeInput : kAudioDevicePropertyScopeOutput;
-    AudioObjectPropertyAddress audioDevicePropertyStreamsAddress = { kAudioDevicePropertyStreams,
-                                                                     audioDevicePropertyScope,
-                                                                     kAudioObjectPropertyElementMaster };
+    const auto audioDevicePropertyStreamsAddress =
+            makePropertyAddress(kAudioDevicePropertyStreams, mode);
 
-    if (AudioObjectGetPropertyDataSize(m_deviceId, &audioDevicePropertyStreamsAddress, 0, NULL, &propSize) == noErr) {
+    if (auto streamIDs = getAudioData<AudioStreamID>(m_deviceId, audioDevicePropertyStreamsAddress,
+                                                     "propertyStreams")) {
+        const auto audioDevicePhysicalFormatPropertyAddress =
+                makePropertyAddress(kAudioStreamPropertyPhysicalFormat, mode);
 
-        const int sc = propSize / sizeof(AudioStreamID);
-
-        if (sc > 0) {
-            AudioStreamID*  streams = new AudioStreamID[sc];
-
-            if (AudioObjectGetPropertyData(m_deviceId, &audioDevicePropertyStreamsAddress, 0, NULL, &propSize, streams) == noErr) {
-
-                AudioObjectPropertyAddress audioDevicePhysicalFormatPropertyAddress = { kAudioStreamPropertyPhysicalFormat,
-                                                                                        kAudioObjectPropertyScopeGlobal,
-                                                                                        kAudioObjectPropertyElementMaster };
-
-                for (int i = 0; i < sc; ++i) {
-                    if (AudioObjectGetPropertyDataSize(streams[i], &audioDevicePhysicalFormatPropertyAddress, 0, NULL, &propSize) == noErr) {
-                        AudioStreamBasicDescription sf;
-
-                        if (AudioObjectGetPropertyData(streams[i], &audioDevicePhysicalFormatPropertyAddress, 0, NULL, &propSize, &sf) == noErr) {
-                            format = CoreAudioUtils::toQAudioFormat(sf);
-                            break;
-                        } else {
-                            qWarning() << "QAudioDevice: Unable to find perferedFormat for stream";
-                        }
-                    } else {
-                        qWarning() << "QAudioDevice: Unable to find size of perferedFormat for stream";
-                    }
-                }
+        for (auto streamID : *streamIDs) {
+            if (auto streamDescription = getAudioObject<AudioStreamBasicDescription>(
+                        streamID, audioDevicePhysicalFormatPropertyAddress,
+                        "prefferedPhysicalFormat")) {
+                format = CoreAudioUtils::toQAudioFormat(*streamDescription);
+                break;
             }
-
-            delete[] streams;
         }
     }
+
     if (!format.isValid())
 #endif
     {
@@ -96,22 +77,14 @@ QAudioFormat QCoreAudioDeviceInfo::determinePreferredFormat() const
 QString QCoreAudioDeviceInfo::getDescription() const
 {
 #ifdef Q_OS_MACOS
-    CFStringRef name;
-    UInt32 size = sizeof(CFStringRef);
-    AudioObjectPropertyScope audioPropertyScope = mode == QAudioDevice::Input ? kAudioDevicePropertyScopeInput : kAudioDevicePropertyScopeOutput;
-
-    AudioObjectPropertyAddress audioDeviceNamePropertyAddress = { kAudioObjectPropertyName,
-                                                                  audioPropertyScope,
-                                                                  kAudioObjectPropertyElementMaster };
-
-    if (AudioObjectGetPropertyData(m_deviceId, &audioDeviceNamePropertyAddress, 0, NULL, &size, &name) != noErr) {
-        qWarning() << "QAudioDevice: Unable to find device description";
-        return QString();
+    const auto propertyAddress = makePropertyAddress(kAudioObjectPropertyName, mode);
+    if (auto name =
+                getAudioObject<CFStringRef>(m_deviceId, propertyAddress, "Device Description")) {
+        auto deleter = qScopeGuard([&name]() { CFRelease(*name); });
+        return QString::fromCFString(*name);
     }
 
-    QString s = QString::fromCFString(name);
-    CFRelease(name);
-    return s;
+    return {};
 #else
     return QString::fromUtf8(id);
 #endif
@@ -120,16 +93,12 @@ QString QCoreAudioDeviceInfo::getDescription() const
 void QCoreAudioDeviceInfo::getChannelLayout()
 {
 #ifdef Q_OS_MACOS
-    AudioObjectPropertyAddress audioDeviceChannelLayoutPropertyAddress = { kAudioDevicePropertyPreferredChannelLayout,
-                                                                    (mode == QAudioDevice::Input ? kAudioDevicePropertyScopeInput : kAudioDevicePropertyScopeOutput),
-                                                                    kAudioObjectPropertyElementMaster };
-    UInt32 propSize;
-    if (AudioObjectGetPropertyDataSize(m_deviceId, &audioDeviceChannelLayoutPropertyAddress, 0, nullptr, &propSize) == noErr) {
-        AudioChannelLayout *layout = static_cast<AudioChannelLayout *>(malloc(propSize));
-        if (AudioObjectGetPropertyData(m_deviceId, &audioDeviceChannelLayoutPropertyAddress, 0, nullptr, &propSize, layout) == noErr) {
-            channelConfiguration = CoreAudioUtils::fromAudioChannelLayout(layout);
-        }
-        free(layout);
+    const auto propertyAddress =
+            makePropertyAddress(kAudioDevicePropertyPreferredChannelLayout, mode);
+    if (auto data = getAudioData<char>(m_deviceId, propertyAddress, "prefferedChannelLayout",
+                                       sizeof(AudioChannelLayout))) {
+        const auto *layout = reinterpret_cast<const AudioChannelLayout *>(data->data());
+        channelConfiguration = CoreAudioUtils::fromAudioChannelLayout(layout);
     }
 #else
     channelConfiguration = (mode == QAudioDevice::Input) ? QAudioFormat::ChannelConfigMono : QAudioFormat::ChannelConfigStereo;
