0c471c47cc52c4f98f11a84089ba75cfe4be7808
Tasuku Suzuki <tasuku.suzuki@signal-slot.co.jp>
2023-01-05 00:48:21 -0800

    Fix include guard name
    Change-Id: Ifae5ba32172379ec220ed00a7779b549cc4b458f
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>

--- src/plugins/multimedia/windows/player/mftvideo_p.h
+++ src/plugins/multimedia/windows/player/mftvideo_p.h
@@ -1,8 +1,8 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#ifndef MFTRANSFORM_H
-#define MFTRANSFORM_H
+#ifndef MFTVIDEO_H
+#define MFTVIDEO_H
 
 //
 //  W A R N I N G
