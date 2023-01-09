749d666088da2bbf4c40fe8c0f01d77476a9d98a
Samuel Mira <samuel.mira@qt.io>
2023-01-09 11:58:33 +0200

    Android: Prevent Camera set automatically active
    The Android backend was different from the rest of the backends and it
    automatically started the camera when it was attached to media capture
    session. This patch changes to set it as the rest of the backends.
    
    Fixes: QTBUG-109415
    Change-Id: I88342e9db068bec05404f5bfd91f73ea5015bb96
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>
    (cherry picked from commit f18be9c59cb38fb3cc5046932c5587997af35cf5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/multimedia/android/mediacapture/qandroidcamera.cpp
+++ src/plugins/multimedia/android/mediacapture/qandroidcamera.cpp
@@ -24,8 +24,11 @@ QAndroidCamera::~QAndroidCamera()
 
 void QAndroidCamera::setActive(bool active)
 {
-    if (m_cameraSession)
+    if (m_cameraSession) {
         m_cameraSession->setActive(active);
+    } else {
+        isPendingSetActive = active;
+    }
 }
 
 bool QAndroidCamera::isActive() const
@@ -99,6 +102,11 @@ void QAndroidCamera::setCaptureSession(QPlatformMediaCaptureSession *session)
     connect(m_cameraSession, &QAndroidCameraSession::activeChanged, this, &QAndroidCamera::activeChanged);
     connect(m_cameraSession, &QAndroidCameraSession::error, this, &QAndroidCamera::error);
     connect(m_cameraSession, &QAndroidCameraSession::opened, this, &QAndroidCamera::onCameraOpened);
+
+    if (isPendingSetActive) {
+        setActive(true);
+        isPendingSetActive = false;
+    }
 }
 
 void QAndroidCamera::setFocusMode(QCamera::FocusMode mode)
