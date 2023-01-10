99ba02125ed1717814294b2b3f566b6c5f7e57d8
Tasuku Suzuki <tasuku.suzuki@signal-slot.co.jp>
2023-01-10 20:51:25 +0900

    Fix an include guard
    That was replaced with it in camel case when renaming the class
    
    Change-Id: I051472500bc1cc8ec0846b1995b84d9608cf403c
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>

--- src/plugins/multimedia/windows/mediacapture/qwindowsimagecapture_p.h
+++ src/plugins/multimedia/windows/mediacapture/qwindowsimagecapture_p.h
@@ -1,8 +1,8 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#ifndef QWindowsImageCapture_H
-#define QWindowsImageCapture_H
+#ifndef QWINDOWSIMAGECAPTURE_H
+#define QWINDOWSIMAGECAPTURE_H
 
 //
 //  W A R N I N G
@@ -61,4 +61,4 @@ private:
 
 QT_END_NAMESPACE
 
-#endif  // QWindowsImageCapture_H
+#endif  // QWINDOWSIMAGECAPTURE_H
