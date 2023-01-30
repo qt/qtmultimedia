qt/qtmultimedia

commit bd2e80867e4e04fff277cf7340747630d2ea55a4

Allow sinks that do not have associated QRhi to receive video frames
AndroidSurfaceTexture was created only when QVideoSink has an associated
QRhi. This enables QVideoSink to display textures created by camera or
media player as the textures are created in the OpenGL context
associated with QRhi. However, we can allow QVideoSink without QRhi to
receive frames as the context sharing can be established later or
frames are processed as QImage.

Fixes: QTBUG-108027
Pick-to: 6.4
Change-Id: Iac8925da43aa100fe03c09f57f3d254c27add49c
Reviewed-by: Samuel Mira <samuel.mira@qt.io>
Reviewed-by: Lars Knoll <lars@knoll.priv.no>

src/plugins/multimedia/android/common/qandroidvideooutput.cpp
@@ -291,10 +291,10 @@ public slots:
         if (m_surfaceTexture)
             return m_surfaceTexture.get();
 
-        if (!rhi)
-            return nullptr;
-
-        initRhi(static_cast<const QRhiGles2NativeHandles *>(rhi->nativeHandles())->context);
+        QOpenGLContext *ctx = rhi
+                ? static_cast<const QRhiGles2NativeHandles *>(rhi->nativeHandles())->context
+                : nullptr;
+        initRhi(ctx);
 
         m_texture.reset(m_rhi->newTexture(QRhiTexture::RGBA8, m_size, 1, QRhiTexture::ExternalOES));
         m_texture->create();
@@ -352,7 +352,7 @@ void QAndroidTextureVideoOutput::setSubtitle(const QString &subtitle)
 
 AndroidSurfaceTexture *QAndroidTextureVideoOutput::surfaceTexture()
 {
-    if (!m_sink || !m_sink->rhi())
+    if (!m_sink)
         return nullptr;
 
     AndroidSurfaceTexture *surface = nullptr;

commit f29bf9e033add62da509708e72bfb6359f1e3602

Android-Backend: update surfaceTexture with Rhi if needed
Regression appeared after bd2e80867e4e04fff277cf7340747630d2ea55a4
commit. Before this change, qandroidvideooutput did not create
AndroidSurfaceTexture without QRhi. That's why creation was postopned
and allows qandroidmediaplayer to try create surfaceTexture again during
the Media setting.

From one hand we need to allow to create surfaceTexture without QRhi.
From the other hand we need to allow to recreate surfaceTexture if
QVideoSink has an associated QRhi.

To handle mentioned case shouldTextureBeUpdated() method was added to
QAndroidTextureVideoOutput.

Pick-to: 6.5 6.4
Task-number: QTBUG-109731
Change-Id: Ic845647ab2a9823d02cfc7edb9c735499c3e563b
Reviewed-by: Samuel Mira <samuel.mira@qt.io>
Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
Reviewed-by: Lars Knoll <lars@knoll.priv.no>
Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>

src/plugins/multimedia/android/common/qandroidvideooutput.cpp
@@ -362,14 +362,28 @@ void QAndroidTextureVideoOutput::setSubtitle(const QString &subtitle)
     }
 }
 
+bool QAndroidTextureVideoOutput::shouldTextureBeUpdated() const
+{
+    return m_sink->rhi() && m_surfaceCreatedWithoutRhi;
+}
+
 AndroidSurfaceTexture *QAndroidTextureVideoOutput::surfaceTexture()
 {
     if (!m_sink)
         return nullptr;
 
     AndroidSurfaceTexture *surface = nullptr;
-    QMetaObject::invokeMethod(m_surfaceThread.get(),
-            [&](){ surface = m_surfaceThread->createSurfaceTexture(m_sink->rhi()); },
+    QMetaObject::invokeMethod(m_surfaceThread.get(), [&]() {
+                auto rhi = m_sink->rhi();
+                if (!rhi) {
+                    m_surfaceCreatedWithoutRhi = true;
+                }
+                else if (m_surfaceCreatedWithoutRhi) {
+                    m_surfaceThread->clearSurfaceTexture();
+                    m_surfaceCreatedWithoutRhi = false;
+                }
+                surface = m_surfaceThread->createSurfaceTexture(rhi);
+            },
             Qt::BlockingQueuedConnection);
     return surface;
 }

src/plugins/multimedia/android/common/qandroidvideooutput_p.h
@@ -60,6 +60,7 @@ public:
     ~QAndroidTextureVideoOutput() override;
 
     QVideoSink *surface() const { return m_sink; }
+    bool shouldTextureBeUpdated() const;
 
     AndroidSurfaceTexture *surfaceTexture() override;
 
@@ -74,6 +75,7 @@ private Q_SLOTS:
 private:
     QVideoSink *m_sink = nullptr;
     QSize m_nativeSize;
+    bool m_surfaceCreatedWithoutRhi = false;
 
     std::unique_ptr<class AndroidTextureThread> m_surfaceThread;
 };

src/plugins/multimedia/android/mediaplayer/qandroidmediaplayer.cpp
@@ -272,7 +272,8 @@ void QAndroidMediaPlayer::setMedia(const QUrl &mediaContent,
         if (mVideoSize.isValid() && mVideoOutput)
             mVideoOutput->setVideoSize(mVideoSize);
 
-        if ((mMediaPlayer->display() == 0) && mVideoOutput)
+        if (mVideoOutput &&
+                        (mMediaPlayer->display() == 0 || mVideoOutput->shouldTextureBeUpdated()))
             mMediaPlayer->setDisplay(mVideoOutput->surfaceTexture());
         mMediaPlayer->setDataSource(QNetworkRequest(mediaContent));
         mMediaPlayer->prepareAsync();