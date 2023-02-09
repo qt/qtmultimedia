b094c2dd7a5ebff91113a93000786588773d83ab
Artem Dyomin <artem.dyomin@qt.io>
2023-02-09 21:42:15 +0100

    Implement custom X11 screen capturing
    The implementation works on linux with run X11. It's disabled with wayland.
    
    Achivements
    
    - Fixed problem with performance.
      The standard implementation affected CPU too much.
      On my local release build the speedup
      in capturing of a single frame is 4-5 times.
      The explanation of such a huge difference is that opening of capturing
      contexts on X11 is an expensive action, whereas implemented single
      context allocation and transferring images through
      shared memory (Xshm) is pretty fast.
      Also, QScreen::grabWindow permorms additional pixels processings
      under the hood what is not needed for videoframes.
    - Improved errors handling
    - The patch allows to run screen capture backend UTs on linux CI
      with X11, namely, ubuntu, opensuse. Test results:
      https://testresults.qt.io/coin/integration/qt/qtmultimedia/tasks/web_qt_qtmultimedia_1675977558004
    - Probably, fixed problem with windows overlaping, the matching
      tests became pass, it should be checked on different platforms.
      Also, the approach cannot capture windows
      out of the screen borders, we throw an error signal.
      Both windows issues are to be investigated deeper in 6.6.
    
    With the next commits I might move some common threading code for
    SC to a separate class.
    
    Task-number: QTBUG-103226
    Change-Id: Ibd3600fdbc9714f14dc80c2a330aea5e0d064791
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>
    (cherry picked from commit fc079734d34747d64fa26645bda36699b26c047e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/plugins/multimedia/ffmpeg/qx11screencapture_p.h
@@ -0,0 +1,44 @@
+// Copyright (C) 2016 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef X11SCREENCAPTURE_P_H
+#define X11SCREENCAPTURE_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include "qffmpegscreencapturebase_p.h"
+
+QT_BEGIN_NAMESPACE
+
+class QX11ScreenCapture : public QFFmpegScreenCaptureBase
+{
+    class Grabber;
+
+public:
+    explicit QX11ScreenCapture(QScreenCapture *screenCapture);
+    ~QX11ScreenCapture() override;
+
+    QVideoFrameFormat format() const override;
+
+    static bool isSupported();
+
+protected:
+    bool setActiveInternal(bool active) override;
+
+private:
+    std::optional<QVideoFrameFormat> m_format;
+    std::unique_ptr<Grabber> m_grabber;
+};
+
+QT_END_NAMESPACE
+
+#endif // X11SCREENCAPTURE_P_H
