e988214e3e3f0e9f9005b8be1266a60255eef011
Pavel Dubsky <pavel.dubsky@qt.io>
2023-05-31 12:28:02 +0200

    Replace calls to CoTaskMemFree via RAII wrapper
    This change replaces all uses of task memory object that should be
    freed with CoTaskMemFree (typically COM-related) with a wrapper class
    that handles all memory deallocation and provides pointer semantic.
    
    Pick-to: 6.6
    Change-Id: I73fe53eaa8ca37eb2ac6c6c05142e10e5a5615e9
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>

--- src/multimedia/windows/qcomtaskresource_p.h
+++ src/multimedia/windows/qcomtaskresource_p.h
@@ -18,30 +18,47 @@
 #include <QtCore/qassert.h>
 
 #include <objbase.h>
+#include <algorithm>
+#include <type_traits>
 #include <utility>
 
-template<typename T>
-class QComTaskResource final
+class QEmptyDeleter final
 {
 public:
-    QComTaskResource() = default;
-    explicit QComTaskResource(T *resource) : m_resource(resource) { }
-    ~QComTaskResource() { reset(); }
+    template<typename T>
+    void operator()(T /*element*/) const
+    {
+    }
+};
 
-    QComTaskResource(const QComTaskResource<T> &source) = delete;
-    QComTaskResource &operator=(const QComTaskResource<T> &right) = delete;
+class QComDeleter final
+{
+public:
+    template<typename T>
+    void operator()(T element) const
+    {
+        element->Release();
+    }
+};
+
+template<typename T>
+class QComTaskResourceBase
+{
+public:
+    QComTaskResourceBase(const QComTaskResourceBase<T> &source) = delete;
+    QComTaskResourceBase &operator=(const QComTaskResourceBase<T> &right) = delete;
 
     explicit operator bool() const { return m_resource != nullptr; }
-    T *operator->() const { return m_resource; }
 
-    T **address()
-    {
-        Q_ASSERT(m_resource == nullptr);
-        return &m_resource;
-    }
     T *get() const { return m_resource; }
+
+protected:
+    QComTaskResourceBase() = default;
+    explicit QComTaskResourceBase(T *const resource) : m_resource(resource) { }
+
     T *release() { return std::exchange(m_resource, nullptr); }
-    void reset(T *resource = nullptr)
+
+    void reset(T *const resource = nullptr)
     {
         if (m_resource != resource) {
             if (m_resource)
@@ -50,8 +67,86 @@ public:
         }
     }
 
-private:
     T *m_resource = nullptr;
+};
+
+template<typename T, typename TElementDeleter = QEmptyDeleter>
+class QComTaskResource final : public QComTaskResourceBase<T>
+{
+    using Base = QComTaskResourceBase<T>;
+
+public:
+    using Base::QComTaskResourceBase;
+
+    ~QComTaskResource() { reset(); }
+
+    T *operator->() const { return m_resource; }
+    T &operator*() const { return *m_resource; }
+
+    T **address()
+    {
+        Q_ASSERT(m_resource == nullptr);
+        return &m_resource;
+    }
+
+    using Base::release;
+    using Base::reset;
+
+private:
+    using Base::m_resource;
+};
+
+template<typename T, typename TElementDeleter>
+class QComTaskResource<T[], TElementDeleter> final : public QComTaskResourceBase<T>
+{
+    using Base = QComTaskResourceBase<T>;
+
+public:
+    QComTaskResource() = default;
+    explicit QComTaskResource(T *const resource, const std::size_t size)
+        : Base(resource), m_size(size)
+    {
+    }
+
+    ~QComTaskResource() { reset(); }
+
+    T &operator[](const std::size_t index) const
+    {
+        Q_ASSERT(index < m_size);
+        return m_resource[index];
+    }
+
+    T *release()
+    {
+        m_size = 0;
+
+        return Base::release();
+    }
+
+    void reset() { reset(nullptr, 0); }
+
+    void reset(T *const resource, const std::size_t size)
+    {
+        if (m_resource != resource) {
+            resetElements();
+
+            Base::reset(resource);
+
+            m_size = size;
+        }
+    }
+
+private:
+    void resetElements()
+    {
+        if constexpr (!std::is_same_v<TElementDeleter, QEmptyDeleter>) {
+            std::for_each(m_resource, m_resource + m_size, TElementDeleter());
+        }
+    }
+
+    std::size_t m_size = 0;
+
+    using Base::m_resource;
 };
 
 #endif
