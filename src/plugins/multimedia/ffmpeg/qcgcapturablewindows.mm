e143a93605a5c2354143a0aa5095bcf733163cac
Artem Dyomin <artem.dyomin@qt.io>
2023-06-26 17:19:53 +0200

    Implement getting captureble windows on macOS
    Change-Id: I3e6b4f372b348a502987efd1c888328485bc8637
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>
    (cherry picked from commit 0e6a68fff78a487c809557fada08a87cec8d2ff0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/multimedia/ffmpeg/qcgcapturablewindows.mm
+++ src/plugins/multimedia/ffmpeg/qcgcapturablewindows.mm
@@ -2,19 +2,47 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qcgcapturablewindows_p.h"
+#include "private/qcapturablewindow_p.h"
+#include "QtCore/private/qcore_mac_p.h"
+
+#include <AppKit/NSWindow.h>
 
 QT_BEGIN_NAMESPACE
 
 QList<QCapturableWindow> QCGCapturableWindows::windows() const
 {
-    // to be implemented
-    return {};
+    QList<QCapturableWindow> result;
+    QCFType<CFArrayRef> windowList(
+            CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID));
+
+    // Iterate through the window dictionaries
+    CFIndex count = CFArrayGetCount(windowList);
+    for (CFIndex i = 0; i < count; ++i) {
+        auto windowInfo = (CFDictionaryRef)CFArrayGetValueAtIndex(windowList, i);
+        auto windowNumber = (CFNumberRef)CFDictionaryGetValue(windowInfo, kCGWindowNumber);
+        auto windowName = (CFStringRef)CFDictionaryGetValue(windowInfo, kCGWindowName);
+
+        CGWindowID windowId = 0;
+        static_assert(sizeof(windowId) == 4,
+                      "CGWindowID size is not compatible with kCFNumberSInt32Type");
+        CFNumberGetValue(windowNumber, kCFNumberSInt32Type, &windowId);
+
+        auto windowData = std::make_unique<QCapturableWindowPrivate>();
+        windowData->id = static_cast<QCapturableWindowPrivate::Id>(windowId);
+        if (windowName)
+            windowData->description = QString::fromCFString(windowName);
+
+        result.push_back(windowData.release()->create());
+    }
+
+    return result;
 }
 
-bool QCGCapturableWindows::isWindowValid(const QCapturableWindowPrivate &) const
+bool QCGCapturableWindows::isWindowValid(const QCapturableWindowPrivate &window) const
 {
-    // to be implemented
-    return false;
+    QCFType<CFArrayRef> windowList(
+            CGWindowListCreate(kCGWindowListOptionIncludingWindow, window.id));
+    return CFArrayGetCount(windowList) > 0;
 }
 
 QT_END_NAMESPACE
