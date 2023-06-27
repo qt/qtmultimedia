qt/qtmultimedia

commit 8e7b5d5a42337036cb2dcd59a1e07489756e67da

Introduce private interface for QCapturableWindow providing
No implementations, interface only in the patch.

Pick-to: 6.6
Change-Id: I63a1fe8a6ca919311211807426a4d044056308a7
Reviewed-by: Lars Knoll <lars@knoll.priv.no>

src/plugins/multimedia/ffmpeg/qffmpegmediaintegration.cpp
@@ -22,10 +22,13 @@
 #include "qavfcamera_p.h"
 #include "qavfscreencapture_p.h"
 #include "qcgwindowcapture_p.h"
+#include "qcgcapturablewindows_p.h"
+
 #elif defined(Q_OS_WINDOWS)
 #include "qwindowscamera_p.h"
 #include "qwindowsvideodevices_p.h"
 #include "qffmpegscreencapture_dxgi_p.h"
+#include "qwincapturablewindows_p.h"
 #endif
 
 #ifdef Q_OS_ANDROID
@@ -49,6 +52,7 @@ extern "C" {
 
 #if QT_CONFIG(xlib)
 #include "qx11surfacecapture_p.h"
+#include "qx11capturablewindows_p.h"
 #endif
 
 QT_BEGIN_NAMESPACE
@@ -99,13 +103,21 @@ QFFmpegMediaIntegration::QFFmpegMediaIntegration()
 
 #if QT_CONFIG(linux_v4l)
     m_videoDevices = std::make_unique<QV4L2CameraDevices>(this);
-#endif
-#ifdef Q_OS_DARWIN
+#elif defined Q_OS_DARWIN
     m_videoDevices = std::make_unique<QAVFVideoDevices>(this);
 #elif defined(Q_OS_ANDROID)
     m_videoDevices = std::make_unique<QAndroidVideoDevices>(this);
 #elif defined(Q_OS_WINDOWS)
     m_videoDevices = std::make_unique<QWindowsVideoDevices>(this);
+#endif
+
+#if QT_CONFIG(xlib)
+    if (QX11SurfaceCapture::isSupported())
+        m_capturableWindows = std::make_unique<QX11CapturableWindows>();
+#elif defined Q_OS_DARWIN
+    m_capturableWindows = std::make_unique<QCGCapturableWindows>();
+#elif defined(Q_OS_WINDOWS)
+    m_capturableWindows = std::make_unique<QWinCapturableWindows>();
 #endif
 
     if (qEnvironmentVariableIsSet("QT_FFMPEG_DEBUG")) {

src/multimedia/CMakeLists.txt
@@ -52,6 +52,7 @@ qt_internal_add_module(Multimedia
         platform/qplatformvideodevices.cpp platform/qplatformvideodevices_p.h
         platform/qplatformvideosink.cpp platform/qplatformvideosink_p.h
         playback/qmediaplayer.cpp playback/qmediaplayer.h playback/qmediaplayer_p.h
+        platform/qplatformcapturablewindows_p.h
         qmediadevices.cpp qmediadevices.h
         qmediaenumdebug.h
         qmediaformat.cpp  qmediaformat.h

src/multimedia/platform/qplatformcapturablewindows_p.h
@@ -1,0 +1,44 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QPLATFORMCAPTURABLEWINDOWS_P_H
+#define QPLATFORMCAPTURABLEWINDOWS_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include "private/qtmultimediaglobal_p.h"
+#include "qcapturablewindow.h"
+
+#include <qlist.h>
+
+QT_BEGIN_NAMESPACE
+
+class QCapturableWindow;
+class QCapturableWindowPrivate;
+
+class QPlatformCapturableWindows
+{
+public:
+    QPlatformCapturableWindows() = default;
+
+    virtual ~QPlatformCapturableWindows() = default;
+
+    virtual QList<QCapturableWindow> windows() const { return {}; }
+
+    virtual bool isWindowValid(const QCapturableWindowPrivate &) const { return false; }
+
+    Q_DISABLE_COPY(QPlatformCapturableWindows);
+};
+
+QT_END_NAMESPACE
+
+#endif // QPLATFORMCAPTURABLEWINDOWS_P_H

src/multimedia/platform/qplatformmediaintegration.cpp
@@ -12,6 +12,7 @@
 #include <qcameradevice.h>
 #include <qloggingcategory.h>
 
+#include "qplatformcapturablewindows_p.h"
 #include "QtCore/private/qfactoryloader_p.h"
 #include "qplatformmediaplugin_p.h"
 
@@ -134,6 +135,16 @@ QMaybe<QPlatformAudioInput *> QPlatformMediaIntegration::createAudioInput(QAudio
 QMaybe<QPlatformAudioOutput *> QPlatformMediaIntegration::createAudioOutput(QAudioOutput *q)
 {
     return new QPlatformAudioOutput(q);
+}
+
+QList<QCapturableWindow> QPlatformMediaIntegration::capturableWindows()
+{
+    return m_capturableWindows ? m_capturableWindows->windows() : QList<QCapturableWindow>{};
+}
+
+bool QPlatformMediaIntegration::isCapturableWindowValid(const QCapturableWindowPrivate &window)
+{
+    return m_capturableWindows && m_capturableWindows->isWindowValid(window);
 }
 
 QPlatformMediaIntegration::QPlatformMediaIntegration() = default;

src/multimedia/platform/qplatformmediaintegration_p.h
@@ -50,6 +50,7 @@ class QPlatformAudioInput;
 class QPlatformAudioOutput;
 class QPlatformVideoDevices;
 class QCapturableWindow;
+class QPlatformCapturableWindows;
 
 class Q_MULTIMEDIA_EXPORT QPlatformMediaIntegration
 {
@@ -80,12 +81,12 @@ public:
 
     virtual QMaybe<QPlatformVideoSink *> createVideoSink(QVideoSink *) { return notAvailable; }
 
-    virtual QList<QCapturableWindow> capturableWindows() { return {}; };
-
-    bool isCapturableWindowValid(const QCapturableWindowPrivate &) { return false; }
+    QList<QCapturableWindow> capturableWindows();
+    bool isCapturableWindowValid(const QCapturableWindowPrivate &);
 
 protected:
     std::unique_ptr<QPlatformVideoDevices> m_videoDevices;
+    std::unique_ptr<QPlatformCapturableWindows> m_capturableWindows;
 };
 
 QT_END_NAMESPACE

src/multimedia/recording/qcapturablewindow_p.h
@@ -32,6 +32,8 @@ public:
     {
         return window.d.get();
     }
+
+    QCapturableWindow create() { return QCapturableWindow(this); }
 };
 
 QT_END_NAMESPACE

src/plugins/multimedia/ffmpeg/CMakeLists.txt
@@ -82,6 +82,7 @@ qt_internal_extend_target(QFFmpegMediaPlugin CONDITION APPLE
         qavfsamplebufferdelegate.mm qavfsamplebufferdelegate_p.h
         qavfscreencapture.mm qavfscreencapture_p.h
         qcgwindowcapture.mm qcgwindowcapture_p.h
+        qcgcapturablewindows.mm qcgcapturablewindows_p.h
     INCLUDE_DIRECTORIES
         ../darwin
         ../darwin/camera
@@ -102,6 +103,7 @@ qt_internal_extend_target(QFFmpegMediaPlugin CONDITION WIN32
         qwindowscamera.cpp qwindowscamera_p.h
         qffmpeghwaccel_d3d11.cpp qffmpeghwaccel_d3d11_p.h
         qffmpegscreencapture_dxgi.cpp qffmpegscreencapture_dxgi_p.h
+        qwincapturablewindows.cpp qwincapturablewindows_p.h
     INCLUDE_DIRECTORIES
         ../windows
     LIBRARIES
@@ -124,13 +126,14 @@ qt_internal_extend_target(QFFmpegMediaPlugin CONDITION QT_FEATURE_cpp_winrt
 qt_internal_extend_target(QFFmpegMediaPlugin CONDITION QT_FEATURE_xlib
     SOURCES
         qx11surfacecapture.cpp qx11surfacecapture_p.h
+        qx11capturablewindows.cpp qx11capturablewindows_p.h
     LIBRARIES
         X11
         Xrandr
         Xext
 )
 
-set_source_files_properties(qx11surfacecapture.cpp # X headers
+set_source_files_properties(qx11surfacecapture.cpp qx11capturablewindows.cpp # X headers
                             PROPERTIES SKIP_UNITY_BUILD_INCLUSION ON)
 
 qt_internal_extend_target(QFFmpegMediaPlugin CONDITION QT_FEATURE_linux_v4l

src/plugins/multimedia/ffmpeg/qcgcapturablewindows.mm
@@ -1,0 +1,20 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qcgcapturablewindows_p.h"
+
+QT_BEGIN_NAMESPACE
+
+QList<QCapturableWindow> QCGCapturableWindows::windows() const
+{
+    // to be implemented
+    return {};
+}
+
+bool QCGCapturableWindows::isWindowValid(const QCapturableWindowPrivate &) const
+{
+    // to be implemented
+    return false;
+}
+
+QT_END_NAMESPACE

commit 6f1f44f7dd84103e6fd679a2d009aa91d159898d

Android: fix not working camera
There is a regression after 8e7b5d5a42337036cb2dcd59a1e07489756e67da
commit. Camera does not work at all on Android. Everything because of
changes in QFFmpegMediaIntegration.

Android is also a linux (a mobile operating system based on a modified
Linux kernel). That is why, after mentioned commit,
QFFmpegMediaIntegration is trying to use QV4L2CameraDevices rather
than QAndroidVideoDevices.

This commit sets m_videoDevices correctly for Android again

Pick-to: 6.6 6.5
Fixes: QTBUG-114889
Change-Id: I7daabe12652febdd3dec5db9ec3309098a12738f
Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

src/plugins/multimedia/ffmpeg/qffmpegmediaintegration.cpp
@@ -101,12 +101,12 @@ QFFmpegMediaIntegration::QFFmpegMediaIntegration()
 {
     m_formatsInfo = new QFFmpegMediaFormatInfo();
 
-#if QT_CONFIG(linux_v4l)
+#if defined(Q_OS_ANDROID)
+    m_videoDevices = std::make_unique<QAndroidVideoDevices>(this);
+#elif QT_CONFIG(linux_v4l)
     m_videoDevices = std::make_unique<QV4L2CameraDevices>(this);
 #elif defined Q_OS_DARWIN
     m_videoDevices = std::make_unique<QAVFVideoDevices>(this);
-#elif defined(Q_OS_ANDROID)
-    m_videoDevices = std::make_unique<QAndroidVideoDevices>(this);
 #elif defined(Q_OS_WINDOWS)
     m_videoDevices = std::make_unique<QWindowsVideoDevices>(this);
 #endif