67d03667a2779ac108bfa3f7530048a287c88d82
Artem Dyomin <artem.dyomin@qt.io>
2023-09-12 11:28:30 +0200

    Move some logic of QAudioStateMachine to AudioStateMachineUtils
    The patch is a part of QAudioStateMachine prettification.
    
    Change-Id: I08092be2d497c5d3fba7f569e443e5139ce30ba8
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 8423c36ac7f5908f1404ad0b4c6d746857c3a2c9)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b96242a23f511bcc8ac7983ced09a2db62d1834e)

--- src/multimedia/audio/qaudiostatemachine.cpp
+++ src/multimedia/audio/qaudiostatemachine.cpp
@@ -9,54 +9,7 @@
 QT_BEGIN_NAMESPACE
 
 using Notifier = QAudioStateMachine::Notifier;
-using RawState = QAudioStateMachine::RawState;
-
-namespace {
-constexpr RawState DrainingFlag = 1 << 16;
-constexpr RawState InProgressFlag = 1 << 17;
-constexpr RawState WaitingFlags = DrainingFlag | InProgressFlag;
-
-inline bool isWaitingState(RawState state)
-{
-    return (state & WaitingFlags) != 0;
-}
-
-inline bool isDrainingState(RawState state)
-{
-    return (state & DrainingFlag) != 0;
-}
-
-inline RawState fromWaitingState(RawState state)
-{
-    return state & ~WaitingFlags;
-}
-
-inline QAudio::State toAudioState(RawState state)
-{
-    return QAudio::State(fromWaitingState(state));
-}
-
-template<typename... States>
-constexpr std::pair<RawState, uint32_t> makeStatesSet(QAudio::State first, States... others)
-{
-    return { first, ((1 << first) | ... | (1 << others)) };
-}
-
-// ensures compareExchange (testAndSet) operation with opportunity
-// to check several states, can be considered as atomic
-template<typename T, typename Predicate>
-bool multipleCompareExchange(std::atomic<T> &target, T &prevValue, T newValue, Predicate predicate)
-{
-    Q_ASSERT(predicate(prevValue));
-    do {
-        if (target.compare_exchange_strong(prevValue, newValue))
-            return true;
-    } while (predicate(prevValue));
-
-    return false;
-}
-
-} // namespace
+using namespace AudioStateMachineUtils;
 
 struct QAudioStateMachine::Synchronizer {
     QMutex m_mutex;
