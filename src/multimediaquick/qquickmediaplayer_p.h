9559eee6a7d6bb0f37f950db39b870d40a71ba87
Artem Dyomin <artem.dyomin@qt.io>
2023-09-16 00:32:56 +0200

    Fix MediaPlayer qml properties position and duration
    We document MediaPlayer.position and MediaPlayer.duraton as int,
    but declare the properties as qint64, which is not supported by qml.
    
    With the patch we init the correct properties in QQuickMediaPlayer,
    where required casts are implemented.
    
    Task-number: QTBUG-116978
    Change-Id: Id9d4d8710986dfa98a014c80b105df96e083e25a
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>
    (cherry picked from commit 2c278d0067f0399defff94c563329cd64e5bff5c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d318d3ca25df6f0f1f49f2a46d902d83a53effeb)

--- src/multimediaquick/qquickmediaplayer_p.h
+++ src/multimediaquick/qquickmediaplayer_p.h
@@ -27,6 +27,12 @@ class Q_MULTIMEDIAQUICK_EXPORT QQuickMediaPlayer : public QMediaPlayer
 {
     Q_OBJECT
     Q_PROPERTY(QUrl source READ qmlSource WRITE qmlSetSource NOTIFY qmlSourceChanged FINAL)
+
+    // qml doesn't support qint64, so we have to convert to the supported type.
+    // Int is expected to be enough for actual purposes.
+    Q_PROPERTY(int duration READ qmlDuration NOTIFY qmlDurationChanged FINAL)
+    Q_PROPERTY(int position READ qmlPosition WRITE setQmlPosition NOTIFY qmlPositionChanged FINAL)
+
     QML_NAMED_ELEMENT(MediaPlayer)
 
 public:
@@ -36,8 +42,20 @@ public:
 
     QUrl qmlSource() const;
 
+    void setQmlPosition(int position);
+
+    int qmlPosition() const;
+
+    int qmlDuration() const;
+
+private:
+    void onPositionChanged(qint64 position);
+    void onDurationChanged(qint64 position);
+
 Q_SIGNALS:
     void qmlSourceChanged(const QUrl &source);
+    void qmlPositionChanged(int position);
+    void qmlDurationChanged(int duration);
 
 private:
     QUrl m_source;
