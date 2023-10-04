632930177d3eceb4ecd47c098fb2ed4b3e32ab1c
Jøger Hansegård <joger.hansegard@qt.io>
2023-10-04 10:11:52 +0200

    Cleanup and harden mediaplayer tests
    The tests should be a specification for the media player. This change hardens
    the media player tests, removes dead code, and improves the media player
    specification.
    
    Change-Id: I2fcad8e06650ca982e03e2129fd60e7396769fa5
    Reviewed-by: Lars Sutterud <lars.sutterud@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 740b9eef8f87b4081786eae13f3285ee696ecf1c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 7057a57ba9d731bee16b6a7ebe860162246aac5e)

---
+++ tests/auto/integration/qmediaplayerbackend/fixture.h
@@ -0,0 +1,54 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#ifndef FIXTURE_H
+#define FIXTURE_H
+
+#include <qmediaplayer.h>
+#include <qaudiooutput.h>
+#include <qtest.h>
+
+QT_USE_NAMESPACE
+
+struct Fixture : QObject
+{
+    Q_OBJECT
+public:
+    Fixture()
+        : playbackStateChanged(&player, &QMediaPlayer::playbackStateChanged),
+          errorOccurred(&player, &QMediaPlayer::errorOccurred),
+          sourceChanged(&player, &QMediaPlayer::sourceChanged),
+          mediaStatusChanged(&player, &QMediaPlayer::mediaStatusChanged),
+          positionChanged(&player, &QMediaPlayer::positionChanged),
+          durationChanged(&player, &QMediaPlayer::durationChanged)
+    {
+        setVideoSinkAsyncFramesCounter(surface, framesCount);
+
+        player.setAudioOutput(&output);
+        player.setVideoOutput(&surface);
+    }
+
+    void clearSpies()
+    {
+        playbackStateChanged.clear();
+        errorOccurred.clear();
+        sourceChanged.clear();
+        mediaStatusChanged.clear();
+        positionChanged.clear();
+        durationChanged.clear();
+    }
+
+    QMediaPlayer player;
+    QAudioOutput output;
+    TestVideoSink surface;
+    std::atomic_int framesCount = 0;
+
+    QSignalSpy playbackStateChanged;
+    QSignalSpy errorOccurred;
+    QSignalSpy sourceChanged;
+    QSignalSpy mediaStatusChanged;
+    QSignalSpy positionChanged;
+    QSignalSpy durationChanged;
+};
+
+#endif // FIXTURE_H
