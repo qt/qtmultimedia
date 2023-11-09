2702a760bf02f05831af34e16a4806ae9a208f78
Tarja Sundqvist <tarja.sundqvist@qt.io>
2023-11-09 20:56:07 +0200

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the files in tqtc-qtmultimedia. Examples, tests, build
    files or documentation files are not updated.
    
    Task-number: QTQAINFRA-5900
    Change-Id: I215f8cd5707452458a26eff8c5e05017442b7941
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/plugins/multimedia/ffmpeg/playbackengine/qffmpegplaybackenginedefs_p.h
+++ src/plugins/multimedia/ffmpeg/playbackengine/qffmpegplaybackenginedefs_p.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Copyright (C) 2021 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 //
 //  W A R N I N G
--- src/plugins/multimedia/windows/mediacapture/qwindowsmediadevicesession_p.h
+++ src/plugins/multimedia/windows/mediacapture/qwindowsmediadevicesession_p.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2021 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #ifndef QWINDOWSMEDIADEVICESESSION_H
 #define QWINDOWSMEDIADEVICESESSION_H
