7754d34516eaca129286241eea7f36a3fa27ff3e
Jaishree Vyas <jaishree.vyas@qt.io>
2023-11-13 16:28:27 +0100

    Doc: Document limitations of QScreenCapture
    Details: Added known limitations in QScreenCapture class
    and the ScreenCapture QML type references.
    
    As the section is identical for both (except for the type name),
    use the \include command to pull in the content from a .qdocinc
    file. For C++ reference, prefix the type name with 'Q'.
    
    Removed [Q]WindowCapture as a see-also entry from 6.5
    
    Task-number: QTBUG-117407
    Change-Id: I1e94fd7d6af04026306fe6fc86aa095ba4e6f376
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 2b8cf0d1b883d562bdfb53f8fca31511381f7aa9)
    Reviewed-by: Andreas Eliasson <andreas.eliasson@qt.io>

--- src/multimedia/recording/qscreencapture.cpp
+++ src/multimedia/recording/qscreencapture.cpp
@@ -35,6 +35,10 @@ public:
     file.
 
     \snippet multimedia-snippets/media.cpp Media recorder
+
+    \include qscreencapture-limitations.qdocinc {content} {Q}
+
+    \sa QMediaCaptureSession
 */
 /*!
     \qmltype ScreenCapture
@@ -65,7 +69,9 @@ public:
     }
 \endqml
 
-    \sa ScreenCapture, CaptureSession
+    \include qscreencapture-limitations.qdocinc {content} {}
+
+    \sa CaptureSession
 */
 
 QScreenCapture::QScreenCapture(QObject *parent)
