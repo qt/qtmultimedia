ba83c4bcd112b02935a1b90596ce02973466faa6
Artem Dyomin <artem.dyomin@qt.io>
2023-11-27 11:24:45 +0100

    Improve the selection of media files in tests
    Media selection is used for prechecking existing urls and selecting
    a supported one of the given candidates.
    
    Let's check in a separate test case that all used test media files
    have been selected, and skip specific tests if the matching video
    is not available (as it was before the patch).
    
    Also, the patch improves error dumping of failed media files.
    
    Pick-to: 6.5
    Change-Id: If38c8d9cacf233db75f1469de61dbe320c00f64c
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 8268f5007ed4cdf13ec9dc3250f1acffc56c65ac)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/integration/shared/mediafileselector.h
+++ tests/auto/integration/shared/mediafileselector.h
@@ -5,47 +5,142 @@
 #define MEDIAFILESELECTOR_H
 
 #include <QUrl>
-#include <QMediaPlayer>
-#include <QAudioOutput>
+#include <qmediaplayer.h>
+#include <qaudiooutput.h>
+#include <qvideosink.h>
 #include <qsignalspy.h>
 #include <qfileinfo.h>
 #include <qtest.h>
+#include <private/qmultimediautils_p.h>
+
+#include <unordered_map>
 
 QT_BEGIN_NAMESPACE
 
-namespace MediaFileSelector {
+using MaybeUrl = QMaybe<QUrl, QString>;
 
-// TODO: refactor or remove the function
-inline QUrl selectMediaFile(const QStringList &mediaCandidates)
+#define CHECK_SELECTED_URL(maybeUrl)                                                             \
+  if (!maybeUrl)                                                                                 \
+  QSKIP((QLatin1String("\nUnable to select none of the media candidates:\n") + maybeUrl.error()) \
+                .toLocal8Bit()                                                                   \
+                .data())
+
+class MediaFileSelector
 {
-    QAudioOutput audioOutput;
-    QVideoSink videoOutput;
-    QMediaPlayer player;
-    player.setAudioOutput(&audioOutput);
-    player.setVideoOutput(&videoOutput);
+public:
+    quint32 failedSelectionsCount() const { return m_failedSelectionsCount; }
 
-    QSignalSpy errorSpy(&player, SIGNAL(errorOccurred(QMediaPlayer::Error, const QString&)));
+    QString dumpErrors() const
+    {
+        QStringList failedMedias;
+        for (const auto &mediaToError : m_mediaToErrors)
+            if (!mediaToError.second.isEmpty())
+                failedMedias.emplace_back(mediaToError.first);
 
-    for (const QString &media : mediaCandidates) {
+        failedMedias.sort();
+        return dumpErrors(failedMedias);
+    }
+
+    template <typename... Media>
+    MaybeUrl select(Media... media)
+    {
+        return select({ std::move(media)... });
+    }
+
+    MaybeUrl select(const QStringList &candidates)
+    {
+        QUrl foundUrl;
+        for (const auto &media : candidates) {
+            auto emplaceRes = m_mediaToErrors.try_emplace(media, QString());
+            if (emplaceRes.second) {
+                auto maybeUrl = selectMediaFile(media);
+                if (!maybeUrl) {
+                    Q_ASSERT(!maybeUrl.error().isEmpty());
+                    emplaceRes.first->second = maybeUrl.error();
+                }
+            }
+
+            if (foundUrl.isEmpty() && emplaceRes.first->second.isEmpty())
+                foundUrl = media;
+        }
+
+        if (!foundUrl.isEmpty())
+            return foundUrl;
+
+        ++m_failedSelectionsCount;
+        return { QUnexpect{}, dumpErrors(candidates) };
+    }
+
+private:
+    QString dumpErrors(const QStringList &medias) const
+    {
+        using namespace Qt::StringLiterals;
+        QString result;
+
+        for (const auto &media : medias) {
+            auto it = m_mediaToErrors.find(media);
+            if (it != m_mediaToErrors.end() && !it->second.isEmpty())
+                result.append("\t"_L1)
+                        .append(it->first)
+                        .append(": "_L1)
+                        .append(it->second)
+                        .append("\n"_L1);
+        }
+
+        return result;
+    }
+
+    static MaybeUrl selectMediaFile(QString media)
+    {
+        using namespace Qt::StringLiterals;
+
+        QAudioOutput audioOutput;
+        QVideoSink videoOutput;
+        QMediaPlayer player;
+        player.setAudioOutput(&audioOutput);
+        player.setVideoOutput(&videoOutput);
+
         player.setSource(media);
         player.play();
 
-        for (int i = 0; i < 2000 && player.mediaStatus() != QMediaPlayer::BufferedMedia && errorSpy.isEmpty(); i+=50) {
-            QTest::qWait(50);
-        }
+        const auto waitingFinished = QTest::qWaitFor([&]() {
+            const auto status = player.mediaStatus();
+            return status == QMediaPlayer::BufferedMedia || status == QMediaPlayer::EndOfMedia
+                    || status == QMediaPlayer::InvalidMedia
+                    || player.error() != QMediaPlayer::NoError;
+        });
 
-        if (player.mediaStatus() == QMediaPlayer::BufferedMedia && errorSpy.isEmpty()) {
-            return media;
-        }
-        errorSpy.clear();
+        auto enumValueToString = [](auto enumValue) {
+            return QString(QMetaEnum::fromType<decltype(enumValue)>().valueToKey(enumValue));
+        };
+
+        if (!waitingFinished)
+            return { QUnexpect{},
+                     "The media got stuck in the status "_L1
+                             + enumValueToString(player.mediaStatus()) };
+
+        if (player.mediaStatus() == QMediaPlayer::InvalidMedia)
+            return { QUnexpect{},
+                     "Unable to load the media. Error ["_L1 + enumValueToString(player.error())
+                             + " "_L1 + player.errorString() + "]"_L1 };
+
+        if (player.error() != QMediaPlayer::NoError)
+            return { QUnexpect{},
+                     "Unable to start playing the media, codecs issues. Error ["_L1
+                             + enumValueToString(player.error()) + " "_L1 + player.errorString()
+                             + "]"_L1 };
+
+        return QUrl(media);
     }
 
-    return QUrl();
-}
-
-} // MediaFileSelector namespace
+private:
+    std::unordered_map<QString, QString> m_mediaToErrors;
+    quint32 m_failedSelectionsCount = 0;
+};
 
 QT_END_NAMESPACE
+
+Q_DECLARE_METATYPE(MaybeUrl)
 
 #endif
 
