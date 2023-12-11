qt/qtmultimedia

commit cd5b8dc4876ecd3c75e1c7377b09ea800ecbf283

Remove unused member variable
Change-Id: Ia4c516322188ae3d88e43483936ddd036567995c
Reviewed-by: Lars Knoll <lars@knoll.priv.no>

src/plugins/multimedia/windows/player/mfvideorenderercontrol.cpp
@@ -2080,7 +2080,6 @@ namespace
 
     private:
         long    m_cRef;
-        bool    m_shutdown;
         MediaSink *m_sink;
         MFVideoRendererControl *m_rendererControl;
         IMFAttributes *m_attributes;

commit 71f2b187c5bbcb77cbc544345a4ec9f3c20f446b

Use QComObject base class
Replace common IUnknown implementations with QComObject base class.

This is a manual cherry-pick with manual but trivial conflict resolution
in src\plugins\multimedia\windows\player\mfvideorenderercontrol.cpp. The
conflict was caused by change cd5b8dc4876ecd3c75e1c7377b09ea800ecbf283,
which was previously not picked.

After this cherry-pick, the 6.5 version of mfvideorendercontrol.cpp is
identical to 6.6 and 6.7.

Change-Id: I2f11c0d8d4a18cb715b37aae36e1756f0fe2fbb4
Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
(cherry picked from commit c75bd69dc81de408a0d9c0fefb4536cea3781206)
(cherry picked from commit 73ac593d2b517d6fc9e668705cee160fccb3c2c8)

src/multimedia/windows/qwindowsmediadevices.cpp
@@ -19,13 +19,13 @@
 #include <qwindowsmfdefs_p.h>
 
 #include <QtCore/qmap.h>
+#include <private/qcomobject_p.h>
 #include <private/qsystemerror_p.h>
 
 QT_BEGIN_NAMESPACE
 
-class CMMNotificationClient : public IMMNotificationClient
+class CMMNotificationClient : public QComObject<IMMNotificationClient>
 {
-    LONG m_cRef;
     ComPtr<IMMDeviceEnumerator> m_enumerator;
     QWindowsMediaDevices *m_windowsMediaDevices;
     QMap<QString, DWORD> m_deviceState;
@@ -33,44 +33,11 @@ class CMMNotificationClient : public IMMNotificationClient
 public:
     CMMNotificationClient(QWindowsMediaDevices *windowsMediaDevices,
                           ComPtr<IMMDeviceEnumerator> enumerator,
-                          QMap<QString, DWORD> &&deviceState) :
-        m_cRef(1),
-        m_enumerator(enumerator),
-        m_windowsMediaDevices(windowsMediaDevices),
-        m_deviceState(deviceState)
+                          QMap<QString, DWORD> &&deviceState)
+        : m_enumerator(enumerator),
+          m_windowsMediaDevices(windowsMediaDevices),
+          m_deviceState(deviceState)
     {}
-
-    virtual ~CMMNotificationClient() {}
-
-    // IUnknown methods -- AddRef, Release, and QueryInterface
-    ULONG STDMETHODCALLTYPE AddRef() override
-    {
-        return InterlockedIncrement(&m_cRef);
-    }
-
-    ULONG STDMETHODCALLTYPE Release() override
-    {
-        ULONG ulRef = InterlockedDecrement(&m_cRef);
-        if (0 == ulRef) {
-            delete this;
-        }
-        return ulRef;
-    }
-
-    HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, VOID **ppvInterface) override
-    {
-        if (IID_IUnknown == riid) {
-            AddRef();
-            *ppvInterface = (IUnknown*)this;
-        } else if (__uuidof(IMMNotificationClient) == riid) {
-            AddRef();
-            *ppvInterface = (IMMNotificationClient*)this;
-        } else {
-            *ppvInterface = NULL;
-            return E_NOINTERFACE;
-        }
-        return S_OK;
-    }
 
     HRESULT STDMETHODCALLTYPE OnDefaultDeviceChanged(EDataFlow flow, ERole role, LPCWSTR) override
     {
@@ -145,6 +112,10 @@ public:
                     emitAudioDevicesChanged(flow);
         }
     }
+
+private:
+    // Destructor is not public. Caller should call Release.
+    ~CMMNotificationClient() override = default;
 };
 
 QWindowsMediaDevices::QWindowsMediaDevices()

src/plugins/multimedia/ffmpeg/qwindowscamera.cpp
@@ -8,6 +8,7 @@
 #include <private/qmemoryvideobuffer_p.h>
 #include <private/qwindowsmfdefs_p.h>
 #include <private/qwindowsmultimediautils_p.h>
+#include <private/qcomobject_p.h>
 
 #include <mfapi.h>
 #include <mfidl.h>
@@ -20,42 +21,9 @@ QT_BEGIN_NAMESPACE
 
 using namespace QWindowsMultimediaUtils;
 
-class CameraReaderCallback : public IMFSourceReaderCallback
+class CameraReaderCallback : public QComObject<IMFSourceReaderCallback>
 {
 public:
-    CameraReaderCallback() : m_cRef(1) { }
-
-    //from IUnknown
-    STDMETHODIMP QueryInterface(REFIID riid, LPVOID *ppvObject) override
-    {
-        if (!ppvObject)
-            return E_POINTER;
-        if (riid == IID_IMFSourceReaderCallback) {
-            *ppvObject = static_cast<IMFSourceReaderCallback*>(this);
-        } else if (riid == IID_IUnknown) {
-            *ppvObject = static_cast<IUnknown *>(this);
-        } else {
-            *ppvObject =  nullptr;
-            return E_NOINTERFACE;
-        }
-        AddRef();
-        return S_OK;
-    }
-
-    STDMETHODIMP_(ULONG) AddRef() override
-    {
-        return InterlockedIncrement(&m_cRef);
-    }
-
-    STDMETHODIMP_(ULONG) Release() override
-    {
-        LONG cRef = InterlockedDecrement(&m_cRef);
-        if (cRef == 0) {
-            delete this;
-        }
-        return cRef;
-    }
-
     //from IMFSourceReaderCallback
     STDMETHODIMP OnReadSample(HRESULT status, DWORD, DWORD, LONGLONG timestamp, IMFSample *sample) override;
     STDMETHODIMP OnFlush(DWORD) override;
@@ -67,10 +35,9 @@ public:
         m_activeCamera = activeCamera;
     }
 private:
-    // Destructor is private. Caller should call Release.
-    virtual ~CameraReaderCallback() { }
+    // Destructor is not public. Caller should call Release.
+    ~CameraReaderCallback() override = default;
 
-    LONG m_cRef;
     ActiveCamera *m_activeCamera = nullptr;
     QMutex m_mutex;
 };

src/plugins/multimedia/windows/player/mfactivate.cpp
@@ -6,8 +6,6 @@
 #include <mfapi.h>
 
 MFAbstractActivate::MFAbstractActivate()
-    : m_attributes(0)
-    , m_cRef(1)
 {
     MFCreateAttributes(&m_attributes, 0);
 }
@@ -16,36 +14,4 @@ MFAbstractActivate::~MFAbstractActivate()
 {
     if (m_attributes)
         m_attributes->Release();
-}
-
-
-HRESULT MFAbstractActivate::QueryInterface(REFIID riid, LPVOID *ppvObject)
-{
-    if (!ppvObject)
-        return E_POINTER;
-    if (riid == IID_IMFActivate) {
-        *ppvObject = static_cast<IMFActivate*>(this);
-    } else if (riid == IID_IMFAttributes) {
-        *ppvObject = static_cast<IMFAttributes*>(this);
-    } else if (riid == IID_IUnknown) {
-        *ppvObject = static_cast<IUnknown*>(static_cast<IMFActivate*>(this));
-    } else {
-        *ppvObject =  NULL;
-        return E_NOINTERFACE;
-    }
-    AddRef();
-    return S_OK;
-}
-
-ULONG MFAbstractActivate::AddRef(void)
-{
-    return InterlockedIncrement(&m_cRef);
-}
-
-ULONG MFAbstractActivate::Release(void)
-{
-    ULONG cRef = InterlockedDecrement(&m_cRef);
-    if (cRef == 0)
-        delete this;
-    return cRef;
 }

src/plugins/multimedia/windows/player/mfactivate_p.h
@@ -16,17 +16,25 @@
 //
 
 #include <mfidl.h>
+#include <private/qcomobject_p.h>
 
-class MFAbstractActivate : public IMFActivate
+namespace QtPrivate {
+
+template <>
+struct QComObjectTraits<IMFActivate>
+{
+    static constexpr bool isGuidOf(REFIID riid) noexcept
+    {
+        return QComObjectTraits<IMFActivate, IMFAttributes>::isGuidOf(riid);
+    }
+};
+
+} // namespace QtPrivate
+
+class MFAbstractActivate : public QComObject<IMFActivate>
 {
 public:
     explicit MFAbstractActivate();
-    virtual ~MFAbstractActivate();
-
-    //from IUnknown
-    STDMETHODIMP QueryInterface(REFIID riid, LPVOID *ppvObject) override;
-    STDMETHODIMP_(ULONG) AddRef(void) override;
-    STDMETHODIMP_(ULONG) Release(void) override;
 
     //from IMFAttributes
     STDMETHODIMP GetItem(REFGUID guidKey, PROPVARIANT *pValue) override
@@ -179,9 +187,12 @@ public:
         return m_attributes->CopyAllItems(pDest);
     }
 
+protected:
+    // Destructor is not public. Caller should call Release.
+    ~MFAbstractActivate() override;
+
 private:
-    IMFAttributes *m_attributes;
-    ULONG m_cRef;
+    IMFAttributes *m_attributes = nullptr;
 };
 
 #endif // MFACTIVATE_H

src/plugins/multimedia/windows/player/mftvideo.cpp
@@ -17,16 +17,6 @@
 
 // The implementation is based on a boilerplate from the MF SDK example.
 
-MFTransform::MFTransform():
-    m_cRef(1),
-    m_inputType(0),
-    m_outputType(0),
-    m_sample(0),
-    m_videoSinkTypeHandler(0),
-    m_bytesPerLine(0)
-{
-}
-
 MFTransform::~MFTransform()
 {
     if (m_inputType)
@@ -67,36 +57,6 @@ void MFTransform::setVideoSink(IUnknown *videoSink)
 
     if (videoSink)
         videoSink->QueryInterface(IID_PPV_ARGS(&m_videoSinkTypeHandler));
-}
-
-STDMETHODIMP MFTransform::QueryInterface(REFIID riid, void** ppv)
-{
-    if (!ppv)
-        return E_POINTER;
-    if (riid == IID_IMFTransform) {
-        *ppv = static_cast<IMFTransform*>(this);
-    } else if (riid == IID_IUnknown) {
-        *ppv = static_cast<IUnknown*>(this);
-    } else {
-        *ppv =  NULL;
-        return E_NOINTERFACE;
-    }
-    AddRef();
-    return S_OK;
-}
-
-STDMETHODIMP_(ULONG) MFTransform::AddRef()
-{
-    return InterlockedIncrement(&m_cRef);
-}
-
-STDMETHODIMP_(ULONG) MFTransform::Release()
-{
-    ULONG cRef = InterlockedDecrement(&m_cRef);
-    if (cRef == 0) {
-        delete this;
-    }
-    return cRef;
 }
 
 STDMETHODIMP MFTransform::GetStreamLimits(DWORD *pdwInputMinimum, DWORD *pdwInputMaximum, DWORD *pdwOutputMinimum, DWORD *pdwOutputMaximum)

src/plugins/multimedia/windows/player/mftvideo_p.h
@@ -20,7 +20,7 @@
 #include <QtCore/qlist.h>
 #include <QtCore/qmutex.h>
 #include <QtMultimedia/qvideoframeformat.h>
-
+#include <private/qcomobject_p.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -28,21 +28,13 @@ class MFVideoProbeControl;
 
 class QVideoFrame;
 
-class MFTransform: public IMFTransform
+class MFTransform : public QComObject<IMFTransform>
 {
 public:
-    MFTransform();
-    virtual ~MFTransform();
-
     void addProbe(MFVideoProbeControl* probe);
     void removeProbe(MFVideoProbeControl* probe);
 
     void setVideoSink(IUnknown *videoSink);
-
-    // IUnknown methods
-    STDMETHODIMP QueryInterface(REFIID iid, void** ppv) override;
-    STDMETHODIMP_(ULONG) AddRef() override;
-    STDMETHODIMP_(ULONG) Release() override;
 
     // IMFTransform methods
     STDMETHODIMP GetStreamLimits(DWORD *pdwInputMinimum, DWORD *pdwInputMaximum, DWORD *pdwOutputMinimum, DWORD *pdwOutputMaximum) override;
@@ -70,16 +62,18 @@ public:
     STDMETHODIMP ProcessOutput(DWORD dwFlags, DWORD cOutputBufferCount, MFT_OUTPUT_DATA_BUFFER *pOutputSamples, DWORD *pdwStatus) override;
 
 private:
+    // Destructor is not public. Caller should call Release.
+    ~MFTransform() override;
+
     HRESULT OnFlush();
     static QVideoFrameFormat videoFormatForMFMediaType(IMFMediaType *mediaType, int *bytesPerLine);
     QVideoFrame makeVideoFrame();
     QByteArray dataFromBuffer(IMFMediaBuffer *buffer, int height, int *bytesPerLine);
     bool isMediaTypeSupported(IMFMediaType *type);
 
-    long m_cRef;
-    IMFMediaType *m_inputType;
-    IMFMediaType *m_outputType;
-    IMFSample *m_sample;
+    IMFMediaType *m_inputType = nullptr;
+    IMFMediaType *m_outputType = nullptr;
+    IMFSample *m_sample = nullptr;
     QMutex m_mutex;
 
     IMFMediaTypeHandler *m_videoSinkTypeHandler;
@@ -88,7 +82,7 @@ private:
     QMutex m_videoProbeMutex;
 
     QVideoFrameFormat m_format;
-    int m_bytesPerLine;
+    int m_bytesPerLine = 0;
 };
 
 QT_END_NAMESPACE

src/plugins/multimedia/windows/player/mfvideorenderercontrol.cpp
@@ -9,6 +9,7 @@
 #include <private/qplatformvideosink_p.h>
 #include <private/qabstractvideobuffer_p.h>
 #include <private/qwindowsmfdefs_p.h>
+#include <private/qcomobject_p.h>
 #include <qvideosink.h>
 #include <qvideoframeformat.h>
 #include <qtimer.h>
@@ -23,6 +24,46 @@
 #define PAD_TO_DWORD(x)  (((x) + 3) & ~3)
 
 QT_BEGIN_NAMESPACE
+
+namespace QtPrivate {
+
+template <>
+struct QComObjectTraits<IMFFinalizableMediaSink>
+{
+    static constexpr bool isGuidOf(REFIID riid) noexcept
+    {
+        return QComObjectTraits<IMFFinalizableMediaSink, IMFMediaSink>::isGuidOf(riid);
+    }
+};
+
+template <>
+struct QComObjectTraits<IMFStreamSink>
+{
+    static constexpr bool isGuidOf(REFIID riid) noexcept
+    {
+        return QComObjectTraits<IMFStreamSink, IMFMediaEventGenerator>::isGuidOf(riid);
+    }
+};
+
+} // namespace QtPrivate
+
+namespace {
+// Custom interface for handling IMFStreamSink::PlaceMarker calls asynchronously.
+static const GUID IID_IMarker = {
+    0xa3ff32de, 0x1031, 0x438a, { 0x8b, 0x47, 0x82, 0xf8, 0xac, 0xda, 0x59, 0xb7 }
+};
+MIDL_INTERFACE("a3ff32de-1031-438a-8b47-82f8acda59b7")
+IMarker : public IUnknown
+{
+    virtual STDMETHODIMP GetMarkerType(MFSTREAMSINK_MARKER_TYPE * pType) = 0;
+    virtual STDMETHODIMP GetMarkerValue(PROPVARIANT * pvar) = 0;
+    virtual STDMETHODIMP GetContext(PROPVARIANT * pvar) = 0;
+};
+} // namespace
+
+#ifdef __CRT_UUID_DECL
+__CRT_UUID_DECL(IMarker, 0xa3ff32de, 0x1031, 0x438a, 0x8b, 0x47, 0x82, 0xf8, 0xac, 0xda, 0x59, 0xb7)
+#endif
 
 namespace
 {
@@ -82,17 +123,7 @@ namespace
         QVideoFrame::MapMode m_mapMode;
     };
 
-    // Custom interface for handling IMFStreamSink::PlaceMarker calls asynchronously.
-    static const GUID IID_IMarker = {0xa3ff32de, 0x1031, 0x438a, {0x8b, 0x47, 0x82, 0xf8, 0xac, 0xda, 0x59, 0xb7}};
-    MIDL_INTERFACE("a3ff32de-1031-438a-8b47-82f8acda59b7")
-    IMarker : public IUnknown
-    {
-        virtual STDMETHODIMP GetMarkerType(MFSTREAMSINK_MARKER_TYPE *pType) = 0;
-        virtual STDMETHODIMP GetMarkerValue(PROPVARIANT *pvar) = 0;
-        virtual STDMETHODIMP GetContext(PROPVARIANT *pvar) = 0;
-    };
-
-    class Marker : public IMarker
+    class Marker : public QComObject<IMarker>
     {
     public:
         static HRESULT Create(
@@ -127,37 +158,6 @@ namespace
             return hr;
         }
 
-        // IUnknown methods.
-        STDMETHODIMP QueryInterface(REFIID iid, void** ppv) override
-        {
-            if (!ppv)
-                return E_POINTER;
-            if (iid == IID_IUnknown) {
-                *ppv = static_cast<IUnknown*>(this);
-            } else if (iid == IID_IMarker) {
-                *ppv = static_cast<IMarker*>(this);
-            } else {
-                *ppv = NULL;
-                return E_NOINTERFACE;
-            }
-            AddRef();
-            return S_OK;
-        }
-
-        STDMETHODIMP_(ULONG) AddRef() override
-        {
-            return InterlockedIncrement(&m_cRef);
-        }
-
-        STDMETHODIMP_(ULONG) Release() override
-        {
-            LONG cRef = InterlockedDecrement(&m_cRef);
-            if (cRef == 0)
-                delete this;
-            // For thread safety, return a temporary variable.
-            return cRef;
-        }
-
         STDMETHODIMP GetMarkerType(MFSTREAMSINK_MARKER_TYPE *pType) override
         {
             if (pType == NULL)
@@ -186,22 +186,21 @@ namespace
         PROPVARIANT m_varContextValue;
 
     private:
-        long    m_cRef = 1;
-
         Marker(MFSTREAMSINK_MARKER_TYPE eMarkerType) : m_eMarkerType(eMarkerType)
         {
             PropVariantInit(&m_varMarkerValue);
             PropVariantInit(&m_varContextValue);
         }
 
-        virtual ~Marker()
+        // Destructor is not public. Caller should call Release.
+        ~Marker() override
         {
             PropVariantClear(&m_varMarkerValue);
             PropVariantClear(&m_varContextValue);
         }
     };
 
-    class MediaStream : public QObject, public IMFStreamSink, public IMFMediaTypeHandler
+    class MediaStream : public QObject, public QComObject<IMFStreamSink, IMFMediaTypeHandler>
     {
         Q_OBJECT
         friend class MFVideoRendererControl;
@@ -218,46 +217,6 @@ namespace
                 qWarning("Failed to create mf event queue!");
             if (FAILED(MFAllocateWorkQueue(&m_workQueueId)))
                 qWarning("Failed to allocated mf work queue!");
-        }
-
-        ~MediaStream()
-        {
-            Q_ASSERT(m_shutdown);
-        }
-
-        //from IUnknown
-        STDMETHODIMP QueryInterface(REFIID riid, void** ppvObject) override
-        {
-            if (!ppvObject)
-                return E_POINTER;
-            if (riid == IID_IMFStreamSink) {
-                *ppvObject = static_cast<IMFStreamSink*>(this);
-            } else if (riid == IID_IMFMediaEventGenerator) {
-                *ppvObject = static_cast<IMFMediaEventGenerator*>(this);
-            } else if (riid == IID_IMFMediaTypeHandler) {
-                *ppvObject = static_cast<IMFMediaTypeHandler*>(this);
-            } else if (riid == IID_IUnknown) {
-                *ppvObject = static_cast<IUnknown*>(static_cast<IMFStreamSink*>(this));
-            } else {
-                *ppvObject =  NULL;
-                return E_NOINTERFACE;
-            }
-            AddRef();
-            return S_OK;
-        }
-
-        STDMETHODIMP_(ULONG) AddRef(void) override
-        {
-            return InterlockedIncrement(&m_cRef);
-        }
-
-        STDMETHODIMP_(ULONG) Release(void) override
-        {
-            LONG cRef = InterlockedDecrement(&m_cRef);
-            if (cRef == 0)
-                delete this;
-            // For thread safety, return a temporary variable.
-            return cRef;
         }
 
         //from IMFMediaEventGenerator
@@ -883,7 +842,7 @@ namespace
         // Used to queue asynchronous operations. When we call MFPutWorkItem, we use this
         // object for the callback state (pState). Then, when the callback is invoked,
         // we can use the object to determine which asynchronous operation to perform.
-        class AsyncOperation : public IUnknown
+        class AsyncOperation : public QComObject<IUnknown>
         {
         public:
             AsyncOperation(StreamOperation op)
@@ -893,46 +852,18 @@ namespace
 
             StreamOperation m_op;   // The operation to perform.
 
-            //from IUnknown
-            STDMETHODIMP QueryInterface(REFIID iid, void** ppv) override
-            {
-                if (!ppv)
-                    return E_POINTER;
-                if (iid == IID_IUnknown) {
-                    *ppv = static_cast<IUnknown*>(this);
-                } else {
-                    *ppv = NULL;
-                    return E_NOINTERFACE;
-                }
-                AddRef();
-                return S_OK;
-            }
-            STDMETHODIMP_(ULONG) AddRef() override
-            {
-                return InterlockedIncrement(&m_cRef);
-            }
-            STDMETHODIMP_(ULONG) Release() override
-            {
-                ULONG uCount = InterlockedDecrement(&m_cRef);
-                if (uCount == 0)
-                    delete this;
-                // For thread safety, return a temporary variable.
-                return uCount;
-            }
-
         private:
-            long    m_cRef = 1;
-            virtual ~AsyncOperation()
-            {
-                Q_ASSERT(m_cRef == 0);
-            }
+            // Destructor is not public. Caller should call Release.
+            ~AsyncOperation() override = default;
         };
+
+        // Destructor is not public. Caller should call Release.
+        ~MediaStream() override { Q_ASSERT(m_shutdown); }
 
         // ValidStateMatrix: Defines a look-up table that says which operations
         // are valid from which states.
         static BOOL ValidStateMatrix[State_Count][Op_Count];
 
-        long m_cRef = 1;
         QMutex m_mutex;
 
         IMFMediaType *m_currentMediaType = nullptr;
@@ -1359,21 +1290,13 @@ namespace
         // 2. While paused, the sink accepts samples but does not process them.
     };
 
-    class MediaSink : public IMFFinalizableMediaSink,
-                      public IMFClockStateSink,
-                      public IMFMediaSinkPreroll,
-                      public IMFGetService,
-                      public IMFRateSupport
+    class MediaSink : public QComObject<IMFFinalizableMediaSink, IMFClockStateSink,
+                                        IMFMediaSinkPreroll, IMFGetService, IMFRateSupport>
     {
     public:
         MediaSink(MFVideoRendererControl *rendererControl)
         {
             m_stream = new MediaStream(this, rendererControl);
-        }
-
-        virtual ~MediaSink()
-        {
-            Q_ASSERT(m_shutdown);
         }
 
         void setSurface(QVideoSink *surface)
@@ -1414,45 +1337,6 @@ namespace
         {
             QMutexLocker locker(&m_mutex);
             return m_playRate;
-        }
-
-        //from IUnknown
-        STDMETHODIMP QueryInterface(REFIID riid, void** ppvObject) override
-        {
-            if (!ppvObject)
-                return E_POINTER;
-            if (riid == IID_IMFMediaSink) {
-                *ppvObject = static_cast<IMFMediaSink*>(this);
-            } else if (riid == IID_IMFGetService) {
-                *ppvObject = static_cast<IMFGetService*>(this);
-            } else if (riid == IID_IMFMediaSinkPreroll) {
-                *ppvObject = static_cast<IMFMediaSinkPreroll*>(this);
-            } else if (riid == IID_IMFClockStateSink) {
-                *ppvObject = static_cast<IMFClockStateSink*>(this);
-            } else if (riid == IID_IMFRateSupport) {
-                *ppvObject = static_cast<IMFRateSupport*>(this);
-            } else if (riid == IID_IUnknown) {
-                *ppvObject = static_cast<IUnknown*>(static_cast<IMFFinalizableMediaSink*>(this));
-            } else {
-                *ppvObject =  NULL;
-                return E_NOINTERFACE;
-            }
-            AddRef();
-            return S_OK;
-        }
-
-        STDMETHODIMP_(ULONG) AddRef(void) override
-        {
-            return InterlockedIncrement(&m_cRef);
-        }
-
-        STDMETHODIMP_(ULONG) Release(void) override
-        {
-            LONG cRef = InterlockedDecrement(&m_cRef);
-            if (cRef == 0)
-                delete this;
-            // For thread safety, return a temporary variable.
-            return cRef;
         }
 
         // IMFGetService methods
@@ -1725,7 +1609,9 @@ namespace
         }
 
     private:
-        long    m_cRef = 1;
+        // Destructor is not public. Caller should call Release.
+        ~MediaSink() override { Q_ASSERT(m_shutdown); }
+
         QMutex  m_mutex;
         bool    m_shutdown = false;
         IMFPresentationClock *m_presentationClock = nullptr;
@@ -1733,56 +1619,14 @@ namespace
         float   m_playRate = 1;
     };
 
-    class VideoRendererActivate : public IMFActivate
+    class VideoRendererActivate : public QComObject<IMFActivate>
     {
     public:
         VideoRendererActivate(MFVideoRendererControl *rendererControl)
-            : m_cRef(1)
-            , m_sink(0)
-            , m_rendererControl(rendererControl)
-            , m_attributes(0)
-            , m_videoSink(0)
+            : m_sink(0), m_rendererControl(rendererControl), m_attributes(0), m_videoSink(0)
         {
             MFCreateAttributes(&m_attributes, 0);
             m_sink = new MediaSink(rendererControl);
-        }
-
-        virtual ~VideoRendererActivate()
-        {
-            m_attributes->Release();
-        }
-
-        //from IUnknown
-        STDMETHODIMP QueryInterface(REFIID riid, void** ppvObject) override
-        {
-            if (!ppvObject)
-                return E_POINTER;
-            if (riid == IID_IMFActivate) {
-                *ppvObject = static_cast<IMFActivate*>(this);
-            } else if (riid == IID_IMFAttributes) {
-                *ppvObject = static_cast<IMFAttributes*>(this);
-            } else if (riid == IID_IUnknown) {
-                *ppvObject = static_cast<IUnknown*>(static_cast<IMFActivate*>(this));
-            } else {
-                *ppvObject =  NULL;
-                return E_NOINTERFACE;
-            }
-            AddRef();
-            return S_OK;
-        }
-
-        STDMETHODIMP_(ULONG) AddRef(void) override
-        {
-            return InterlockedIncrement(&m_cRef);
-        }
-
-        STDMETHODIMP_(ULONG) Release(void) override
-        {
-            LONG cRef = InterlockedDecrement(&m_cRef);
-            if (cRef == 0)
-                delete this;
-            // For thread safety, return a temporary variable.
-            return cRef;
         }
 
         //from IMFActivate
@@ -2079,8 +1923,9 @@ namespace
         }
 
     private:
-        long    m_cRef;
-        bool    m_shutdown;
+        // Destructor is not public. Caller should call Release.
+        ~VideoRendererActivate() override { m_attributes->Release(); }
+
         MediaSink *m_sink;
         MFVideoRendererControl *m_rendererControl;
         IMFAttributes *m_attributes;
@@ -2094,8 +1939,6 @@ class EVRCustomPresenterActivate : public MFAbstractActivate
 {
 public:
     EVRCustomPresenterActivate(QVideoSink *sink);
-    ~EVRCustomPresenterActivate()
-    { }
 
     STDMETHODIMP ActivateObject(REFIID riid, void **ppv) override;
     STDMETHODIMP ShutdownObject() override;
@@ -2105,6 +1948,9 @@ public:
     void setCropRect(QRect cropRect);
 
 private:
+    // Destructor is not public. Caller should call Release.
+    ~EVRCustomPresenterActivate() override { }
+
     EVRCustomPresenter *m_presenter;
     QVideoSink *m_videoSink;
     QRect m_cropRect;

src/plugins/multimedia/windows/player/samplegrabber.cpp
@@ -3,39 +3,6 @@
 
 #include "samplegrabber_p.h"
 
-STDMETHODIMP SampleGrabberCallback::QueryInterface(REFIID riid, void** ppv)
-{
-    if (!ppv)
-        return E_POINTER;
-    if (riid == IID_IMFSampleGrabberSinkCallback) {
-        *ppv = static_cast<IMFSampleGrabberSinkCallback*>(this);
-    } else if (riid == IID_IMFClockStateSink) {
-        *ppv = static_cast<IMFClockStateSink*>(this);
-    } else if (riid == IID_IUnknown) {
-        *ppv = static_cast<IUnknown*>(this);
-    } else {
-        *ppv =  NULL;
-        return E_NOINTERFACE;
-    }
-    AddRef();
-    return S_OK;
-}
-
-STDMETHODIMP_(ULONG) SampleGrabberCallback::AddRef()
-{
-    return InterlockedIncrement(&m_cRef);
-}
-
-STDMETHODIMP_(ULONG) SampleGrabberCallback::Release()
-{
-    ULONG cRef = InterlockedDecrement(&m_cRef);
-    if (cRef == 0) {
-        delete this;
-    }
-    return cRef;
-
-}
-
 // IMFClockStateSink methods.
 
 STDMETHODIMP SampleGrabberCallback::OnClockStart(MFTIME hnsSystemTime, LONGLONG llClockStartOffset)

src/plugins/multimedia/windows/player/samplegrabber_p.h
@@ -20,17 +20,26 @@
 #include <QtMultimedia/qaudioformat.h>
 #include <mfapi.h>
 #include <mfidl.h>
+#include <private/qcomobject_p.h>
 
 QT_BEGIN_NAMESPACE
 
-class SampleGrabberCallback : public IMFSampleGrabberSinkCallback
+namespace QtPrivate {
+
+template <>
+struct QComObjectTraits<IMFSampleGrabberSinkCallback>
+{
+    static constexpr bool isGuidOf(REFIID riid) noexcept
+    {
+        return QComObjectTraits<IMFSampleGrabberSinkCallback, IMFClockStateSink>::isGuidOf(riid);
+    }
+};
+
+} // namespace QtPrivate
+
+class SampleGrabberCallback : public QComObject<IMFSampleGrabberSinkCallback>
 {
 public:
-    // IUnknown methods
-    STDMETHODIMP QueryInterface(REFIID iid, void** ppv) override;
-    STDMETHODIMP_(ULONG) AddRef() override;
-    STDMETHODIMP_(ULONG) Release() override;
-
     // IMFClockStateSink methods
     STDMETHODIMP OnClockStart(MFTIME hnsSystemTime, LONGLONG llClockStartOffset) override;
     STDMETHODIMP OnClockStop(MFTIME hnsSystemTime) override;
@@ -43,13 +52,10 @@ public:
     STDMETHODIMP OnShutdown() override;
 
 protected:
-    SampleGrabberCallback() : m_cRef(1) {}
+    SampleGrabberCallback() = default;
 
-public:
-    virtual ~SampleGrabberCallback() {}
-
-private:
-    long m_cRef;
+    // Destructor is not public. Caller should call Release.
+    ~SampleGrabberCallback() override = default;
 };
 
 class AudioSampleGrabberCallback: public SampleGrabberCallback {