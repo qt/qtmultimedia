b2ab32dbafde552f2db7e5c1d86e85b9b5d04dde
Artem Dyomin <artem.dyomin@qt.io>
2023-12-22 13:01:37 +0100

    Add empty eglfs screen capture
    We need custom eglfs screen capture implementation as
    common QScreen::grabWindow implementation has valuable overhead.
    
    The patch adds matching classes and methods,
    implementation is supposed to go in next commits.
    
    Change-Id: I40a605f3bd3651de51c75732041ffe28e385db91
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Mikko Hallamaa <mikko.hallamaa@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit 8c814014c4131ca1193c443714e2d2756e4f7e58)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 7220147c1be55a40c5f8ff1b7bb1d0a0984ef829)
    (cherry picked from commit c0844046ef6be7e8dfd683462dcfe8374ddd5412)

---
+++ src/plugins/multimedia/ffmpeg/qeglfsscreencapture_p.h
@@ -0,0 +1,45 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR
+// GPL-3.0-only
+
+#ifndef QEGLFSSCREENCAPTURE_H
+#define QEGLFSSCREENCAPTURE_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <private/qplatformsurfacecapture_p.h>
+#include <memory>
+
+QT_BEGIN_NAMESPACE
+
+class QEglfsScreenCapture : public QPlatformSurfaceCapture
+{
+public:
+    QEglfsScreenCapture();
+
+    ~QEglfsScreenCapture() override;
+
+    QVideoFrameFormat frameFormat() const override;
+
+    static bool isSupported();
+
+private:
+    bool setActiveInternal(bool active) override;
+
+private:
+    class Grabber;
+    std::unique_ptr<Grabber> m_grabber;
+};
+
+QT_END_NAMESPACE
+
+#endif // QEGLFSSCREENCAPTURE_H
--- src/plugins/multimedia/ffmpeg/qffmpegmediaintegration.cpp
+++ src/plugins/multimedia/ffmpeg/qffmpegmediaintegration.cpp
@@ -49,6 +49,10 @@ extern "C" {
 #include "qx11surfacecapture_p.h"
 #endif
 
+#if QT_CONFIG(eglfs)
+#include "qeglfsscreencapture_p.h"
+#endif
+
 QT_BEGIN_NAMESPACE
 
 class QFFmpegMediaPlugin : public QPlatformMediaPlugin
@@ -172,6 +176,11 @@ QPlatformSurfaceCapture *QFFmpegMediaIntegration::createScreenCapture(QScreenCap
 #if QT_CONFIG(xlib)
     if (QX11SurfaceCapture::isSupported())
         return new QX11SurfaceCapture(QPlatformSurfaceCapture::ScreenSource{});
+#endif
+
+#if QT_CONFIG(eglfs)
+    if (QEglfsScreenCapture::isSupported())
+        return new QEglfsScreenCapture;
 #endif
 
 #if defined(Q_OS_WINDOWS)
