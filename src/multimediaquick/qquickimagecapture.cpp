53eafd93b0a028be45adb2c440a7b4da207ca7f2
Bartlomiej Moskal <bartlomiej.moskal@qt.io>
2024-01-15 21:48:12 +0100

    Doc: Update parameter names in QML ImageCapture's signals
    The ImageCapture QML type contains invalid signal parameter names in the
    documentation. They should be changed so that they do not mislead users.
    
    Pick-to: 6.6 6.5
    Fixes: QTBUG-118593
    Change-Id: Icb4e46e1081a0b1bfc3eab1362a006eabeab5f7e
    Reviewed-by: Nicholas Bennett <nicholas.bennett@qt.io>
    (cherry picked from commit dd3701e6c8497947d73ac588c59726a049f50ca0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/multimediaquick/qquickimagecapture.cpp
+++ src/multimediaquick/qquickimagecapture.cpp
@@ -148,38 +148,37 @@ void QQuickImageCapture::_q_imageCaptured(int id, const QImage &preview)
 }
 
 /*!
-    \qmlsignal QtMultimedia::ImageCapture::errorOccurred(requestId, error, message)
+    \qmlsignal QtMultimedia::ImageCapture::errorOccurred(id, error, errorString)
 
-    This signal is emitted when an error occurs during capture with \a requestId.
+    This signal is emitted when an error occurs during capture with requested \a id.
     \a error is an enumeration of type ImageCapture::Error.
-    A descriptive message is available in \a message.
+    A descriptive message is available in \a errorString.
 */
 
 /*!
-    \qmlsignal QtMultimedia::ImageCapture::imageCaptured(requestId, previewImage)
+    \qmlsignal QtMultimedia::ImageCapture::imageCaptured(requestId, preview)
 
-    This signal is emitted when an image with \a requestId has been captured
-    but not yet saved to the filesystem.  The \a previewImage
+    This signal is emitted when an image with requested \a id has been captured
+    but not yet saved to the filesystem.  The \a preview
     parameter is the captured image.
 
     \sa imageSaved, preview
 */
 
 /*!
-    \qmlsignal QtMultimedia::ImageCapture::imageSaved(requestId, path)
+    \qmlsignal QtMultimedia::ImageCapture::imageSaved(id, fileName)
 
-    This signal is emitted after the image with \a requestId has been written to the filesystem.
-    The \a path is a local file path, not a URL.
+    This signal is emitted after the image with requested \a id has been written to the filesystem.
+    The \a fileName is a local file path, not a URL.
 
     \sa imageCaptured
 */
 
 
 /*!
-    \qmlsignal QtMultimedia::ImageCapture::imageMetadataAvailable(requestId, key, value)
+    \qmlsignal QtMultimedia::ImageCapture::imageMetadataAvailable(id, metaData)
 
-    This signal is emitted when the image with \a requestId has new metadata
-    available with the key \a key and value \a value.
+    This signal is emitted when the image with requested \a id has new \a metaData.
 
     \sa imageCaptured
 */
