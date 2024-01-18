828b4ab8defd8de9d35c80935f79ad9f8d443a56
Mikko Hallamaa <mikko.hallamaa@qt.io>
2024-01-18 16:14:39 +0100

    eglfs: Add QOpenGLVideoBuffer for EGLFS screen capture
    Part of EGLFS screen capture optimizations. Add a dedicated OpenGL video
    buffer class that exposes the FBO texture for rendering, and lazily
    creates an underlying QImageVideoBuffer when mapping on any thread.
    
    Task-number: QTBUG-121835
    Pick-to: 6.7 6.6 6.5
    Change-Id: I49663def4bcb5a5be615e6779e8f7346d6ad4faf
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

---
+++ src/plugins/multimedia/ffmpeg/qopenglvideobuffer_p.h
@@ -0,0 +1,46 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR
+// GPL-3.0-only
+
+#ifndef QOPENGLVIDEOBUFFER_P_H
+#define QOPENGLVIDEOBUFFER_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <private/qabstractvideobuffer_p.h>
+
+QT_BEGIN_NAMESPACE
+
+class QImageVideoBuffer;
+class QOpenGLFramebufferObject;
+
+class QOpenGLVideoBuffer : public QAbstractVideoBuffer
+{
+public:
+    QOpenGLVideoBuffer(std::unique_ptr<QOpenGLFramebufferObject> fbo);
+    ~QOpenGLVideoBuffer();
+
+    QVideoFrame::MapMode mapMode() const override;
+    MapData map(QVideoFrame::MapMode mode) override;
+    void unmap() override;
+    quint64 textureHandle(int plane) const override;
+
+    QImageVideoBuffer &ensureImageBuffer();
+
+private:
+    std::unique_ptr<QOpenGLFramebufferObject> m_fbo;
+    std::unique_ptr<QImageVideoBuffer> m_imageBuffer;
+};
+
+QT_END_NAMESPACE
+
+#endif // QOPENGLVIDEOBUFFER_P_H
