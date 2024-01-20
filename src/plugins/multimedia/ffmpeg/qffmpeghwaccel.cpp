bee1f55109e8c6c324648f8462ced806c271c2bd
Jøger Hansegård <joger.hansegard@qt.io>
2024-01-20 18:07:10 +0100

    Re-enable HW texture conversion on Windows with FFmpeg backend
    To reduce CPU load and improve media playback performance, we can keep
    decoded video frames on the GPU all the way from decoding with FFmpeg
    until the frame is displayed on screen.
    
    This feature has previoulsy been disabled by default on Windows, because
    we were facing issues with bumpy video playback when hw texture
    conversion was enabled. Commit 3363496a7f0a0690bc3a2e50172cf6563f198701
    fixes this issue, and we can re-enable the feature.
    
    This patch re-enables hw texture conversion by default with FFmpeg
    backend.
    
    Before staging, this patch must be tested on an AMD GPU, where we
    previously experienced crashes due to texture sharing.
    
    Fixes: QTBUG-115308
    Change-Id: I8d95bebd2566492aa73fed6e2029b56018755396
    Reviewed-by: Pavel Dubsky <pavel.dubsky@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 1e3e0fac0d1c3b0772958bb590be263f7114bd45)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit c6880ff17fb2158f9a1253a1ab6497bb54dab1f7)
    (cherry picked from commit b6138a0f497392d525df3d0333e3a611d6d9f44b)

--- src/plugins/multimedia/ffmpeg/qffmpeghwaccel.cpp
+++ src/plugins/multimedia/ffmpeg/qffmpeghwaccel.cpp
@@ -227,36 +227,20 @@ static bool isNoConversionFormat(AVPixelFormat f)
 
 namespace {
 
-bool hwTextureConversionEnabled(AVPixelFormat fmt)
+bool hwTextureConversionEnabled()
 {
 
     // HW textures conversions are not stable in specific cases, dependent on the hardware and OS.
     // We need the env var for testing with no textures conversion on the user's side.
-    static bool isDisableConversionSet = false;
-    static const int disableHwConversion = qEnvironmentVariableIntValue(
-            "QT_DISABLE_HW_TEXTURES_CONVERSION", &isDisableConversionSet);
+    static const int disableHwConversion =
+            qEnvironmentVariableIntValue("QT_DISABLE_HW_TEXTURES_CONVERSION");
 
-    if (disableHwConversion)
-        return false;
-
-#if QT_CONFIG(wmf)
-    if (fmt == AV_PIX_FMT_D3D11) {
-        // On Windows, HW texture conversion currently causes stuttering video display and possibly
-        // crash on AMD GPUs. See for example QTBUG-113832 and QTBUG-111543. On this platform, HW
-        // texture conversions have to be explicitly enabled for debugging and testing.
-        if (!isDisableConversionSet)
-            return false;
-    }
-#else
-    Q_UNUSED(fmt);
-#endif
-
-    return true;
+    return !disableHwConversion;
 }
 
 void setupDecoder(const AVPixelFormat format, AVCodecContext *const codecContext)
 {
-    if (!hwTextureConversionEnabled(format))
+    if (!hwTextureConversionEnabled())
         return;
 
 #if QT_CONFIG(wmf)
@@ -267,6 +251,7 @@ void setupDecoder(const AVPixelFormat format, AVCodecContext *const codecContext
         QFFmpeg::MediaCodecTextureConverter::setupDecoderSurface(codecContext);
 #else
     Q_UNUSED(codecContext);
+    Q_UNUSED(format);
 #endif
 }
 
@@ -464,7 +449,7 @@ void TextureConverter::updateBackend(AVPixelFormat fmt)
     if (!d->rhi)
         return;
 
-    if (!hwTextureConversionEnabled(fmt))
+    if (!hwTextureConversionEnabled())
         return;
 
     switch (fmt) {
