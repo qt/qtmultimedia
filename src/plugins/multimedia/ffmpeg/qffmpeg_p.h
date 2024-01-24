9e163841042107c1a390427c7656901fa5cd7ce7
Artem Dyomin <artem.dyomin@qt.io>
2024-01-24 21:50:27 +0100

    Rename ResampleAudioFormat to AVAudioFormat and move to a separate file
    * The structure ResampleAudioFormat is to be used not only for
      resampling, AVAudioFormat is a better name.
    * The structure is to be extended, let's have it in a separate file.
    * The new header qffmpegdefs_p.h is also needed to ensure
      a correct headers dependency.
    
    Change-Id: I06fbc4370a40cc5da9a0f680f9c81db0c6506754
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>
    (cherry picked from commit 95aef698fe65d6198e22bd2e9d202f1710d7f05b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 80526e43a7f65f2e83118ba66ec3fa24ff5db2d3)
    (cherry picked from commit 7265a051b0b4ca8e84087b84c78176db8e983c5e)

--- src/plugins/multimedia/ffmpeg/qffmpeg_p.h
+++ src/plugins/multimedia/ffmpeg/qffmpeg_p.h
@@ -3,29 +3,22 @@
 #ifndef QFFMPEG_P_H
 #define QFFMPEG_P_H
 
-#include <private/qtmultimediaglobal_p.h>
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include "qffmpegdefs_p.h"
+#include "qffmpegavaudioformat_p.h"
+
 #include <qstring.h>
 #include <optional>
-
-extern "C" {
-#include <libavformat/avformat.h>
-#include <libavcodec/avcodec.h>
-#include <libswresample/swresample.h>
-#include <libavutil/avutil.h>
-#include <libswscale/swscale.h>
-}
-
-#define QT_FFMPEG_OLD_CHANNEL_LAYOUT (LIBAVCODEC_VERSION_INT < AV_VERSION_INT(59,24,100))
-#define QT_FFMPEG_HAS_VULKAN \
-  (LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(58, 91, 100)) // since ffmpeg n4.3
-#define QT_FFMPEG_HAS_FRAME_TIME_BASE \
-  (LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(59, 18, 100)) // since ffmpeg n5.0
-#define QT_FFMPEG_HAS_FRAME_DURATION \
-  (LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(60, 3, 100)) // since ffmpeg n6.0
-#define QT_FFMPEG_STREAM_SIDE_DATA_DEPRECATED \
-  (LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(60, 15, 100)) // since ffmpeg n6.1
-#define QT_FFMPEG_SWR_CONST_CH_LAYOUT \
-    (LIBSWRESAMPLE_VERSION_INT >= AV_VERSION_INT(4, 9, 100))
 
 inline bool operator==(const AVRational &lhs, const AVRational &rhs)
 {
@@ -38,8 +31,6 @@ inline bool operator!=(const AVRational &lhs, const AVRational &rhs)
 }
 
 QT_BEGIN_NAMESPACE
-
-class QAudioFormat;
 
 namespace QFFmpeg
 {
@@ -225,23 +216,8 @@ AVPixelFormat pixelFormatForHwDevice(AVHWDeviceType deviceType);
 
 const AVPacketSideData *streamSideData(const AVStream *stream, AVPacketSideDataType type);
 
-struct ResampleAudioFormat
-{
-    ResampleAudioFormat(const AVCodecParameters* codecPar);
-
-    ResampleAudioFormat(const QAudioFormat& audioFormat);
-
-#if QT_FFMPEG_OLD_CHANNEL_LAYOUT
-    uint64_t channelLayoutMask;
-#else
-    AVChannelLayout channelLayout;
-#endif
-    AVSampleFormat sampleFormat;
-    int sampleRate;
-};
-
-SwrContextUPtr createResampleContext(const ResampleAudioFormat& inputFormat,
-                                     const ResampleAudioFormat& outputFormat);
+SwrContextUPtr createResampleContext(const AVAudioFormat &inputFormat,
+                                     const AVAudioFormat &outputFormat);
 
 #ifdef Q_OS_DARWIN
 bool isCVFormatSupported(uint32_t format);
