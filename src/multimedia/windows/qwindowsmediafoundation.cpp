e87334c3a98031903cc9cb0c90d998cdc5a1c89a
Jøger Hansegård <joger.hansegard@qt.io>
2024-01-27 18:28:43 +0100

    Simplify QWindowsMediaFoundation singleton implementation
    This change makes it a bit easier to maintain the
    QWindowsMediaFoundation singleton. Since construction of static
    variables are thread safe, also with the Q_GLOBAL_STATIC, we don't need
    mutexes to protect the instance.
    
    Pick-to: 6.6 6.5
    Change-Id: I22511a5acc64f8864b3184c12d0fbebd80e2a3a8
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 73928cc1c2fb38bf3b1c65f11558d1f8e130876c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/multimedia/windows/qwindowsmediafoundation.cpp
+++ src/multimedia/windows/qwindowsmediafoundation.cpp
@@ -2,59 +2,47 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qwindowsmediafoundation_p.h"
-#include "qwindowdefs_win.h"
-#include <QDebug>
-#include <QMutex>
 
 QT_BEGIN_NAMESPACE
 
 namespace {
 
-struct Holder {
-    ~Holder()
-    {
-        QMutexLocker locker(&mutex);
-        instance = nullptr;
-    }
-    bool loadFailed = false;
-    QBasicMutex mutex;
-    std::unique_ptr<QWindowsMediaFoundation> instance;
-};
+Q_GLOBAL_STATIC(QWindowsMediaFoundation, s_wmf);
 
-Q_GLOBAL_STATIC(Holder, holder)
+template <typename T>
+bool setProcAddress(QSystemLibrary &lib, T &f, const char name[])
+{
+    f = reinterpret_cast<T>(lib.resolve(name));
+    return static_cast<bool>(f);
+}
 
 } // namespace
 
-QWindowsMediaFoundation::~QWindowsMediaFoundation() = default;
-
-template<typename T> bool setProcAddress(QSystemLibrary &lib, T &f, std::string_view name)
-{
-    f = reinterpret_cast<T>(lib.resolve(name.data()));
-    return bool(f);
-}
-
 QWindowsMediaFoundation *QWindowsMediaFoundation::instance()
 {
-    QMutexLocker locker(&holder->mutex);
-    if (holder->instance)
-        return holder->instance.get();
+    if (s_wmf->valid())
+        return s_wmf;
 
-    if (holder->loadFailed)
-        return nullptr;
-
-    std::unique_ptr<QWindowsMediaFoundation> wmf(new QWindowsMediaFoundation);
-    if (wmf->m_mfplat.load(false)) {
-        if (setProcAddress(wmf->m_mfplat, wmf->mfCreateMediaType, "MFCreateMediaType")
-            && setProcAddress(wmf->m_mfplat, wmf->mfCreateMemoryBuffer, "MFCreateMemoryBuffer")
-            && setProcAddress(wmf->m_mfplat, wmf->mfCreateSample, "MFCreateSample"))
-        {
-            holder->instance = std::move(wmf);
-            return holder->instance.get();
-        }
-    }
-
-    holder->loadFailed = true;
     return nullptr;
+}
+
+QWindowsMediaFoundation::QWindowsMediaFoundation()
+{
+    if (!m_mfplat.load(false))
+        return;
+
+    m_valid = setProcAddress(m_mfplat, mfCreateMediaType, "MFCreateMediaType")
+            && setProcAddress(m_mfplat, mfCreateMemoryBuffer, "MFCreateMemoryBuffer")
+            && setProcAddress(m_mfplat, mfCreateSample, "MFCreateSample");
+
+    Q_ASSERT(m_valid); // If it is not valid at this point, we have a programming bug
+}
+
+QWindowsMediaFoundation::~QWindowsMediaFoundation() = default;
+
+bool QWindowsMediaFoundation::valid() const
+{
+    return m_valid;
 }
 
 QT_END_NAMESPACE
