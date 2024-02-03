1acb307a20c85ed43286cfcbb8410b5657ab01ca
Artem Dyomin <artem.dyomin@qt.io>
2024-02-04 00:28:44 +0100

    Move QVideoFramePrivate to a separate file
    The patch is need for following improvements.
    
    Task-number: QTBUG-121793
    Change-Id: I3ed128fc7059d7321af1e6f83bfade6c56cccad9
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit ab3e26a39c41cf389ad153d4aedec59903776f73)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 227e976f2921c51e360287b569aeff30fef29fa3)
    (cherry picked from commit 4f4c578bbd8dd58d077c003ad55502ff889a20e2)

---
+++ src/multimedia/video/qvideoframe_p.h
@@ -0,0 +1,53 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QVIDEOFRAME_P_H
+#define QVIDEOFRAME_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include "qabstractvideobuffer_p.h"
+#include "qshareddata.h"
+#include "private/qtvideo_p.h"
+#include <qmutex.h>
+#include <mutex> // std::once
+
+QT_BEGIN_NAMESPACE
+
+class QVideoFramePrivate : public QSharedData
+{
+public:
+    QVideoFramePrivate() = default;
+    QVideoFramePrivate(const QVideoFrameFormat &format) : format(format) { }
+
+    ~QVideoFramePrivate() { delete buffer; }
+
+    qint64 startTime = -1;
+    qint64 endTime = -1;
+    QAbstractVideoBuffer::MapData mapData;
+    QVideoFrameFormat format;
+    QAbstractVideoBuffer *buffer = nullptr;
+    int mappedCount = 0;
+    QMutex mapMutex;
+    QString subtitleText;
+    QtVideo::Rotation rotation = QtVideo::Rotation::None;
+    bool mirrored = false;
+    QImage image;
+    std::once_flag imageOnceFlag;
+
+private:
+    Q_DISABLE_COPY(QVideoFramePrivate)
+};
+
+QT_END_NAMESPACE
+
+#endif // QVIDEOFRAMEPRIVATE_P_H
