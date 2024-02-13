00d9e06d6a8f6533908afb466477e34faca92934
Tim Blechmann <tim@klingt.org>
2024-02-13 12:09:19 +0800

    gstreamer: fix EGL dependencies
    EGL is only required for FEATURE_gstreamer_gl. Re-shuffle code to
    reflect the dependency properly
    
    Change-Id: I1b81e210ef0775f0ae9b7d3285036591cc4bbad2
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Lars Sutterud <lars.sutterud@qt.io>
    Reviewed-by: Mikko Hallamaa <mikko.hallamaa@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit e8375a4992937603ab586cc3b0ec8eae7c4ef9f1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e2ed0863c32a969e8738d1a3bb6965aa5d51c4be)
    (cherry picked from commit ff153c538a06c85d546a64b5d3ab615e1c1b87b6)

--- src/plugins/multimedia/gstreamer/common/qgstvideobuffer.cpp
+++ src/plugins/multimedia/gstreamer/common/qgstvideobuffer.cpp
@@ -15,21 +15,21 @@
 #include "qgstutils_p.h"
 
 #if QT_CONFIG(gstreamer_gl)
-#include <QtGui/private/qrhi_p.h>
-#include <QtGui/private/qrhigles2_p.h>
-#include <QtGui/qopenglcontext.h>
-#include <QtGui/qopenglfunctions.h>
-#include <QtGui/qopengl.h>
+#  include <QtGui/private/qrhigles2_p.h>
+#  include <QtGui/qopenglcontext.h>
+#  include <QtGui/qopenglfunctions.h>
+#  include <QtGui/qopengl.h>
 
-#include <gst/gl/gstglconfig.h>
-#include <gst/gl/gstglmemory.h>
-#include <gst/gl/gstglsyncmeta.h>
-#if QT_CONFIG(linux_dmabuf)
-#include <gst/allocators/gstdmabuf.h>
-#endif
+#  include <gst/gl/gstglconfig.h>
+#  include <gst/gl/gstglmemory.h>
+#  include <gst/gl/gstglsyncmeta.h>
 
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
+#  include <EGL/egl.h>
+#  include <EGL/eglext.h>
+
+#  if QT_CONFIG(linux_dmabuf)
+#    include <gst/allocators/gstdmabuf.h>
+#  endif
 #endif
 
 QT_BEGIN_NAMESPACE
@@ -67,6 +67,10 @@ QGstVideoBuffer::QGstVideoBuffer(GstBuffer *buffer, const GstVideoInfo &info, QG
         eglDisplay =  sink->eglDisplay();
         eglImageTargetTexture2D = sink->eglImageTargetTexture2D();
     }
+
+#if !QT_CONFIG(gstreamer_gl)
+    Q_UNUSED(memoryFormat);
+#endif
 }
 
 QGstVideoBuffer::~QGstVideoBuffer()
