0d5ac7423d250ffd6fcef5570f35b16506ea39ce
Artem Dyomin <artem.dyomin@qt.io>
2024-03-10 09:32:46 +0100

    Move QMaybe to a separate file
    Let's keep qmultimediautils_p.h clean.
    
    Pick-to: 6.7 6.6 6.5
    Change-Id: Ibe10de86da614f94153b89a054b8975b1ea34ade
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- src/multimedia/qmultimediautils_p.h
+++ src/multimedia/qmultimediautils_p.h
@@ -17,83 +17,12 @@
 
 #include <QtMultimedia/qtmultimediaglobal.h>
 #include <QtMultimedia/qtvideo.h>
-#include <QtCore/private/qglobal_p.h>
-#include <qstring.h>
+#include <QtMultimedia/private/qmaybe_p.h>
 #include <qsize.h>
-#include <utility>
-#include <optional>
 
 QT_BEGIN_NAMESPACE
 
 class QVideoFrame;
-
-struct QUnexpect
-{
-};
-
-static constexpr QUnexpect unexpect{};
-
-template<typename Value, typename Error = QString>
-class QMaybe
-{
-public:
-    QMaybe(const Value &v)
-    {
-        if constexpr (std::is_pointer_v<Value>) {
-            if (!v)
-                return; // nullptr is treated as nullopt (for raw pointer types only)
-        }
-        m_value = v;
-    }
-
-    QMaybe(Value &&v)
-    {
-        if constexpr (std::is_pointer_v<Value>) {
-            if (!v)
-                return; // nullptr is treated as nullopt (for raw pointer types only)
-        }
-        m_value = std::move(v);
-    }
-
-    QMaybe(const QMaybe &other) = default;
-
-    QMaybe &operator=(const QMaybe &other) = default;
-
-    QMaybe(const Error& error) : m_error(error) { }
-
-    template<class... Args>
-    QMaybe(QUnexpect, Args &&...args) : m_error{ std::forward<Args>(args)... }
-    {
-        static_assert(std::is_constructible_v<Error, Args &&...>,
-                      "Invalid arguments for creating an error type");
-    }
-
-    constexpr explicit operator bool() const noexcept { return m_value.has_value(); }
-
-    constexpr Value &value()
-    {
-        Q_ASSERT(m_value.has_value());
-        return *m_value;
-    }
-
-    constexpr const Value &value() const
-    {
-        Q_ASSERT(m_value.has_value());
-        return *m_value;
-    }
-
-    constexpr Value *operator->() noexcept { return &value(); }
-    constexpr const Value *operator->() const noexcept { return &value(); }
-
-    constexpr Value &operator*() & noexcept { return value(); }
-    constexpr const Value &operator*() const & noexcept { return value(); }
-
-    constexpr const Error &error() const { return m_error; }
-
-private:
-    std::optional<Value> m_value;
-    Error m_error;
-};
 
 struct Fraction {
     int numerator;
