e21ee7aa4d4fe2f84a9c578bc464407a299b2166
Artem Dyomin <artem.dyomin@qt.io>
2024-03-11 15:50:03 +0100

    Move TestVideoSink to the common folder
    TestVideoSink will be reused by other tests, let's have it in
    the folder 'shared'.
    
    Pick-to: 6.6 6.5
    Change-Id: I59e1576aab6eb984f070de0aca30ae95f25db74c
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Tim Blechmann <tim@klingt.org>
    Reviewed-by: Mikko Hallamaa <mikko.hallamaa@qt.io>
    (cherry picked from commit 0cebd440ed8f9a7e2694ed2734f41030c67b5403)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ tests/auto/integration/shared/testvideosink.h
@@ -0,0 +1,62 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#ifndef TESTVIDEOSINK_H
+#define TESTVIDEOSINK_H
+
+#include <qvideosink.h>
+#include <qvideoframe.h>
+#include <qelapsedtimer.h>
+#include <qsignalspy.h>
+
+QT_BEGIN_NAMESPACE
+
+/*
+    This is a simple video surface which records all presented frames.
+*/
+class TestVideoSink : public QVideoSink
+{
+    Q_OBJECT
+public:
+    explicit TestVideoSink(bool storeFrames = false) : m_storeFrames(storeFrames)
+    {
+        connect(this, &QVideoSink::videoFrameChanged, this, &TestVideoSink::addVideoFrame);
+        connect(this, &QVideoSink::videoFrameChanged, this, &TestVideoSink::videoFrameChangedSync);
+    }
+
+    QVideoFrame waitForFrame()
+    {
+        QSignalSpy spy(this, &TestVideoSink::videoFrameChangedSync);
+        return spy.wait() ? spy.at(0).at(0).value<QVideoFrame>() : QVideoFrame{};
+    }
+
+    void setStoreFrames(bool storeFrames = true) { m_storeFrames = storeFrames; }
+
+private Q_SLOTS:
+    void addVideoFrame(const QVideoFrame &frame)
+    {
+        if (!m_elapsedTimer.isValid())
+            m_elapsedTimer.start();
+        else
+            m_elapsedTimer.restart();
+
+        if (m_storeFrames)
+            m_frameList.append(frame);
+        ++m_totalFrames;
+    }
+
+signals:
+    void videoFrameChangedSync(const QVideoFrame &frame);
+
+public:
+    QList<QVideoFrame> m_frameList;
+    int m_totalFrames = 0; // used instead of the list when frames are not stored
+    QElapsedTimer m_elapsedTimer;
+
+private:
+    bool m_storeFrames;
+};
+
+QT_END_NAMESPACE
+
+#endif // TESTVIDEOSINK_H
