1a3e559799a9dbb62a335fa3a06897f61841a87e
Tim Blechmann <tim@klingt.org>
2024-03-13 10:04:31 +0800

    tests: add minimal cpp player
    Change-Id: If4b7a2f7778b67e43ead1f4bbf671b3db46d942d
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    (cherry picked from commit 1b5cdf65f83d9ccdd019b9d163056c1abd8d0e02)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d5acfb2102eb9522dd54145beed7fbb29cb58590)
    (cherry picked from commit cabce3f954c7e3e2c5a6bd5a86e0aeb8b12c962e)

---
+++ tests/manual/minimal-player/minimal-player.cpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+#include <QApplication>
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+#include <QtMultimedia/QMediaPlayer>
+#include <QtMultimedia/QAudioOutput>
+#include <QtMultimediaWidgets/QVideoWidget>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    QMediaPlayer player;
+    QVideoWidget widget;
+    QAudioOutput audioOutput;
+    player.setVideoOutput(&widget);
+    player.setAudioOutput(&audioOutput);
+
+    if (QApplication::arguments().size() > 1) {
+        player.setSource(QApplication::arguments()[1]);
+    } else {
+        qInfo() << "Please specify a video source";
+        return 0;
+    }
+
+    widget.show();
+    player.play();
+    return QApplication::exec();
+}
