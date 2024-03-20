a773a4fc5fe30770e5d6049de36964824e095c2b
Tim Blechmann <tim@klingt.org>
2024-03-20 11:18:42 +0800

    tests: add manual test for GStreamer's RTP stream receiver
    Task-number: QTBUG-112999
    Change-Id: I588ad68fdcf53fa3d6f1475b5128e7aa0ef8ed19
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 3ffe29e4603841acf36a66ab5d989470938e0d12)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 644e012377f69bf0b00198987fd0c7eefe7a1ac0)
    (cherry picked from commit e598ecab5473d74ee875b80ec6624018f8c2d429)

---
+++ tests/manual/qml-gstreamer-rtp/qml-gstreamer-rtp.cpp
@@ -0,0 +1,32 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+#include <QGuiApplication>
+#include <QProcess>
+#include <QQmlApplicationEngine>
+#include <QtEnvironmentVariables>
+
+int main(int argc, char *argv[])
+{
+    qputenv("QT_MEDIA_BACKEND", "gstreamer");
+
+    QProcess process;
+    process.startCommand(
+            "gst-launch-1.0 videotestsrc ! x264enc ! udpsink host=127.0.0.1 port=50004");
+
+    auto scopeGuard = qScopeGuard([&] {
+        process.terminate();
+        process.waitForFinished();
+    });
+
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
+    process.waitForStarted();
+
+    engine.load(QUrl("qrc:/qml-gstreamer-rtp.qml"));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
