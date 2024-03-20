832ce83750bb870d8433c44c5a0d7e6f18e9c513
Lucie Gérard <lucie.gerard@qt.io>
2024-03-20 15:19:57 +0100

    Get SPDX license tag on one line only
    If spread on two lines, the license test does not read the entire tag
    
    Pick-to: 6.7
    Task-number: QTBUG-121787
    Change-Id: I2703440020b36ea2838034d50a7e03aa2404fc4a
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>

--- src/plugins/multimedia/ffmpeg/qffmpegscreencapture_dxgi.cpp
+++ src/plugins/multimedia/ffmpeg/qffmpegscreencapture_dxgi.cpp
@@ -1,6 +1,5 @@
 // Copyright (C) 2021 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR
-// GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qffmpegscreencapture_dxgi_p.h"
 #include "qffmpegsurfacecapturegrabber_p.h"
