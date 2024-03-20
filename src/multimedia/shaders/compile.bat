291bc6a73ef105cbfd0dede49f37479e1ffeaa51
Lucie Gérard <lucie.gerard@qt.io>
2024-03-20 15:39:56 +0100

    Change license for tool files
    According to QUIP-18 [1], all tool files should be
    LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Task-number: QTBUG-121787
    Change-Id: Iaf62416538ec630520630d84c588d19cdb4c5ddf
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    (cherry picked from commit 0204bf0c1a5ee1022a4b3b8c1a2289d503839ffa)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/multimedia/shaders/compile.bat
+++ src/multimedia/shaders/compile.bat
@@ -1,5 +1,5 @@
 :: Copyright (C) 2019 The Qt Company Ltd.
-:: SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+:: SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 qsb -b --glsl "150,120,100 es" --hlsl 50 --msl 12 -o rgba.vert.qsb rgba.vert
 qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o rgba.frag.qsb rgba.frag
