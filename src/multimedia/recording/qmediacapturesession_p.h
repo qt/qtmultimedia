85c41da64717fca14d58b15c1f35beb52791fd24
Tim Blechmann <tim@klingt.org>
2024-03-29 08:09:49 +0800

    GStreamer: add private API to access pipeline for capture session
    Allows users to mess with the underlying gstreamer pipeline of the
    `QMediaCaptureSession` like they can with the `QMediaPlayer`
    
    [ChangeLog] QMediaCaptureSession: GStreamer - private interface to
    access underlying GstPipeline of QMediaCaptureSession
    
    Pick-to: 6.6 6.5
    Task-number: QTBUG-123830
    Change-Id: I5b17240c9951ee8e05dd6cb4a78b203a56794922
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    (cherry picked from commit d27311be4a07051e0fd17b0535ea378c29e464e8)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/multimedia/recording/qmediacapturesession_p.h
@@ -0,0 +1,44 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QMEDIACAPTURESESSION_P_H
+#define QMEDIACAPTURESESSION_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtMultimedia/qmediacapturesession.h>
+
+#include <QtCore/qpointer.h>
+
+QT_BEGIN_NAMESPACE
+
+class QMediaCaptureSessionPrivate
+{
+public:
+    QMediaCaptureSession *q = nullptr;
+    QPlatformMediaCaptureSession *captureSession = nullptr;
+    QAudioInput *audioInput = nullptr;
+    QAudioOutput *audioOutput = nullptr;
+    QPointer<QCamera> camera;
+    QPointer<QScreenCapture> screenCapture;
+    QPointer<QWindowCapture> windowCapture;
+    QPointer<QImageCapture> imageCapture;
+    QPointer<QMediaRecorder> recorder;
+    QPointer<QVideoSink> videoSink;
+    QPointer<QObject> videoOutput;
+
+    void setVideoSink(QVideoSink *sink);
+};
+
+QT_END_NAMESPACE
+
+#endif // QMEDIACAPTURESESSION_P_H
