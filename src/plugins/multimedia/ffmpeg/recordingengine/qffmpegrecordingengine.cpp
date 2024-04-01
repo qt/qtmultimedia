0e9a4bc4c83f4c4d1597dbd65e2a54d52a432edc
Jøger Hansegård <joger.hansegard@qt.io>
2024-04-01 16:23:00 +0200

    Move FFmpeg VideoEncoder to its own files
    This helps readability because qffmpegvideoencoder_p.h file is quite
    big and contains multiple classes.
    
    Task-number: QTBUG-121792
    Pick-to: 6.6 6.5
    Change-Id: Ia7295daa879375547e0b4db37c270f9aad768fc3
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit ba1da65044863c79a23bb21ea0126de572d917cd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/multimedia/ffmpeg/recordingengine/qffmpegrecordingengine.cpp
+++ src/plugins/multimedia/ffmpeg/recordingengine/qffmpegrecordingengine.cpp
@@ -12,6 +12,7 @@
 #include <private/qplatformcamera_p.h>
 #include <private/qplatformvideosource_p.h>
 #include "qffmpegvideobuffer_p.h"
+#include "qffmpegvideoencoder_p.h"
 #include "qffmpegmediametadata_p.h"
 #include "qffmpegmuxer_p.h"
 #include <qloggingcategory.h>
@@ -177,92 +178,6 @@ void RecordingEngine::addMediaFrameHandler(Args &&...args)
 {
     auto connection = connect(std::forward<Args>(args)..., Qt::DirectConnection);
     m_connections.append(connection);
-}
-
-VideoEncoder::VideoEncoder(RecordingEngine *encoder, const QMediaEncoderSettings &settings,
-                           const QVideoFrameFormat &format, std::optional<AVPixelFormat> hwFormat)
-    : EncoderThread(encoder)
-{
-    setObjectName(QLatin1String("VideoEncoder"));
-
-    AVPixelFormat swFormat = QFFmpegVideoBuffer::toAVPixelFormat(format.pixelFormat());
-    AVPixelFormat ffmpegPixelFormat =
-            hwFormat && *hwFormat != AV_PIX_FMT_NONE ? *hwFormat : swFormat;
-    auto frameRate = format.frameRate();
-    if (frameRate <= 0.) {
-        qWarning() << "Invalid frameRate" << frameRate << "; Using the default instead";
-
-        // set some default frame rate since ffmpeg has UB if it's 0.
-        frameRate = 30.;
-    }
-
-    m_frameEncoder =
-            VideoFrameEncoder::create(settings, format.frameSize(), frameRate, ffmpegPixelFormat,
-                                      swFormat, encoder->avFormatContext());
-}
-
-VideoEncoder::~VideoEncoder() = default;
-
-bool VideoEncoder::isValid() const
-{
-    return m_frameEncoder != nullptr;
-}
-
-void VideoEncoder::addFrame(const QVideoFrame &frame)
-{
-    QMutexLocker locker(&m_queueMutex);
-
-    // Drop frames if encoder can not keep up with the video source data rate
-    const bool queueFull = m_videoFrameQueue.size() >= m_maxQueueSize;
-
-    if (queueFull) {
-        qCDebug(qLcFFmpegEncoder) << "RecordingEngine frame queue full. Frame lost.";
-    } else if (!m_paused.loadRelaxed()) {
-        m_videoFrameQueue.push(frame);
-
-        locker.unlock(); // Avoid context switch on wake wake-up
-
-        dataReady();
-    }
-}
-
-QVideoFrame VideoEncoder::takeFrame()
-{
-    QMutexLocker locker(&m_queueMutex);
-    return dequeueIfPossible(m_videoFrameQueue);
-}
-
-void VideoEncoder::retrievePackets()
-{
-    if (!m_frameEncoder)
-        return;
-    while (auto packet = m_frameEncoder->retrievePacket())
-        m_encoder->m_muxer->addPacket(std::move(packet));
-}
-
-void VideoEncoder::init()
-{
-    qCDebug(qLcFFmpegEncoder) << "VideoEncoder::init started video device thread.";
-    bool ok = m_frameEncoder->open();
-    if (!ok)
-        emit m_encoder->error(QMediaRecorder::ResourceError, "Could not initialize encoder");
-}
-
-void VideoEncoder::cleanup()
-{
-    while (!m_videoFrameQueue.empty())
-        processOne();
-    if (m_frameEncoder) {
-        while (m_frameEncoder->sendFrame(nullptr) == AVERROR(EAGAIN))
-            retrievePackets();
-        retrievePackets();
-    }
-}
-
-bool VideoEncoder::hasData() const
-{
-    QMutexLocker locker(&m_queueMutex);
-    return !m_videoFrameQueue.empty();
 }
 
 struct QVideoFrameHolder
