5d582987a14d6a0ca6707e1c99f076ebd973036e
Lucie Gérard <lucie.gerard@qt.io>
2024-04-04 13:06:12 +0200

    Correct license for build system files
    According to QUIP-18 [1], all build system
    files should be BSD-3-Clause
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Pick-to: 6.7
    Task-number: QTBUG-121787
    Change-Id: Ia7e7a25d1570ad2af423ca558bc6c4915a1d1869
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>

--- config.tests/gpu_vivante/main.cpp
+++ config.tests/gpu_vivante/main.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// SPDX-License-Identifier: BSD-3-Clause
 
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
