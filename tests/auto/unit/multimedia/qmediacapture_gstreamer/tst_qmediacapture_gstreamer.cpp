17643efb77aef199b58c6c9efbfe439755f013cb
Tim Blechmann <tim@klingt.org>
2024-04-05 18:37:02 +0800

    GStreamer: custom audio io - introduce factory functions
    We provide factory functions to allow users to inject custom elements
    as source/sinks
    
    [ChangeLog] QAudioInput/QAudioOutput: GStreamer - add private API to
    customize audio input/output via a pipeline string.
    
    Pick-to: 6.5
    Change-Id: I75304b66ea8ea01c551df89bc7861c76d571dc32
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit a42df322e92d28d6c3d48d39472e52b75b930d33)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/unit/multimedia/qmediacapture_gstreamer/tst_qmediacapture_gstreamer.cpp
+++ tests/auto/unit/multimedia/qmediacapture_gstreamer/tst_qmediacapture_gstreamer.cpp
@@ -3,12 +3,19 @@
 
 #include <QtTest/QtTest>
 #include <QtMultimedia/QMediaCaptureSession>
+#include <QtMultimedia/QAudioInput>
+#include <QtMultimedia/QAudioOutput>
 #include <QtMultimedia/private/qplatformmediacapture_p.h>
 #include <QtQGstreamerMediaPlugin/private/qgstpipeline_p.h>
+#include <QtQGstreamerMediaPlugin/private/qgstreameraudiodevice_p.h>
+
+#include <qscopedenvironmentvariable.h>
 
 #include <memory>
 
 QT_USE_NAMESPACE
+
+using namespace Qt::Literals;
 
 class tst_QMediaCaptureGStreamer : public QObject
 {
@@ -23,6 +30,8 @@ public slots:
 
 private slots:
     void constructor_preparesGstPipeline();
+    void audioInput_customAudioDevice_fromPipelineDescription();
+    void audioOutput_customAudioDevice_fromPipelineDescription();
 
 private:
     std::unique_ptr<QMediaCaptureSession> session;
@@ -31,6 +40,14 @@ private:
     {
         return reinterpret_cast<GstPipeline *>(
                 QPlatformMediaCaptureSession::nativePipeline(session.get()));
+    }
+
+    QGstPipeline getPipeline()
+    {
+        return QGstPipeline{
+            getGstPipeline(),
+            QGstPipeline::NeedsRef,
+        };
     }
 
     void dumpGraph(const char *fileNamePrefix)
@@ -68,6 +85,55 @@ void tst_QMediaCaptureGStreamer::constructor_preparesGstPipeline()
     QVERIFY(pipeline);
 
     dumpGraph("constructor_preparesGstPipeline");
+}
+
+void tst_QMediaCaptureGStreamer::audioInput_customAudioDevice_fromPipelineDescription()
+{
+    auto pipelineString =
+            "audiotestsrc wave=2 freq=200 name=myOscillator ! identity name=myConverter"_ba;
+
+    QAudioInput input{
+        qMakeCustomGStreamerAudioInput(pipelineString),
+    };
+    session->setAudioInput(&input);
+
+    QGstPipeline pipeline = getPipeline();
+    QTEST_ASSERT(pipeline);
+
+    pipeline.finishStateChange();
+
+    QVERIFY(pipeline.findByName("myOscillator"));
+    QVERIFY(pipeline.findByName("myConverter"));
+
+    dumpGraph("audioInput_customAudioDevice");
+}
+
+void tst_QMediaCaptureGStreamer::audioOutput_customAudioDevice_fromPipelineDescription()
+{
+    auto pipelineStringInput =
+            "audiotestsrc wave=2 freq=200 name=myOscillator ! identity name=myConverter"_ba;
+
+    QAudioInput input{
+        qMakeCustomGStreamerAudioInput(pipelineStringInput),
+    };
+    session->setAudioInput(&input);
+
+    auto pipelineStringOutput = "identity name=myConverter ! fakesink name=mySink"_ba;
+
+    QAudioOutput output{
+        qMakeCustomGStreamerAudioOutput(pipelineStringOutput),
+    };
+    session->setAudioOutput(&output);
+
+    QGstPipeline pipeline = getPipeline();
+    QTEST_ASSERT(pipeline);
+
+    pipeline.finishStateChange();
+
+    QVERIFY(pipeline.findByName("mySink"));
+    QVERIFY(pipeline.findByName("myConverter"));
+
+    dumpGraph("audioOutput_customAudioDevice");
 }
 
 QTEST_GUILESS_MAIN(tst_QMediaCaptureGStreamer)
