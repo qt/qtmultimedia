1c0716eceeaaf0a0217b5ffb40f502500742284e
Tim Blechmann <tim@klingt.org>
2024-04-11 09:11:26 +0800

    GStreamer: metadata - reduce complexity for metadata lookup
    Instead of doing a linear search, we can do a binary search in a
    constexpr array. Reducing O(N) to O(log(N)).
    
    Pick-to: 6.5
    Change-Id: I5bdc42bfc73dc9993e80c7352d64e54bbffe9a28
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by:  Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit f0dca46cdcc406ade7b06f765a64c8522bd9c84b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ tests/auto/unit/multimedia/gstreamer_backend/tst_gstreamer_backend.h
@@ -0,0 +1,24 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#ifndef TST_GSTREAMER_BACKEND_H
+#define TST_GSTREAMER_BACKEND_H
+
+#include <QtTest/QtTest>
+
+#include <QtQGstreamerMediaPlugin/private/qgstreamerintegration_p.h>
+
+QT_USE_NAMESPACE
+
+class tst_GStreamer : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void metadata_fromGstTagList();
+
+private:
+    QGstreamerIntegration integration;
+};
+
+#endif // TST_GSTREAMER_BACKEND_H
