271afce00b6dc7e060331a1cd7d3370cfb005f02
Tim Blechmann <tim@klingt.org>
2024-04-22 08:38:57 +0800

    Tests: Fix use after scope
    `videoFrameChanged` can be called from `~QMediaPlayer`, causing
    stack-use-after-scope address sanitizer errors. We need to make sure
    that the counters outlive the QMediaPlayer.
    
    Change-Id: I05d9d225a8f5a4b8b6c8c1bf120d93d070e7fc1b
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    (cherry picked from commit 2713dac55412a49f79ddcec20efd667d11b3427c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 58b2e97b3aadd77f0a5e107b64e375665f6f89f9)

--- tests/auto/integration/qmediaplayerbackend/tst_qmediaplayerbackend.cpp
+++ tests/auto/integration/qmediaplayerbackend/tst_qmediaplayerbackend.cpp
@@ -2810,6 +2810,9 @@ void tst_QMediaPlayerBackend::lazyLoadVideo()
 
 void tst_QMediaPlayerBackend::videoSinkSignals()
 {
+    std::atomic<int> videoFrameCounter = 0;
+    std::atomic<int> videoSizeCounter = 0;
+
     // TODO: come up with custom frames source,
     // create the test target tst_QVideoSinkBackend,
     // and move the test there
@@ -2819,9 +2822,6 @@ void tst_QMediaPlayerBackend::videoSinkSignals()
     QVideoSink sink;
     QMediaPlayer player;
     player.setVideoSink(&sink);
-
-    std::atomic<int> videoFrameCounter = 0;
-    std::atomic<int> videoSizeCounter = 0;
 
     player.setSource(*m_localVideoFile2);
 
