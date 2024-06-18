220711c2dc68d92416a2fe7bb6a645f7af3c92c8
Tim Blechmann <tim.blechmann@qt.io>
2024-06-18 12:14:18 +0800

    GStreamer: QGstPipeline - add logging category
    It's useful to be able to trace pipeline.
    
    Change-Id: I9976d04dd0c70288b24822992ee1b216fec774cc
    Reviewed-by: Mikko Hallamaa <mikko.hallamaa@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 928ee785c038b5faeaa6acd5bc0034770b31947b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 85967c5b9d0048a130c68ae29deb590609b62ecd)
    (cherry picked from commit bd0fd781238ecbeb4f4b1a96baf6e3b75576b3c0)
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>

--- src/plugins/multimedia/gstreamer/common/qgstpipeline.cpp
+++ src/plugins/multimedia/gstreamer/common/qgstpipeline.cpp
@@ -1,18 +1,21 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
-#include <QtCore/qmap.h>
-#include <QtCore/qtimer.h>
-#include <QtCore/qmutex.h>
-#include <QtCore/qlist.h>
 #include <QtCore/qabstracteventdispatcher.h>
 #include <QtCore/qcoreapplication.h>
+#include <QtCore/qlist.h>
+#include <QtCore/qloggingcategory.h>
+#include <QtCore/qmap.h>
+#include <QtCore/qmutex.h>
 #include <QtCore/qproperty.h>
+#include <QtCore/qtimer.h>
 
 #include "qgstpipeline_p.h"
 #include "qgstreamermessage_p.h"
 
 QT_BEGIN_NAMESPACE
+
+static Q_LOGGING_CATEGORY(qLcGstPipeline, "qt.multimedia.gstpipeline");
 
 static constexpr GstSeekFlags rateChangeSeekFlags =
 #if GST_CHECK_VERSION(1, 18, 0)
@@ -290,6 +293,8 @@ void QGstPipeline::seek(std::chrono::nanoseconds pos, double rate)
     // always adjust the rate, so it can be set before playback starts
     // setting position needs a loaded media file that's seekable
 
+    qCDebug(qLcGstPipeline) << "QGstPipeline::seek to" << pos.count() << "rate:" << rate;
+
     bool success = (rate > 0)
             ? gst_element_seek(element(), rate, GST_FORMAT_TIME, GST_SEEK_FLAG_FLUSH,
                                GST_SEEK_TYPE_SET, pos.count(), GST_SEEK_TYPE_END, 0)
@@ -306,6 +311,7 @@ void QGstPipeline::seek(std::chrono::nanoseconds pos, double rate)
 
 void QGstPipeline::seek(std::chrono::nanoseconds pos)
 {
+    qCDebug(qLcGstPipeline) << "QGstPipeline::seek to" << pos.count();
     seek(pos, getPrivate()->m_rate);
 }
 
@@ -316,6 +322,8 @@ void QGstPipeline::setPlaybackRate(double rate)
         return;
 
     d->m_rate = rate;
+
+    qCDebug(qLcGstPipeline) << "QGstPipeline::setPlaybackRate to" << rate;
 
     applyPlaybackRate(/*instantRateChange =*/true);
 }
@@ -333,6 +341,7 @@ void QGstPipeline::applyPlaybackRate(bool instantRateChange)
     // do not GST_SEEK_FLAG_FLUSH with GST_SEEK_TYPE_NONE
     // https://gitlab.freedesktop.org/gstreamer/gstreamer/-/issues/3604
     if (instantRateChange && GST_CHECK_VERSION(1, 18, 0)) {
+        qCDebug(qLcGstPipeline) << "QGstPipeline::applyPlaybackRate instantly";
         bool success = gst_element_seek(
                 element(), d->m_rate, GST_FORMAT_UNDEFINED, rateChangeSeekFlags, GST_SEEK_TYPE_NONE,
                 GST_CLOCK_TIME_NONE, GST_SEEK_TYPE_NONE, GST_CLOCK_TIME_NONE);
@@ -352,10 +361,13 @@ std::chrono::nanoseconds QGstPipeline::position() const
 {
     QGstPipelinePrivate *d = getPrivate();
     std::optional<std::chrono::nanoseconds> pos = QGstElement::position();
-    if (pos)
+    if (pos) {
         d->m_position = *pos;
-    else
+        qCDebug(qLcGstPipeline) << "QGstPipeline::position:"
+                                << std::chrono::round<std::chrono::milliseconds>(*pos).count();
+    } else {
         qDebug() << "QGstPipeline: failed to query position, using previous position";
+    }
 
     return d->m_position;
 }
