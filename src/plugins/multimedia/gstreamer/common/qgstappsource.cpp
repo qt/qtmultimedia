04087892f0354a4fb2975ace6a0009b27d1d6f6e
Tim Blechmann <tim.blechmann@qt.io>
2024-06-18 14:08:48 +0800

    GStreamer: QGstAppSource - remove dead code
    `QGstAppSource` had a lot of dead code. Removing this code makes it
    easier to reason about it.
    
    Pick-to: 6.7 6.5
    Change-Id: I3289dbc1691fdca59fa7a437096cb8bc6bf91c0a
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 005df040a0cd0c429ab3d69182b2baa19d5ccca0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/multimedia/gstreamer/common/qgstappsource.cpp
+++ src/plugins/multimedia/gstreamer/common/qgstappsource.cpp
@@ -1,12 +1,12 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#include <QDebug>
-
 #include "qgstappsource_p.h"
+
+#include <QtCore/qdebug.h>
+#include <QtCore/qloggingcategory.h>
+
 #include <common/qgstutils_p.h>
-#include "qnetworkreply.h"
-#include "qloggingcategory.h"
 
 static Q_LOGGING_CATEGORY(qLcAppSrc, "qt.multimedia.appsrc")
 
@@ -53,7 +53,6 @@ bool QGstAppSource::setup(QIODevice *stream, qint64 offset)
 
     GstAppSrc *appSrc = m_appSrc.appSrc();
     m_maxBytes = gst_app_src_get_max_bytes(appSrc);
-    m_suspended = false;
 
     if (m_sequential)
         m_streamType = GST_APP_STREAM_TYPE_STREAM;
@@ -62,23 +61,7 @@ bool QGstAppSource::setup(QIODevice *stream, qint64 offset)
     gst_app_src_set_stream_type(appSrc, m_streamType);
     gst_app_src_set_size(appSrc, m_sequential ? -1 : m_stream->size() - m_offset);
 
-    m_noMoreData = true;
-
     return true;
-}
-
-void QGstAppSource::setAudioFormat(const QAudioFormat &f)
-{
-    QMutexLocker locker(&m_mutex);
-
-    m_format = f;
-    if (!m_format.isValid())
-        return;
-
-    auto caps = QGstUtils::capsForAudioFormat(m_format);
-    Q_ASSERT(!caps.isNull());
-    m_appSrc.set("caps", caps);
-    m_appSrc.set("format", GST_FORMAT_TIME);
 }
 
 void QGstAppSource::setExternalAppSrc(QGstAppSrc appsrc)
@@ -98,7 +81,6 @@ bool QGstAppSource::setStream(QIODevice *stream, qint64 offset)
     m_dataRequestSize = 0;
     m_sequential = true;
     m_maxBytes = 0;
-    streamedSamples = 0;
 
     if (stream) {
         if (!stream->isOpen() && !stream->open(QIODevice::ReadOnly))
@@ -122,38 +104,6 @@ QGstElement QGstAppSource::element() const
     return m_appSrc;
 }
 
-void QGstAppSource::write(const char *data, qsizetype size)
-{
-    QMutexLocker locker(&m_mutex);
-
-    qCDebug(qLcAppSrc) << "write" << size << m_noMoreData << m_dataRequestSize;
-    if (!size)
-        return;
-    Q_ASSERT(!m_stream);
-    m_buffer.append(data, size);
-    m_noMoreData = false;
-    pushData();
-}
-
-bool QGstAppSource::canAcceptMoreData() const
-{
-    QMutexLocker locker(&m_mutex);
-    return m_noMoreData || m_dataRequestSize != 0;
-}
-
-void QGstAppSource::suspend()
-{
-    QMutexLocker locker(&m_mutex);
-    m_suspended = true;
-}
-
-void QGstAppSource::resume()
-{
-    QMutexLocker locker(&m_mutex);
-    m_suspended = false;
-    m_noMoreData = true;
-}
-
 void QGstAppSource::onDataReady()
 {
     qCDebug(qLcAppSrc) << "onDataReady" << m_stream->bytesAvailable() << m_stream->size();
@@ -165,29 +115,27 @@ void QGstAppSource::streamDestroyed()
     qCDebug(qLcAppSrc) << "stream destroyed";
     m_stream = nullptr;
     m_dataRequestSize = 0;
-    streamedSamples = 0;
     sendEOS();
 }
 
 void QGstAppSource::pushData()
 {
-    if (m_appSrc.isNull() || !m_dataRequestSize || m_suspended) {
-        qCDebug(qLcAppSrc) << "push data: return immediately" << m_appSrc.isNull() << m_dataRequestSize << m_suspended;
+    if (m_appSrc.isNull() || !m_dataRequestSize) {
+        qCDebug(qLcAppSrc) << "push data: return immediately" << m_appSrc.isNull()
+                           << m_dataRequestSize;
         return;
     }
 
-    qCDebug(qLcAppSrc) << "pushData" << (m_stream ? m_stream : nullptr) << m_buffer.size();
+    Q_ASSERT(m_stream);
+
+    qCDebug(qLcAppSrc) << "pushData" << m_stream;
     if ((m_stream && m_stream->atEnd())) {
-        eosOrIdle();
-        qCDebug(qLcAppSrc) << "end pushData" << (m_stream ? m_stream : nullptr) << m_buffer.size();
+        sendEOS();
+        qCDebug(qLcAppSrc) << "end pushData" << m_stream;
         return;
     }
 
-    qint64 size;
-    if (m_stream)
-        size = m_stream->bytesAvailable();
-    else
-        size = m_buffer.size();
+    qint64 size = m_stream->bytesAvailable();
 
     if (!m_dataRequestSize)
         m_dataRequestSize = m_maxBytes;
@@ -196,51 +144,41 @@ void QGstAppSource::pushData()
 
     GstBuffer* buffer = gst_buffer_new_and_alloc(size);
 
-    if (m_sequential || !m_stream)
+    if (m_sequential)
         buffer->offset = bytesReadSoFar;
     else
         buffer->offset = m_stream->pos();
-
-    if (m_format.isValid()) {
-        // timestamp raw audio data
-        uint nSamples = size/m_format.bytesPerFrame();
-
-        GST_BUFFER_TIMESTAMP(buffer) = gst_util_uint64_scale(streamedSamples, GST_SECOND, m_format.sampleRate());
-        GST_BUFFER_DURATION(buffer) = gst_util_uint64_scale(nSamples, GST_SECOND, m_format.sampleRate());
-        streamedSamples += nSamples;
-    }
 
     GstMapInfo mapInfo;
     gst_buffer_map(buffer, &mapInfo, GST_MAP_WRITE);
     void* bufferData = mapInfo.data;
 
     qint64 bytesRead;
-    if (m_stream)
-        bytesRead = m_stream->read((char*)bufferData, size);
-    else
-        bytesRead = m_buffer.read((char*)bufferData, size);
-    buffer->offset_end =  buffer->offset + bytesRead - 1;
+    bytesRead = m_stream->read((char *)bufferData, size);
+
+    buffer->offset_end = buffer->offset + bytesRead - 1;
     bytesReadSoFar += bytesRead;
 
     gst_buffer_unmap(buffer, &mapInfo);
     qCDebug(qLcAppSrc) << "pushing bytes into gstreamer" << buffer->offset << bytesRead;
     if (bytesRead == 0) {
         gst_buffer_unref(buffer);
-        eosOrIdle();
-        qCDebug(qLcAppSrc) << "end pushData" << (m_stream ? m_stream : nullptr) << m_buffer.size();
+        sendEOS();
+        qCDebug(qLcAppSrc) << "end pushData" << m_stream;
         return;
     }
-    m_noMoreData = false;
-    emit bytesProcessed(bytesRead);
 
     GstFlowReturn ret = m_appSrc.pushBuffer(buffer);
-    if (ret == GST_FLOW_ERROR) {
-        qWarning() << "QGstAppSrc: push buffer error";
-    } else if (ret == GST_FLOW_FLUSHING) {
-        qWarning() << "QGstAppSrc: push buffer wrong state";
-    }
-    qCDebug(qLcAppSrc) << "end pushData" << (m_stream ? m_stream : nullptr) << m_buffer.size();
+    switch (ret) {
+    case GST_FLOW_OK:
+        break;
 
+    default:
+        qWarning() << "QGstAppSrc: push buffer error -" << gst_flow_get_name(ret);
+        break;
+    }
+
+    qCDebug(qLcAppSrc) << "end pushData" << m_stream;
 }
 
 bool QGstAppSource::doSeek(qint64 value)
@@ -295,23 +233,6 @@ void QGstAppSource::sendEOS()
         return;
 
     gst_app_src_end_of_stream(GST_APP_SRC(m_appSrc.element()));
-}
-
-void QGstAppSource::eosOrIdle()
-{
-    qCDebug(qLcAppSrc) << "eosOrIdle";
-    if (m_appSrc.isNull())
-        return;
-
-    if (!m_sequential) {
-        sendEOS();
-        return;
-    }
-    if (m_noMoreData)
-        return;
-    qCDebug(qLcAppSrc) << "    idle!";
-    m_noMoreData = true;
-    emit noMoreData();
 }
 
 QT_END_NAMESPACE
--- src/plugins/multimedia/gstreamer/common/qgstappsource_p.h
+++ src/plugins/multimedia/gstreamer/common/qgstappsource_p.h
@@ -15,15 +15,13 @@
 // We mean it.
 //
 
-#include <private/qtmultimediaglobal_p.h>
-#include <private/qmultimediautils_p.h>
-#include <qaudioformat.h>
 
 #include <QtCore/qobject.h>
 #include <QtCore/qiodevice.h>
-#include <QtCore/private/qringbuffer_p.h>
 #include <QtCore/qatomic.h>
 #include <QtCore/qmutex.h>
+
+#include <QtMultimedia/private/qtmultimediaglobal_p.h>
 
 #include <common/qgst_p.h>
 #include <gst/app/gstappsrc.h>
@@ -38,29 +36,18 @@ public:
     ~QGstAppSource();
 
     bool setup(QIODevice *stream = nullptr, qint64 offset = 0);
-    void setAudioFormat(const QAudioFormat &f);
 
     void setExternalAppSrc(QGstAppSrc);
     QGstElement element() const;
 
-    void write(const char *data, qsizetype size);
-
-    bool canAcceptMoreData() const;
-
-    void suspend();
-    void resume();
-
-Q_SIGNALS:
-    void bytesProcessed(int bytes);
-    void noMoreData();
-
 private Q_SLOTS:
-    void pushData();
-    bool doSeek(qint64);
     void onDataReady();
-
     void streamDestroyed();
+
 private:
+    bool doSeek(qint64);
+    void pushData();
+
     QGstAppSource(QGstAppSrc appsrc, QObject *parent);
 
     bool setStream(QIODevice *, qint64 offset);
@@ -71,24 +58,18 @@ private:
     static void on_need_data(GstAppSrc *element, uint arg0, gpointer userdata);
 
     void sendEOS();
-    void eosOrIdle();
 
     mutable QMutex m_mutex;
 
     QIODevice *m_stream = nullptr;
-    QRingBuffer m_buffer;
-    QAudioFormat m_format;
 
     QGstAppSrc m_appSrc;
     bool m_sequential = true;
-    bool m_suspended = false;
-    bool m_noMoreData = false;
     GstAppStreamType m_streamType = GST_APP_STREAM_TYPE_RANDOM_ACCESS;
     qint64 m_offset = 0;
     qint64 m_maxBytes = 0;
     qint64 bytesReadSoFar = 0;
     QAtomicInteger<unsigned int> m_dataRequestSize = 0;
-    int streamedSamples = 0;
 };
 
 QT_END_NAMESPACE
