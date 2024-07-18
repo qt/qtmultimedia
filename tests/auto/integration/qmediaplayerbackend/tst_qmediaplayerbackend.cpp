09182df1b4cb5fa4991ef90b4c482bd241ae64e2
Bartlomiej Moskal <bartlomiej.moskal@qt.io>
2024-07-18 08:59:09 +0200

    FFmpeg decoder: Retry receiveAVFrames for EAGAIN if stream is end
    [0]avcodec_receive_frame function may return EAGAIN error. It indicates
    that the decoder needs more packets before it can produce a frame. Some
    codecs, especially H.264, might need several packets to start producing
    frames.
    
    What is more, for FFAMediaCodec on Android, FFmpeg relies on
    [1]dequeueOutputBuffer JNI call with timeout. It seems that in
    [2]ff_AMediaCodec_dequeueOutputBuffer call, the timeout set to 1 second
    may not be always enough.
    
    When all packets were send, there will be a flush packet (that signals
    End of the Stream). After that there will be no more packets.
    In case when all packets were sent and avcodec_receive_frame call
    returns EAGAIN error, it is possible that the reason of the error is
    just a timeout on dequeueOutputBuffer JNI call.
    
    That is why, in this case, we will give one more try to call
    avcodec_receive_frame.
    
    [0]https://ffmpeg.org/doxygen/7.0/group__lavc__decoding.html#ga11e6542c4e66d3028668788a1a74217c
    [1]https://developer.android.com/reference/android/media/MediaCodec#dequeueOutputBuffer(android.media.MediaCodec.BufferInfo,%20long)
    [2]https://ffmpeg.org/doxygen/7.0/mediacodecdec__common_8c_source.html#l00726
    
    Fixes: QTBUG-126428
    Change-Id: I0fbb6a7eb6fb225303537402085dbdca825c524c
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 1296afc3ae3441c37b0dde953bd6747df35de699)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8c9031d1a2681113c329c0f5a2333b90839439d6)
    (cherry picked from commit cbf66b439dac2753f035de1b62cd1cbc9af08ff2)

--- tests/auto/integration/qmediaplayerbackend/tst_qmediaplayerbackend.cpp
+++ tests/auto/integration/qmediaplayerbackend/tst_qmediaplayerbackend.cpp
@@ -1060,9 +1060,6 @@ void tst_QMediaPlayerBackend::
 void tst_QMediaPlayerBackend::
         setSourceAndPlay_setCorrectVideoSize_whenVideoHasNonStandardPixelAspectRatio()
 {
-#ifdef Q_OS_ANDROID
-    QSKIP("SKIP initTestCase on CI, because of QTBUG-126428");
-#endif
     if (isGStreamerPlatform() && isCI())
         QSKIP("QTBUG-124005: Fails with gstreamer on CI");
 
@@ -1546,9 +1543,6 @@ void tst_QMediaPlayerBackend::
 
 void tst_QMediaPlayerBackend::play_waitsForLastFrameEnd_whenPlayingVideoWithLongFrames()
 {
-#ifdef Q_OS_ANDROID
-    QSKIP("SKIP initTestCase on CI, because of QTBUG-126428");
-#endif
     if (isCI() && isGStreamerPlatform())
         QSKIP_GSTREAMER("QTBUG-124005: spurious failures with gstreamer");
 
@@ -3171,9 +3165,6 @@ void tst_QMediaPlayerBackend::pause_rendersVideoAtCorrectResolution_data()
 
 void tst_QMediaPlayerBackend::pause_rendersVideoAtCorrectResolution()
 {
-#ifdef Q_OS_ANDROID
-    QSKIP("SKIP initTestCase on CI, because of QTBUG-126428");
-#endif
     QFETCH(const MaybeUrl, mediaFile);
     QFETCH(const int, width);
     QFETCH(const int, height);
@@ -3189,8 +3180,10 @@ void tst_QMediaPlayerBackend::pause_rendersVideoAtCorrectResolution()
 
     // Act
     player.pause();
-
+#ifndef Q_OS_ANDROID
+    // isCI() does not work on Android (variable is set on host instead of device where tests are run)
     if (isCI() && isFFMPEGPlatform())
+#endif
         QEXPECT_FAIL("av1", "QTBUG-119711: AV1 decoding requires HW support in the FFMPEG backend",
                      Abort);
 
@@ -3743,9 +3736,6 @@ void tst_QMediaPlayerBackend::lazyLoadVideo()
 
 void tst_QMediaPlayerBackend::videoSinkSignals()
 {
-#ifdef Q_OS_ANDROID
-    QSKIP("SKIP initTestCase on CI, because of QTBUG-126428");
-#endif
     std::atomic<int> videoFrameCounter = 0;
     std::atomic<int> videoSizeCounter = 0;
 
