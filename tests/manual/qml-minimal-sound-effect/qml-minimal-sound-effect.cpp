6d23bc167f45f8daed1fae44252f9d24606a0642
Jøger Hansegård <joger.hansegard@qt.io>
2024-10-15 22:11:40 +0200

    Add a manual test for QSoundEffect
    This manual test demonstrates use of SoundEffect QML type, and
    illustrates a problem on Windows where a short sound is being cut.
    
    Task-number: QTBUG-130114
    Pick-to: 6.8
    Change-Id: Ia50c7aae2d5a39403227bf5cd2c974d155d1b780
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

---
+++ tests/manual/qml-minimal-sound-effect/qml-minimal-sound-effect.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl("qrc:/qml-minimal-sound-effect.qml"));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
