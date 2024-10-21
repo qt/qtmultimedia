120db99b4ae1c7e308d687f706abc60d728ecad3
Jøger Hansegård <joger.hansegard@qt.io>
2024-10-21 18:42:27 +0200

    Turn tst_qmediaformat into a unit test
    tst_qmediaformat tests functionality that benefits from unit testing
    without actual backends. This will help us improving the test coverage
    of the cross-platform parts of QMediaFormat, and can enable testing of
    situations that are not reproducible with real backends.
    
    This can enable testing of functionality that is only exposed for
    example if the FFmpeg media backend is compiled without any video
    codecs.
    
    Task-number: QTBUG-130367
    Pick-to: 6.5
    Change-Id: I8ec985e1a91ecb499641066547318410b81fe25b
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Pavel Dubsky <pavel.dubsky@qt.io>
    (cherry picked from commit 09a81135191dc871ed98dcbec864a706253d074d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/unit/mockbackend/qmockintegration.h
+++ tests/auto/unit/mockbackend/qmockintegration.h
@@ -25,6 +25,7 @@ class QMockCamera;
 class QMockMediaCaptureSession;
 class QMockVideoSink;
 class QMockSurfaceCapture;
+class QPlatformMediaFormatInfo;
 
 class QMockIntegration : public QPlatformMediaIntegration
 {
@@ -66,13 +67,13 @@ public:
     QMockVideoSink *lastVideoSink() const { return m_lastVideoSink; }
     QMockSurfaceCapture *lastScreenCapture() { return m_lastScreenCapture; }
     QMockSurfaceCapture *lastWindowCapture() { return m_lastWindowCapture; }
+    QPlatformMediaFormatInfo *getWritableFormatInfo();
 
 protected:
     QPlatformVideoDevices *createVideoDevices() override;
     std::unique_ptr<QPlatformMediaDevices> createMediaDevices() override;
 
 private:
-
     Flags m_flags = {};
     QMockMediaPlayer *m_lastPlayer = nullptr;
     QMockAudioDecoder *m_lastAudioDecoderControl = nullptr;
