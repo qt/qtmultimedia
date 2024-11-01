bfa95627f3482df83e4b3d9f32089c7be0648e8b
Tim Blechmann <tim.blechmann@qt.io>
2024-11-01 11:53:24 +0800

    GStreamer: player - improve media handling
    * avoid flushing seek in setMedia by seeking while READY
    * setVideoSink does not need to flush the pipeline when the player is
    stopped
    
    Change-Id: I3abe38f2c7e631195a0d5b47dbf32b239f11131a
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    (cherry picked from commit e547bdd26ceb25b9146a4fba9d3aecfc21906ad9)
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>

--- src/plugins/multimedia/gstreamer/common/qgstreamermediaplayer.cpp
+++ src/plugins/multimedia/gstreamer/common/qgstreamermediaplayer.cpp
@@ -1435,6 +1435,8 @@ void QGstreamerMediaPlayer::setMedia(const QUrl &content, QIODevice *stream)
     }
 
     mediaStatusChanged(QMediaPlayer::LoadingMedia);
+    playerPipeline.setStateSync(GST_STATE_PAUSED);
+    playerPipeline.setPosition(0ns, /*flush=*/false);
 
     if (!playerPipeline.setStateSync(GST_STATE_PAUSED)) {
         qCWarning(qLcMediaPlayer) << "Unable to set the pipeline to the paused state.";
@@ -1446,7 +1448,6 @@ void QGstreamerMediaPlayer::setMedia(const QUrl &content, QIODevice *stream)
         return;
     }
 
-    m_pendingSeekPosition = 0ns;
     resetMetadata.dismiss();
 }
 
@@ -1497,7 +1498,12 @@ void QGstreamerMediaPlayer::setVideoSink(QVideoSink *sink)
     using namespace std::chrono_literals;
     gstVideoOutput->setVideoSink(sink);
 
-    playerPipeline.flush(); // ensure that we send the current video frame to the new sink
+    if (sink && state() != QMediaPlayer::StoppedState) {
+        if (!m_pendingSeekPosition) {
+            m_pendingSeekPosition = playerPipeline.position();
+            applyPendingOperations();
+        }
+    }
     playerPipeline.dumpGraph("setVideoSink");
 }
 
