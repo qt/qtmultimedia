0f9eaa119556fe10ca2a1408134f07f97e6dc5ba
Artem Dyomin <artem.dyomin@qt.io>
2024-12-09 15:40:58 +0100

    Implement RhiValueMapper
    The class implements mapping of rhi => value, and manages lifetime
    of the values according to the rhi's lifetime.
    
    Task-number: QTBUG-131918
    Change-Id: I5cee8a9c473e6e765c5c1471ecefb1ccded0bff5
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    (cherry picked from commit 507c6849a91f9c17cbb7c9bb5d2e642486d3edfa)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 87d34727dea9fa30b283ad5ebaa6bb1e368ae9e9)
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

---
+++ src/multimedia/qrhivaluemapper_p.h
@@ -0,0 +1,131 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QRHIVALUEMAPPER_P_H
+#define QRHIVALUEMAPPER_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtMultimedia/qtmultimediaglobal.h>
+
+#include <qreadwritelock.h>
+
+#include <memory>
+#include <map>
+
+QT_BEGIN_NAMESPACE
+
+class QRhi;
+
+class Q_MULTIMEDIA_EXPORT QRhiCallback : public std::enable_shared_from_this<QRhiCallback>
+{
+public:
+    class Manager;
+
+    QRhiCallback();
+    virtual ~QRhiCallback();
+
+    void registerCallback(QRhi &rhi);
+
+protected:
+    virtual void onRhiCleanup(QRhi &rhi) = 0;
+
+private:
+    std::shared_ptr<Manager> m_manager;
+};
+
+template <typename Value>
+class QRhiValueMapper
+{
+    struct Data : QRhiCallback
+    {
+        QReadWriteLock lock;
+        // In most cases, one or 2 elements will be here, so let's use map
+        // instead of unordered_map for better efficiency with few elements.
+        std::map<QRhi *, Value> storage;
+
+        void onRhiCleanup(QRhi &rhi) override
+        {
+            QWriteLocker locker(&lock);
+            storage.erase(&rhi);
+        }
+    };
+
+public:
+    ~QRhiValueMapper()
+    {
+        clear(); // it must be cleared when on the destruction to synchronize with rhi cleanup
+    }
+
+    Q_DISABLE_COPY(QRhiValueMapper);
+
+    QRhiValueMapper(QRhiValueMapper&& ) = default;
+    QRhiValueMapper& operator = (QRhiValueMapper&&) = default;
+
+    QRhiValueMapper() : m_data(std::make_shared<Data>()) { }
+
+    template <typename V>
+    std::pair<Value *, bool> tryMap(QRhi &rhi, V &&value)
+    {
+        QWriteLocker locker(&m_data->lock);
+
+        auto [rhiIt, rhiAdded] = m_data->storage.try_emplace(&rhi, std::forward<V>(value));
+
+        if (rhiAdded)
+            m_data->registerCallback(rhi);
+
+        return { &rhiIt->second, rhiAdded };
+    }
+
+    Value *get(QRhi &rhi) const
+    {
+        QReadLocker locker(&m_data->lock);
+        auto rhiIt = m_data->storage.find(&rhi);
+        return rhiIt == m_data->storage.end() ? nullptr : &rhiIt->second;
+    }
+
+    //    To be added for thread-safe value read:
+    //
+    //    template <typename Reader>
+    //    void read(QRhi &rhi, Reader&& reader) const {
+    //        QReadLocker locker(&m_data->lock);
+    //        auto rhiIt = m_data->storage.find(&rhi);
+    //        if (rhiIt != m_data->storage.end())
+    //            reader(const_cast<const Value&>(rhiIt->second));
+    //    }
+
+    void clear()
+    {
+        if (!m_data)
+            return; // the object has been moved
+        QWriteLocker locker(&m_data->lock);
+        m_data->storage.clear();
+    }
+
+    template <typename Predicate>
+    QRhi *findRhi(Predicate &&p) const
+    {
+        QReadLocker locker(&m_data->lock);
+        auto &storage = m_data->storage;
+
+        auto it = std::find_if(storage.begin(), storage.end(),
+                               [&p](auto &rhiItem) { return p(*rhiItem.first); });
+        return it == storage.end() ? nullptr : it->first;
+    }
+
+private:
+    std::shared_ptr<Data> m_data;
+};
+
+QT_END_NAMESPACE
+
+#endif // QRHIVALUEMAPPER_P_H
