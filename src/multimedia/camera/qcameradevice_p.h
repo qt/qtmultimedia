77551d744afc9a52e9bbddb55357c9f5e62c9bbe
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-01-02 18:35:57 +0200

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit 2702a760bf02f05831af34e16a4806ae9a208f78.
    
    Revert of commercial SPDX license identifiers is required for the
    Qt 6.5.x opensource releases, Qt 6.5.4 onwards.
    
    Change-Id: I2b74c5f86f3f49042cdd48ad9bd79f135b58ba9e
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>

--- src/multimedia/camera/qcameradevice_p.h
+++ src/multimedia/camera/qcameradevice_p.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2021 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #ifndef QCAMERAINFO_P_H
 #define QCAMERAINFO_P_H
