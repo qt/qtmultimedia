6da512546f935d9ae4d59b4127a124f42b1ec3ed
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-01-06 11:22:13 +0200

    Fix the SPDX-License-Identifiers speard on two lines
    The SPDX-License-Identifier needs to be on a single line.
    Otherwise the license checking test will fail.
    
    Change-Id: I0ae0a0979d0a80214d59de0368b3524745043738
    Reviewed-by: Lucie Gerard <lucie.gerard@qt.io>

--- src/plugins/multimedia/ffmpeg/qffmpegscreencapture_dxgi_p.h
+++ src/plugins/multimedia/ffmpeg/qffmpegscreencapture_dxgi_p.h
@@ -1,6 +1,5 @@
 // Copyright (C) 2021 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR
-// GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #ifndef QFFMPEGSCREENCAPTURE_WINDOWS_H
 #define QFFMPEGSCREENCAPTURE_WINDOWS_H
