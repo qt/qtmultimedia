efea8d9a4578c10330593c9de3a661dafb8f70ac
Tim Blechmann <tim.blechmann@qt.io>
2025-01-16 09:53:25 +0800

    Pipewire: symbol loader - order symbols by functionality
    Change-Id: I88fa2d0786d65746e2ae0818db4eb043a2b85e1b
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit ab1a75e74284bd2191e32520c734488227e14188)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d11d015d8ac05f26a65c3656449ea05d1888882a)
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>

--- src/multimedia/pipewire/qpipewire_symbolloader.cpp
+++ src/multimedia/pipewire/qpipewire_symbolloader.cpp
@@ -16,11 +16,16 @@ BEGIN_INIT_FUNCS("pipewire-" PW_API_VERSION, "0")
 
 INIT_FUNC(pw_init);
 INIT_FUNC(pw_deinit);
-INIT_FUNC(pw_thread_loop_new);
-INIT_FUNC(pw_thread_loop_get_loop);
+INIT_OPT_FUNC(pw_check_library_version);
 INIT_FUNC(pw_context_new);
+INIT_FUNC(pw_context_destroy);
 INIT_FUNC(pw_context_connect_fd);
+INIT_FUNC(pw_core_disconnect);
+INIT_FUNC(pw_thread_loop_new);
+INIT_FUNC(pw_thread_loop_destroy);
+INIT_FUNC(pw_thread_loop_get_loop);
 INIT_FUNC(pw_thread_loop_start);
+INIT_FUNC(pw_thread_loop_stop);
 INIT_FUNC(pw_thread_loop_lock);
 INIT_FUNC(pw_thread_loop_unlock);
 INIT_FUNC(pw_thread_loop_timed_wait);
@@ -28,29 +33,29 @@ INIT_FUNC(pw_thread_loop_signal);
 INIT_FUNC(pw_properties_new_dict);
 INIT_FUNC(pw_properties_free);
 INIT_FUNC(pw_stream_new);
+INIT_FUNC(pw_stream_destroy);
 INIT_FUNC(pw_stream_add_listener);
 INIT_FUNC(pw_stream_connect);
 INIT_FUNC(pw_stream_disconnect);
-INIT_FUNC(pw_stream_destroy);
 INIT_FUNC(pw_stream_dequeue_buffer);
-INIT_FUNC(pw_thread_loop_stop);
 INIT_FUNC(pw_stream_queue_buffer);
 INIT_FUNC(pw_proxy_destroy);
-INIT_FUNC(pw_core_disconnect);
-INIT_FUNC(pw_context_destroy);
-INIT_FUNC(pw_thread_loop_destroy);
 INIT_FUNC(pw_get_library_version);
-INIT_OPT_FUNC(pw_check_library_version);
 
 END_INIT_FUNCS()
 
 DEFINE_FUNC(pw_init, 2);
 DEFINE_FUNC(pw_deinit, 0);
-DEFINE_FUNC(pw_thread_loop_new, 2);
-DEFINE_FUNC(pw_thread_loop_get_loop, 1);
+DEFINE_FUNC(pw_check_library_version, 3);
 DEFINE_FUNC(pw_context_new, 3);
+DEFINE_FUNC(pw_context_destroy, 1);
 DEFINE_FUNC(pw_context_connect_fd, 4);
+DEFINE_FUNC(pw_core_disconnect, 1);
+DEFINE_FUNC(pw_thread_loop_new, 2);
+DEFINE_FUNC(pw_thread_loop_destroy, 1);
+DEFINE_FUNC(pw_thread_loop_get_loop, 1);
 DEFINE_FUNC(pw_thread_loop_start, 1);
+DEFINE_FUNC(pw_thread_loop_stop, 1);
 DEFINE_FUNC(pw_thread_loop_lock, 1);
 DEFINE_FUNC(pw_thread_loop_unlock, 1);
 DEFINE_FUNC(pw_thread_loop_timed_wait, 2);
@@ -58,18 +63,13 @@ DEFINE_FUNC(pw_thread_loop_signal, 2);
 DEFINE_FUNC(pw_properties_new_dict, 1);
 DEFINE_FUNC(pw_properties_free, 1);
 DEFINE_FUNC(pw_stream_new, 3);
+DEFINE_FUNC(pw_stream_destroy, 1);
 DEFINE_FUNC(pw_stream_add_listener, 4);
 DEFINE_FUNC(pw_stream_connect, 6);
 DEFINE_FUNC(pw_stream_disconnect, 1);
-DEFINE_FUNC(pw_stream_destroy, 1);
 DEFINE_FUNC(pw_stream_dequeue_buffer, 1);
-DEFINE_FUNC(pw_thread_loop_stop, 1);
 DEFINE_FUNC(pw_stream_queue_buffer, 2);
 DEFINE_FUNC(pw_proxy_destroy, 1);
-DEFINE_FUNC(pw_core_disconnect, 1);
-DEFINE_FUNC(pw_context_destroy, 1);
-DEFINE_FUNC(pw_thread_loop_destroy, 1);
 DEFINE_FUNC(pw_get_library_version, 0);
-DEFINE_FUNC(pw_check_library_version, 3);
 
 DEFINE_IS_LOADED_CHECKER(qPipewireIsLoaded)
