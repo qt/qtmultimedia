47cfb601954871e858ae797d97eb24adc0778bca
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2025-01-27 11:54:00 +0100

    QImageCapture: Fire qualityChanged signal when setQuality is called
    Likely typo meant we emitted the incorrect signal when setQuality is
    successfully applied.
    
    Change-Id: I4f6a8f3667ff990436419ed315732ccf038bea9f
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit b35600d3fc3bd32daaaf93fda733e5eb345e9919)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d072cf422010e8ce083360a1a16840aec87481fc)
    (cherry picked from commit 396b5aaea07aabbaa0684077638f5f3d74877b6a)

--- src/multimedia/camera/qimagecapture.cpp
+++ src/multimedia/camera/qimagecapture.cpp
@@ -538,7 +538,7 @@ void QImageCapture::setQuality(Quality quality)
         return;
     fmt.setQuality(quality);
     d->control->setImageSettings(fmt);
-    emit resolutionChanged();
+    emit qualityChanged();
 }
 
 /*!
