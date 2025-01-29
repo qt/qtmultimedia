6cf89523035228c97000c9cd38ff974bdd8dd178
Pavel Dubsky <pavel.dubsky@qt.io>
2025-01-29 20:13:52 +0100

    Add unit tests for QFFmpeg::getScaleConversionType
    Fixes: QTBUG-133334
    Change-Id: I7e7fc6cdd7130b00e82c5854c10af82857d5435e
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    (cherry picked from commit 0957f80783144a95f32783558e5271043d1e0dbb)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit f0cbd9f0b517f0124a9e78c3c737e899d6908d8c)

---
+++ tests/auto/unit/plugins/multimedia/ffmpeg/qffmpegvideoencoderutils/tst_qffmpegvideoencoderutils.cpp
@@ -0,0 +1,69 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include <QtTest/QtTest>
+
+#include <qobject.h>
+
+#include <QtQFFmpegMediaPluginImpl/private/qffmpegvideoencoderutils_p.h>
+
+QT_USE_NAMESPACE
+
+class tst_QFFmpegVideoEncoderUtils : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void getScaleConversionType_returnsCorrectConversionType_basedOnScaling();
+    void getScaleConversionType_returnsCorrectConversionType_basedOnScaling_data();
+};
+
+void tst_QFFmpegVideoEncoderUtils::getScaleConversionType_returnsCorrectConversionType_basedOnScaling_data()
+{
+    QTest::addColumn<QSize>("sourceSize");
+    QTest::addColumn<QSize>("targetSize");
+    QTest::addColumn<int>("expectedConversionType");
+
+#ifdef Q_OS_ANDROID
+    int expectedConversionTypeForUpscaling = SWS_BICUBIC;
+#else
+    int expectedConversionTypeForUpscaling = SWS_FAST_BILINEAR;
+#endif
+
+    QTest::newRow("Sizes are equal")
+            << QSize{ 800, 600 } << QSize{ 800, 600 } << SWS_FAST_BILINEAR;
+    QTest::newRow("Uniform downscaling")
+            << QSize{ 800, 600 } << QSize{ 400, 300 } << SWS_FAST_BILINEAR;
+    QTest::newRow("Uniform upscaling")
+            << QSize{ 400, 300 } << QSize{ 800, 600 } << expectedConversionTypeForUpscaling;
+    QTest::newRow("Anisotropic downscaling by width")
+            << QSize{ 800, 600 } << QSize{ 400, 600 } << SWS_FAST_BILINEAR;
+    QTest::newRow("Anisotropic downscaling by height")
+            << QSize{ 800, 600 } << QSize{ 800, 300 } << SWS_FAST_BILINEAR;
+    QTest::newRow("Anisotropic upscaling by width")
+            << QSize{ 400, 300 } << QSize{ 800, 300 } << expectedConversionTypeForUpscaling;
+    QTest::newRow("Anisotropic upscaling by height")
+            << QSize{ 400, 300 } << QSize{ 400, 600 } << expectedConversionTypeForUpscaling;
+    QTest::newRow("Anisotropic mixed scaling (width up, height down)")
+            << QSize{ 400, 600 } << QSize{ 800, 300 } << expectedConversionTypeForUpscaling;
+    QTest::newRow("Anisotropic mixed scaling (width down, height up)")
+            << QSize{ 800, 300 } << QSize{ 400, 600 } << expectedConversionTypeForUpscaling;
+}
+
+void tst_QFFmpegVideoEncoderUtils::getScaleConversionType_returnsCorrectConversionType_basedOnScaling()
+{
+    // Arrange
+    QFETCH(QSize, sourceSize);
+    QFETCH(QSize, targetSize);
+    QFETCH(int, expectedConversionType);
+
+    // Act
+    const int actualConversionType = QFFmpeg::getScaleConversionType(sourceSize, targetSize);
+
+    // Assert
+    QCOMPARE(actualConversionType, expectedConversionType);
+}
+
+QTEST_MAIN(tst_QFFmpegVideoEncoderUtils)
+
+#include "tst_qffmpegvideoencoderutils.moc"
