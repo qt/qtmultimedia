af1555072aa2ba09b9493fb3ec7aab5764a64fb0
Jøger Hansegård <joger.hansegard@qt.io>
2025-02-06 16:09:03 +0100

    Make existing debug logging easier to understand
    Task-number: QTBUG-128908
    Pick-to: 6.5
    Change-Id: Iab5d0793ef5ff140623550f111d8c6a303dd0a7f
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>
    (cherry picked from commit 27661b06a9129fbe8eccc14e45c681ff4ec01011)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8ec966271fb5b148dc2a3b046035e8427f47d9d8)

--- src/plugins/multimedia/ffmpeg/qffmpegresampler.cpp
+++ src/plugins/multimedia/ffmpeg/qffmpegresampler.cpp
@@ -17,8 +17,13 @@ QFFmpegResampler::QFFmpegResampler(const QAudioFormat &inputFormat, const QAudio
     Q_ASSERT(inputFormat.isValid());
     Q_ASSERT(outputFormat.isValid());
 
-    m_resampler =
-            createResampleContext(AVAudioFormat(m_inputFormat), AVAudioFormat(m_outputFormat));
+    const auto inputAvFormat = AVAudioFormat(m_inputFormat);
+    const auto outputAvFormat = AVAudioFormat(m_outputFormat);
+
+    m_resampler = createResampleContext(inputAvFormat, outputAvFormat);
+
+    qCDebug(qLcResampler) << "Created QFFmpegResampler with offset" << m_startTime
+                          << "us. Converting from" << inputAvFormat << "to" << outputAvFormat;
 }
 
 QFFmpegResampler::QFFmpegResampler(const CodecContext *codecContext,
@@ -28,15 +33,19 @@ QFFmpegResampler::QFFmpegResampler(const CodecContext *codecContext,
 {
     Q_ASSERT(codecContext);
 
-    qCDebug(qLcResampler) << "createResampler";
     const AVStream *audioStream = codecContext->stream();
 
     if (!m_outputFormat.isValid())
         // want the native format
         m_outputFormat = QFFmpegMediaFormatInfo::audioFormatFromCodecParameters(*audioStream->codecpar);
 
-    m_resampler = createResampleContext(AVAudioFormat(audioStream->codecpar),
-                                        AVAudioFormat(m_outputFormat));
+    const auto inputAvFormat = AVAudioFormat(audioStream->codecpar);
+    const auto outputAvFormat = AVAudioFormat(m_outputFormat);
+
+    m_resampler = createResampleContext(inputAvFormat, outputAvFormat);
+
+    qCDebug(qLcResampler).nospace() << "Created QFFmpegResampler. Offset: " << m_startTime
+                                    << "us. From: " << inputAvFormat << " to: " << outputAvFormat;
 }
 
 QFFmpegResampler::~QFFmpegResampler() = default;
@@ -69,8 +78,10 @@ QAudioBuffer QFFmpegResampler::resample(const uint8_t **inputData, int inputSamp
     const qint64 startTime = m_outputFormat.durationForFrames(m_samplesProcessed) + m_startTime;
     m_samplesProcessed += outSamples;
 
-    qCDebug(qLcResampler) << "    new frame" << startTime << "in_samples" << inputSamplesCount
-                          << outSamples << maxOutSamples;
+    qCDebug(qLcResampler).nospace()
+            << "Created output buffer. Time stamp: " << startTime
+            << "us. Samples in: " << inputSamplesCount
+            << ", Samples out: " << outSamples << ", Max samples: " << maxOutSamples;
     return QAudioBuffer(samples, m_outputFormat, startTime);
 }
 
