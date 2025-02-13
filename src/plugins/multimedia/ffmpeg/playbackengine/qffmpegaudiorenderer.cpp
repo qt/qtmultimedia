cd81f690bd0c559e5ca19de04c4a3996c389e291
Artem Dyomin <artem.dyomin@qt.io>
2025-02-13 15:16:07 +0100

    Add an alias 'RealClock' in ffmpegtime_p.h
    The alias is reused in different files.
    No functional changes.
    
    Task-number: QTBUG-133798
    Change-Id: Ia13146cb46a6a69d2c9103d1a3b3386dccc77b2e
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    (cherry picked from commit 84b0c83de5b00d0db894ee1949d0f1ba39117369)
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- src/plugins/multimedia/ffmpeg/playbackengine/qffmpegaudiorenderer.cpp
+++ src/plugins/multimedia/ffmpeg/playbackengine/qffmpegaudiorenderer.cpp
@@ -140,7 +140,7 @@ AudioRenderer::RenderingResult AudioRenderer::pushFrameToOutput(const Frame &fra
     auto firstFrameFlagGuard = qScopeGuard([&]() { m_firstFrameToSink = false; });
 
     const SynchronizationStamp syncStamp{ m_sink->state(), m_sink->bytesFree(),
-                                          m_bufferedData.offset, Clock::now() };
+                                          m_bufferedData.offset, RealClock::now() };
 
     if (!m_bufferedData.isValid()) {
         if (!frame.isValid()) {
