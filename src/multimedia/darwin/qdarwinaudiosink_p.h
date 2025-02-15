3c722956a8a4cf44490431cd8633aea3a3f86ecd
Tim Blechmann <tim.blechmann@qt.io>
2025-02-15 16:13:22 +0700

    CoreAudio: modernize QDarwinAudioSink
    We use the same architecture used for the pipewire backend in order to
    run the CoreAudio backend.
    
    Change-Id: I4384de1b88e318e466720721d1934625a771546f
    Reviewed-by: Nils Petter Skålerud <nils.petter.skalerud@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit ece4344dd8237ddeb7f5ba17f00d38b1bc83d4ff)
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>
    (cherry picked from commit dbc233f7f8b9a196f4fca485be0a911c2c418ebd)

--- src/multimedia/darwin/qdarwinaudiosink_p.h
+++ src/multimedia/darwin/qdarwinaudiosink_p.h
@@ -31,71 +31,10 @@
 
 QT_BEGIN_NAMESPACE
 
-class QDarwinAudioSinkBuffer;
-class QTimer;
-class QCoreAudioDeviceInfo;
-class CoreAudioRingBuffer;
+class QCoreAudioSinkStream;
 
-class QDarwinAudioSinkBuffer : public QObject
+class QDarwinAudioSink final : public QPlatformAudioSink
 {
-    Q_OBJECT
-
-public:
-    QDarwinAudioSinkBuffer(int bufferSize, int maxPeriodSize, QAudioFormat const& audioFormat);
-    ~QDarwinAudioSinkBuffer() override;
-
-    qint64 readFrames(char *data, qint64 maxFrames);
-    qint64 writeBytes(const char *data, qint64 maxSize);
-
-    int available() const;
-
-    bool deviceAtEnd() const;
-
-    void reset();
-
-    void setPrefetchDevice(QIODevice *device);
-
-    QIODevice *prefetchDevice() const;
-
-    void setFillingEnabled(bool enabled);
-
-signals:
-    void readyRead();
-
-private slots:
-    void fillBuffer();
-
-private:
-    bool m_deviceError = false;
-    bool m_fillingEnabled = false;
-    bool m_deviceAtEnd = false;
-    const int m_maxPeriodSize = 0;
-    const int m_bytesPerFrame = 0;
-    const int m_periodTime = 0;
-    QIODevice *m_device = nullptr;
-    QTimer *m_fillTimer = nullptr;
-    QtPrivate::QAudioRingBuffer<char> m_buffer;
-};
-
-class QDarwinAudioSinkDevice : public QIODevice
-{
-public:
-    QDarwinAudioSinkDevice(QDarwinAudioSinkBuffer *audioBuffer, QObject *parent);
-
-    qint64 readData(char *data, qint64 len) override;
-    qint64 writeData(const char *data, qint64 len) override;
-
-    bool isSequential() const override { return true; }
-
-private:
-    QDarwinAudioSinkBuffer *m_audioBuffer;
-};
-
-
-class QDarwinAudioSink : public QPlatformAudioSink
-{
-    Q_OBJECT
-
 public:
     QDarwinAudioSink(const QAudioDevice &device, const QAudioFormat &format, QObject *parent);
     ~QDarwinAudioSink() override;
@@ -110,61 +49,23 @@ public:
     void setBufferSize(qsizetype value) override;
     qsizetype bufferSize() const override;
     qint64 processedUSecs() const override;
-    QAudio::Error error() const override;
-    QAudio::State state() const override;
     QAudioFormat format() const override;
 
     void setVolume(qreal volume) override;
     qreal volume() const override;
 
-private slots:
-    void inputReady();
-    void updateAudioDevice();
-    void appStateChanged(Qt::ApplicationState state);
-
 private:
-    enum ThreadState { Running, Draining, Stopped };
+    void resumeStreamIfNecessary();
 
-    static OSStatus renderCallback(void *inRefCon,
-                                    AudioUnitRenderActionFlags *ioActionFlags,
-                                    const AudioTimeStamp *inTimeStamp,
-                                    UInt32 inBusNumber,
-                                    UInt32 inNumberFrames,
-                                    AudioBufferList *ioData);
-
-    bool open();
-    void close();
-    void onAudioDeviceIdle();
-    void onAudioDeviceError();
-    void onAudioDeviceDrained();
-
+    friend class QCoreAudioSinkStream;
     QAudioDevice m_audioDevice;
 
-    static constexpr int DEFAULT_BUFFER_SIZE = 8 * 1024;
-
-    bool m_isOpen = false;
-    int m_internalBufferSize = DEFAULT_BUFFER_SIZE;
-    int m_periodSizeBytes = 0;
-    qint64 m_totalFrames = 0;
     const QAudioFormat m_audioFormat;
-    QIODevice *m_audioIO = nullptr;
-    AudioUnit m_audioUnit = 0;
-    AudioStreamBasicDescription m_streamFormat;
-    std::unique_ptr<QDarwinAudioSinkBuffer> m_audioBuffer;
+    std::optional<int> m_internalBufferSize;
+
     qreal m_cachedVolume = 1.;
-#if defined(Q_OS_MACOS)
-    bool addDisconnectListener(AudioObjectID);
-    void removeDisconnectListener();
 
-    QCoreAudioUtils::DeviceDisconnectMonitor m_disconnectMonitor;
-    QFuture<void> m_stopOnDisconnected;
-
-    qreal m_volume = 1.;
-#endif
-
-    QAudioStateMachine m_stateMachine;
-    QSemaphore m_drainSemaphore;
-    AudioUnitState m_audioUnitState = AudioUnitState::Stopped;
+    std::shared_ptr<QCoreAudioSinkStream> m_stream;
 };
 
 QT_END_NAMESPACE
