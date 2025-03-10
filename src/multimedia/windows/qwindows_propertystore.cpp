289b5cf17a594b846219bd238caf7e0990593d88
Tim Blechmann <tim.blechmann@qt.io>
2025-03-10 11:55:44 +0800

    Windows: introduce PropertyStoreHelper
    We often access the IPropertyStore so it makes sense to write a small
    abstraction layer to make implementation more expressive.
    
    Change-Id: I2f708781d99120afcd76ce4659069ac9b042729e
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    (cherry picked from commit e542ad6c26d67bcbe290458f039899d7a27bd47d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2e44db9611379eed760c70ec0b2525f893e2d06c)

---
+++ src/multimedia/windows/qwindows_propertystore.cpp
@@ -0,0 +1,76 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qwindows_propertystore_p.h"
+
+#include <QtCore/qscopeguard.h>
+#include <QtCore/qstring.h>
+#include <QtCore/qdebug.h>
+#include <QtCore/private/qsystemerror_p.h>
+#include <QtMultimedia/private/qcomtaskresource_p.h>
+
+#include <mmdeviceapi.h>
+#include <propsys.h>
+#include <propvarutil.h>
+
+QT_BEGIN_NAMESPACE
+
+namespace QtMultimediaPrivate {
+
+PropertyStoreHelper::PropertyStoreHelper(ComPtr<IPropertyStore> props) : m_props(std::move(props))
+{
+}
+
+QMaybe<PropertyStoreHelper> PropertyStoreHelper::open(const ComPtr<IMMDevice> &device)
+{
+    ComPtr<IPropertyStore> props;
+    HRESULT hr = device->OpenPropertyStore(STGM_READ, props.GetAddressOf());
+    if (!SUCCEEDED(hr)) {
+        return QUnexpected{ QSystemError::windowsComString(hr) };
+    }
+    return PropertyStoreHelper(std::move(props));
+}
+
+std::optional<QString> PropertyStoreHelper::getString(const PROPERTYKEY &property)
+{
+    PROPVARIANT variant;
+    PropVariantInit(&variant);
+
+    auto cleanup = qScopeGuard([&] {
+        PropVariantClear(&variant);
+    });
+
+    if (!SUCCEEDED(m_props->GetValue(property, &variant)))
+        return std::nullopt;
+
+    QComTaskResource<WCHAR> str;
+    HRESULT hr = PropVariantToStringAlloc(variant, str.address());
+    if (SUCCEEDED(hr))
+        return QString::fromWCharArray(variant.pwszVal);
+
+    qWarning() << "PropertyStoreHelper::getString: PropVariantToStringAlloc failed"
+               << QSystemError::windowsComString(hr);
+    return std::nullopt;
+}
+
+std::optional<uint32_t> PropertyStoreHelper::getUInt32(const PROPERTYKEY &property)
+{
+    PROPVARIANT variant;
+    PropVariantInit(&variant);
+
+    if (!SUCCEEDED(m_props->GetValue(property, &variant)))
+        return std::nullopt;
+
+    ULONG ret;
+    HRESULT hr = PropVariantToUInt32(variant, &ret);
+    if (SUCCEEDED(hr))
+        return uint32_t{ ret };
+
+    qWarning() << "PropertyStoreHelper::getUInt32: PropVariantToUInt32 failed"
+               << QSystemError::windowsComString(hr);
+    return std::nullopt;
+}
+
+} // namespace QtMultimediaPrivate
+
+QT_END_NAMESPACE
