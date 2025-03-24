8ea2281f924f19d757ea71a5971d753b4c001fa5
Tim Blechmann <tim.blechmann@qt.io>
2025-03-24 15:26:17 +0800

    Tests: silence -Wunused-lambda-capture
    Pick-to: 6.8 6.9
    Change-Id: I0eeb0411e0f60ea0bfec33c416edfd8919144c6e
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

--- tests/auto/integration/qmediaplayer_concurrent/tst_qmediaplayer_concurrent.cpp
+++ tests/auto/integration/qmediaplayer_concurrent/tst_qmediaplayer_concurrent.cpp
@@ -37,7 +37,7 @@ private Q_SLOTS:
         // Stress test QMediaPlayer a bit to make sure it works concurrently
         // in multiple threads
         for (size_t i = 0; i < threadCount; ++i) {
-            threads[i].reset(QThread::create([&, i] { //
+            threads[i].reset(QThread::create([&] {
                 if (!playVideo(url))
                     success = false;
             }));
