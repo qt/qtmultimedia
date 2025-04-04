cba21510ff4095edbf670495e610e59ec2ba3a6d
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2025-04-04 15:45:44 +0200

    declarative-camera: Modernize permission usage
    Current declarative-camera relies on using permissions APIs in C++.
    Permissions have been exposed to QML and our examples should use them
    where applicable.
    
    This patch modifies declarative-camera example to use QML permissions
    API.
    
    Pick-to: 6.8
    Change-Id: I04dee077daa73b2aaa9bac0026d7c141bea3ceba
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>
    (cherry picked from commit 8ed6f4e34632d7ed22df7a139b9beca9db887cbe)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/multimedia/declarative-camera/qmlcamera.cpp
+++ examples/multimedia/declarative-camera/qmlcamera.cpp
@@ -5,35 +5,15 @@
 #include <QQmlEngine>
 #include <QQuickView>
 
-#if QT_CONFIG(permissions)
-  #include <QPermission>
-#endif
-
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     QQuickView view;
     view.setResizeMode(QQuickView::SizeRootObjectToView);
 
-    auto setupView = [&view](const QUrl &viewSource) {
-        // Qt.quit() called in embedded .qml by default only emits
-        // quit() signal, so do this (optionally use Qt.exit()).
-        QObject::connect(view.engine(), &QQmlEngine::quit, qApp, &QGuiApplication::quit);
-        view.setSource(viewSource);
-        view.show();
-    };
-
-#if QT_CONFIG(permissions)
-    QCameraPermission cameraPermission;
-    qApp->requestPermission(cameraPermission, [&setupView](const QPermission &permission) {
-        if (permission.status() == Qt::PermissionStatus::Denied)
-            setupView(QUrl("qrc:///permission-denied.qml"));
-        else
-            setupView(QUrl("qrc:///declarative-camera.qml"));
-    });
-#else
-    setupView();
-#endif
+    QObject::connect(view.engine(), &QQmlEngine::quit, qApp, &QGuiApplication::quit);
+    view.setSource(QUrl("qrc:///declarative-camera.qml"));
+    view.show();
 
     return app.exec();
 }
