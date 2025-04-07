d4926efd8ebc71fbfb404cdc96452e839a01e995
Artem Dyomin <artem.dyomin@qt.io>
2025-04-07 17:35:49 +0200

    Enhance checking OpenGL rendering capabilities
    If we render in non-ui thread, we have to check ThreadedOpenGL
    and PlatformOffscreenSurface qpa capabilities.
    
    Pick-to: 6.8 6.10
    Fixes: QTBUG-135598
    Task-number: QTBUG-131107
    Change-Id: I24efa91a647e2927193ef4c623e51b691fa7d731
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- src/multimedia/qthreadlocalrhi.cpp
+++ src/multimedia/qthreadlocalrhi.cpp
@@ -19,6 +19,18 @@ namespace {
 
 static thread_local QRhi::Implementation s_preferredBackend = QRhi::Null;
 
+#if QT_CONFIG(opengl)
+
+bool openGLCapsSupported(const QPlatformIntegration &qpa)
+{
+     return qpa.hasCapability(QPlatformIntegration::OpenGL) &&
+            !QCoreApplication::testAttribute(Qt::AA_ForceRasterWidgets) &&
+            (QThread::isMainThread() ||
+                (qpa.hasCapability(QPlatformIntegration::ThreadedOpenGL) &&
+                 qpa.hasCapability(QPlatformIntegration::OffscreenSurface)));
+}
+#endif
+
 class ThreadLocalRhiHolder
 {
 public:
@@ -51,8 +63,7 @@ public:
 
 #if QT_CONFIG(opengl)
             if (!m_rhi && canUseRhiImpl(QRhi::OpenGLES2, referenceBackend)) {
-                if (qpa->hasCapability(QPlatformIntegration::OpenGL)
-                    && !QCoreApplication::testAttribute(Qt::AA_ForceRasterWidgets)) {
+                if (openGLCapsSupported(*qpa)) {
 
                     m_fallbackSurface.reset(QRhiGles2InitParams::newFallbackSurface());
                     QRhiGles2InitParams params;
