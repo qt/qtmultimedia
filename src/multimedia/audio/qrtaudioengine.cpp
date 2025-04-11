ca1c0e835d750741f17ca7539aeb67ee015b4483
Tim Blechmann <tim.blechmann@qt.io>
2025-04-11 08:08:05 +0800

    Audio: introduce QRtAudioEngine
    QRtAudioEngine is a simple audio playback engine for floating
    point streams:
    * it performs mixing (summing) of streams
    * using the callback-based QAudioSink API to run voices on the real-time
    thread
    * using a simple message-passing between app and rt thread
    
    Change-Id: I6032ed3fd6a9772fb826f926a7142a4bca98d2ea
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
    Reviewed-by: Lars Sutterud <lars.sutterud@qt.io>
    (cherry picked from commit 5833064218ca351c2deb1a0a56071b02cf9b283d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/multimedia/audio/qrtaudioengine.cpp
@@ -0,0 +1,356 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qrtaudioengine_p.h"
+
+#include <QtCore/qcoreapplication.h>
+#include <QtCore/qdebug.h>
+#include <QtCore/qmutex.h>
+#include <QtCore/qthread.h>
+
+#include <QtMultimedia/private/qaudio_rtsan_support_p.h>
+#include <QtMultimedia/private/qaudiosystem_p.h>
+#include <QtMultimedia/private/qmemory_resource_tlsf_p.h>
+
+#include <QtCore/q20map.h>
+#include <mutex>
+
+#ifdef Q_CC_MINGW
+// mingw-13.1 seems to have a false positive when using std::function inside a std::variant
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_GCC("-Wmaybe-uninitialized")
+#endif
+
+QT_BEGIN_NAMESPACE
+
+namespace QtMultimediaPrivate {
+
+using namespace QtPrivate;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace {
+struct AudioDeviceFormatLess
+{
+    bool operator()(const std::pair<QAudioDevice, QAudioFormat> &lhs,
+                    const std::pair<QAudioDevice, QAudioFormat> &rhs) const
+    {
+        auto cmp = qCompareThreeWay(lhs.first.id(), rhs.first.id());
+        if (cmp == Qt::strong_ordering::less)
+            return true;
+        if (cmp == Qt::strong_ordering::greater)
+            return false;
+
+        return std::tuple(lhs.second.sampleRate(), lhs.second.sampleFormat(),
+                          lhs.second.channelCount())
+                < std::tuple(rhs.second.sampleRate(), rhs.second.sampleFormat(),
+                             rhs.second.channelCount());
+    }
+};
+} // namespace
+
+std::shared_ptr<QRtAudioEngine>
+QRtAudioEngine::getEngineFor(const QAudioDevice &device, const QAudioFormat &format)
+{
+    if (device.isNull()) {
+        qWarning() << "QRtAudioEngine needs to be called with a valid device";
+        return nullptr;
+    }
+
+    if (format.sampleFormat() != QAudioFormat::Float) {
+        qWarning() << "QRtAudioEngine requires floating point samples";
+        return nullptr;
+    }
+
+    if (!device.isFormatSupported(format)) {
+        qWarning() << "QRtAudioEngine needs to be called with a supported fromat";
+        return nullptr;
+    }
+
+    static QMutex s_playerRegistryMutex;
+    static std::map<std::pair<QAudioDevice, QAudioFormat>, std::weak_ptr<QRtAudioEngine>,
+                    AudioDeviceFormatLess>
+            s_playerRegistry;
+
+    auto guard = std::lock_guard{ s_playerRegistryMutex };
+
+    auto key = std::pair{ device, format };
+    auto found = s_playerRegistry.find(key);
+    if (found != s_playerRegistry.end()) {
+        auto player = found->second.lock();
+        if (player)
+            return player;
+    }
+
+    // lazy clean up
+    q20::erase_if(s_playerRegistry, [](auto &&keyValuePair) {
+        return keyValuePair.second.expired();
+    });
+
+    auto player = std::shared_ptr<QRtAudioEngine>(new QRtAudioEngine{ device, format },
+                                                  [](QRtAudioEngine *engine) {
+        engine->deleteLater();
+    });
+    s_playerRegistry.emplace(key, player);
+
+    return player;
+}
+
+QRtAudioEngine::QRtAudioEngine(const QAudioDevice &device, const QAudioFormat &format)
+    : m_sink{
+          device,
+          format,
+      },
+      m_rtMemoryPool {
+           std::make_unique<QTlsfMemoryResource>(poolSize)
+      }
+{
+    m_notificationEvent.callOnActivated([this] {
+        runNonRtNotifications();
+    });
+
+    if (!QThread::isMainThread()) {
+        QThread *appThread = qApp->thread();
+        moveToThread(appThread);
+        m_sink.moveToThread(appThread);
+        m_notificationEvent.moveToThread(appThread);
+    }
+
+    QPlatformAudioSink *platformSink = QPlatformAudioSink::get(m_sink);
+
+    platformSink->start([this](QSpan<float> outputBuffer) {
+        audioCallback(outputBuffer);
+    });
+
+    // we start suspended
+    platformSink->suspend();
+}
+
+QRtAudioEngine::~QRtAudioEngine()
+{
+    m_sink.reset();
+
+    // consume the ringbuffers
+    m_appToRt.consumeAll([](auto) {
+    });
+    m_rtToApp.consumeAll([](auto) {
+    });
+}
+
+void QRtAudioEngine::play(SharedVoice voice)
+{
+    auto lock = std::lock_guard{ m_mutex };
+
+    // TODO: where do we expect reampling to happen?
+    Q_ASSERT(voice->format() == m_sink.format());
+
+    if (m_voices.empty())
+        m_sink.resume();
+
+    m_voices.insert(voice);
+
+    m_appToRt.write(PlayCommand{
+            std::move(voice),
+    });
+}
+
+void QRtAudioEngine::stop(const SharedVoice &voice)
+{
+    stop(voice->voiceId());
+}
+
+void QRtAudioEngine::stop(VoiceId voiceId)
+{
+    auto lock = std::lock_guard{ m_mutex };
+    m_appToRt.write(StopCommand{ voiceId });
+}
+
+void QRtAudioEngine::visitVoiceRt(VoiceId voiceId, RtVoiceVisitor fn, bool visitorIsTrivial)
+{
+    auto lock = std::lock_guard{ m_mutex };
+
+    if (visitorIsTrivial) {
+        m_appToRt.write(VisitCommandTrivial{
+                voiceId,
+                std::move(fn),
+        });
+
+    } else {
+        m_appToRt.write(VisitCommand{
+                voiceId,
+                std::move(fn),
+        });
+    }
+}
+
+VoiceId QRtAudioEngine::allocateVoiceId()
+{
+    static std::atomic_uint64_t allocator{ 0 };
+    return VoiceId{ allocator.fetch_add(1, std::memory_order_relaxed) };
+}
+
+void QRtAudioEngine::audioCallback(QSpan<float> outputBuffer) noexcept QT_MM_NONBLOCKING
+{
+    runRtCommands();
+
+    std::fill(outputBuffer.begin(), outputBuffer.end(), 0.f);
+
+    std::vector<SharedVoice, pmr::polymorphic_allocator<SharedVoice>> finishedVoices{
+        m_rtMemoryPool.get(),
+    };
+
+    for (const SharedVoice &voice : m_rtVoiceRegistry) {
+        Q_ASSERT(voice.use_count() >= 2); // voice in both m_rtVoiceRegistry and m_voices
+
+        VoicePlayResult playResult = voice->play(outputBuffer);
+        if (playResult == VoicePlayResult::Finished)
+            finishedVoices.push_back(voice);
+    }
+
+    if (!finishedVoices.empty()) {
+        withRTSanDisabled([&] {
+            for (const SharedVoice &voice : finishedVoices) {
+                m_rtVoiceRegistry.erase(voice);
+                m_rtToApp.write(StopNotification{ voice });
+            }
+        });
+        m_notificationEvent.set();
+    }
+
+    // TODO: we should probably (soft)clip the output buffer
+
+    cleanupRetiredVoices();
+}
+
+void QRtAudioEngine::cleanupRetiredVoices() noexcept QT_MM_NONBLOCKING
+{
+    bool notifyApp = false;
+
+#if __cpp_lib_erase_if >= 202002L
+    using std::erase_if;
+#else
+    auto erase_if = [](auto &c, auto &&pred) {
+        auto old_size = c.size();
+        for (auto first = c.begin(), last = c.end(); first != last;) {
+            if (pred(*first))
+                first = c.erase(first);
+            else
+                ++first;
+        }
+        return old_size - c.size();
+    };
+#endif
+    withRTSanDisabled([&] {
+        erase_if(m_rtVoiceRegistry, [&](const SharedVoice &voice) {
+            bool voiceIsActive = voice->isActive();
+            if (!voiceIsActive) {
+                m_rtToApp.write(StopNotification{ voice });
+                notifyApp = true;
+            }
+
+            return false;
+        });
+    });
+
+    if (notifyApp)
+        m_notificationEvent.set();
+}
+
+void QRtAudioEngine::runRtCommands() noexcept QT_MM_NONBLOCKING
+{
+    m_appToRt.consumeAll([&](QSpan<RtCommand> commands) {
+        for (RtCommand &cmd : commands) {
+            std::visit([&](auto cmd) {
+                runRtCommand(std::move(cmd));
+            }, std::move(cmd));
+        }
+    });
+}
+
+void QRtAudioEngine::runRtCommand(PlayCommand cmd) noexcept QT_MM_NONBLOCKING
+{
+    withRTSanDisabled([&] {
+        m_rtVoiceRegistry.insert(cmd.voice);
+    });
+}
+
+void QRtAudioEngine::runRtCommand(StopCommand cmd) noexcept QT_MM_NONBLOCKING
+{
+    auto it = m_rtVoiceRegistry.find(cmd.voiceId);
+    Q_ASSERT(it != m_rtVoiceRegistry.end());
+
+    SharedVoice voice = *it;
+    m_rtVoiceRegistry.erase(it);
+
+    m_rtToApp.write(StopNotification{
+            std::move(voice),
+    });
+    m_notificationEvent.set();
+}
+
+void QRtAudioEngine::runRtCommand(VisitCommand cmd) noexcept QT_MM_NONBLOCKING
+{
+    auto it = m_rtVoiceRegistry.find(cmd.voiceId);
+    Q_ASSERT(it != m_rtVoiceRegistry.end());
+
+    cmd.callback(**it);
+
+    // send callback back to application for destruction
+    m_rtToApp.write(VisitReply{
+            std::move(cmd.callback),
+    });
+    m_notificationEvent.set();
+}
+
+void QRtAudioEngine::runRtCommand(VisitCommandTrivial cmd) noexcept QT_MM_NONBLOCKING
+{
+    auto it = m_rtVoiceRegistry.find(cmd.voiceId);
+    Q_ASSERT(it != m_rtVoiceRegistry.end());
+
+    cmd.callback(**it);
+}
+
+void QRtAudioEngine::runNonRtNotifications()
+{
+    std::vector<VoiceId> finishedVoices;
+    {
+        auto lock = std::lock_guard{ m_mutex };
+        m_rtToApp.consumeAll([&](QSpan<Notification> notifications) {
+            for (Notification &notification : notifications) {
+                std::visit([&](auto notification) {
+                    runNonRtNotification(std::move(notification));
+                }, std::move(notification));
+            }
+        });
+
+        finishedVoices = std::move(m_finishedVoices);
+        m_finishedVoices.clear();
+    }
+
+    // emit voiceFinished outside of the lock
+    for (VoiceId voiceId : finishedVoices)
+        emit voiceFinished(voiceId);
+}
+
+void QRtAudioEngine::runNonRtNotification(StopNotification notification)
+{
+    m_voices.erase(notification.voice);
+    if (m_voices.empty())
+        m_sink.suspend();
+    m_finishedVoices.push_back(notification.voice->voiceId());
+}
+
+void QRtAudioEngine::runNonRtNotification(VisitReply)
+{
+    // nop (just making sure to delete on the application thread);
+}
+
+} // namespace QtMultimediaPrivate
+
+QT_END_NAMESPACE
+
+#ifdef Q_CC_MINGW
+QT_WARNING_POP
+#endif
+
+#include "moc_qrtaudioengine_p.cpp"
---
+++ src/multimedia/audio/qrtaudioengine_p.h
@@ -0,0 +1,284 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QRTAUDIOENGINE_P_H
+#define QRTAUDIOENGINE_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtCore/qtclasshelpermacros.h>
+#include <QtCore/qmutex.h>
+
+#include <QtMultimedia/qaudiosink.h>
+#include <QtMultimedia/qtmultimediaglobal.h>
+#include <QtMultimedia/private/qaudio_rtsan_support_p.h>
+#include <QtMultimedia/private/qaudioringbuffer_p.h>
+#include <QtMultimedia/private/qautoresetevent_p.h>
+#include <QtMultimedia/private/q_pmr_emulation_p.h>
+
+#include <cstdint>
+#include <set>
+#include <variant>
+
+QT_BEGIN_NAMESPACE
+
+namespace QtMultimediaPrivate {
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ID to uniquely identify a voice
+enum class VoiceId : uint64_t
+{
+};
+
+enum class VoicePlayResult : uint8_t
+{
+    Playing,
+    Finished,
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Q_MULTIMEDIA_EXPORT QRtAudioEngineVoice
+{
+public:
+    using VoicePlayResult = QtMultimediaPrivate::VoicePlayResult;
+    using VoiceId = QtMultimediaPrivate::VoiceId;
+
+    explicit QRtAudioEngineVoice(VoiceId id) : m_voiceId{ id } { }
+    Q_DISABLE_COPY_MOVE(QRtAudioEngineVoice)
+    virtual ~QRtAudioEngineVoice() = default;
+
+    // once play() returns finished or isActive is false, the QAudioPlaybackEngine will stop the
+    // voice
+    [[nodiscard]] virtual VoicePlayResult play(QSpan<float>) noexcept QT_MM_NONBLOCKING = 0;
+    virtual bool isActive() noexcept QT_MM_NONBLOCKING = 0;
+
+    virtual const QAudioFormat &format() noexcept = 0;
+
+    VoiceId voiceId() const { return m_voiceId; }
+
+private:
+    const VoiceId m_voiceId;
+};
+
+struct QRtAudioEngineVoiceCompare : std::less<uint64_t>
+{
+    using std::less<uint64_t>::operator();
+    template <typename Lhs, typename Rhs>
+    bool operator()(const Lhs &lhs, const Rhs &rhs) const
+    {
+        return operator()(getId(lhs), getId(rhs));
+    }
+
+    static uint64_t getId(VoiceId arg) { return qToUnderlying(arg); }
+    static uint64_t getId(const QRtAudioEngineVoice &arg) { return getId(arg.voiceId()); }
+    static uint64_t getId(const std::shared_ptr<QRtAudioEngineVoice> &arg) { return getId(*arg); }
+
+    using is_transparent = std::true_type;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Impl {
+template <typename T>
+struct visitor_arg;
+
+template <typename R, typename Arg>
+struct visitor_arg<R(Arg)>
+{
+    using type = Arg;
+};
+
+template <typename R, typename Arg>
+struct visitor_arg<R (*)(Arg)>
+{
+    using type = Arg;
+};
+
+template <typename F>
+struct visitor_arg : visitor_arg<decltype(&F::operator())>
+{
+};
+
+template <typename C, typename R, typename Arg>
+struct visitor_arg<R (C::*)(Arg) const>
+{
+    using type = Arg;
+};
+
+template <typename C, typename R, typename Arg>
+struct visitor_arg<R (C::*)(Arg)>
+{
+    using type = Arg;
+};
+
+} // namespace Impl
+
+template <typename F>
+using visitor_arg_t = typename Impl::visitor_arg<F>::type;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// playback engine for QRtAudioEngineVoice instances
+// keeps a QAudioSink alive, but in a suspended state if no voices are playing
+class Q_MULTIMEDIA_EXPORT QRtAudioEngine final : public QObject
+{
+public:
+    using RtVoiceVisitor = std::function<void(QRtAudioEngineVoice &)>;
+    using SharedVoice = std::shared_ptr<QRtAudioEngineVoice>;
+
+    Q_OBJECT
+
+    // commands (app->rt)
+    struct PlayCommand
+    {
+        SharedVoice voice;
+    };
+
+    struct StopCommand
+    {
+        const VoiceId voiceId;
+    };
+
+    // visitors are sent back to the non-rt thread, so that they are destroyed in a safe context
+    struct VisitCommand
+    {
+        const VoiceId voiceId;
+        RtVoiceVisitor callback;
+    };
+
+    // "trivial" visitors are not sent back to the non-rt thread
+    struct VisitCommandTrivial
+    {
+        const VoiceId voiceId;
+        RtVoiceVisitor callback;
+    };
+
+    using RtCommand = std::variant<PlayCommand, StopCommand, VisitCommand, VisitCommandTrivial>;
+
+    // notifications (rt->app)
+    struct StopNotification
+    {
+        SharedVoice voice;
+    };
+
+    struct VisitReply
+    {
+        RtVoiceVisitor callback;
+    };
+
+    using Notification = std::variant<StopNotification, VisitReply>;
+
+public:
+    // we keep a pool of engines with one engine per device/format
+    static std::shared_ptr<QRtAudioEngine> getEngineFor(const QAudioDevice &, const QAudioFormat &);
+
+    QRtAudioEngine(const QAudioDevice &, const QAudioFormat &);
+    Q_DISABLE_COPY_MOVE(QRtAudioEngine)
+    ~QRtAudioEngine() override;
+
+    // play/stop/visitVoiceRT are thread-safe
+    void play(SharedVoice);
+    void stop(const SharedVoice &);
+    void stop(VoiceId);
+
+    template <typename Visitor>
+    void visitVoiceRt(VoiceId id, Visitor visitor)
+    {
+        using visitorArg = visitor_arg_t<Visitor>;
+        static_assert(std::is_reference_v<visitorArg>);
+
+        // we need to prevent that the visitor function is going to be destroyed on the real-time
+        // thread, unless:
+        // * it can be trivially destroyed
+        // * it is sufficiently small to fit into the small-buffer-optimization of std::function.
+        //   we don't know what the value is, so we are conservative and assume only the size of a
+        //   pointer (we could relax it with something like std::inplace_function)
+        constexpr size_t smallBufferOptimizationEstimate = 2 * sizeof(void *);
+        constexpr bool visitorIsTrivial = std::is_trivially_destructible_v<std::decay_t<Visitor>>
+                && sizeof(Visitor) <= smallBufferOptimizationEstimate;
+
+        auto wrapped = [visitor = std::move(visitor)](QRtAudioEngineVoice &voice) {
+            visitor(static_cast<visitorArg>(voice));
+        };
+        visitVoiceRt(id, RtVoiceVisitor{ wrapped }, visitorIsTrivial);
+    }
+
+    template <typename Visitor>
+    void visitVoiceRt(const SharedVoice &voice, Visitor visitor)
+    {
+        visitVoiceRt(voice->voiceId(), std::move(visitor));
+    }
+
+    static VoiceId allocateVoiceId();
+
+    std::unique_ptr<pmr::memory_resource> &rtMemoryResource() { return m_rtMemoryPool; }
+
+    // testing
+    QAudioSink &audioSink() { return m_sink; }
+    const auto &voices() const { return m_voices; }
+
+Q_SIGNALS:
+    void voiceFinished(VoiceId);
+
+private:
+    void visitVoiceRt(VoiceId, RtVoiceVisitor, bool visitorIsTrivial);
+
+    void audioCallback(QSpan<float>) noexcept QT_MM_NONBLOCKING;
+    void cleanupRetiredVoices() noexcept QT_MM_NONBLOCKING;
+
+    void runRtCommands() noexcept QT_MM_NONBLOCKING;
+    void runRtCommand(PlayCommand) noexcept QT_MM_NONBLOCKING;
+    void runRtCommand(StopCommand) noexcept QT_MM_NONBLOCKING;
+    void runRtCommand(VisitCommand) noexcept QT_MM_NONBLOCKING;
+    void runRtCommand(VisitCommandTrivial) noexcept QT_MM_NONBLOCKING;
+
+    void runNonRtNotifications();
+    void runNonRtNotification(StopNotification);
+    void runNonRtNotification(VisitReply);
+
+    QAudioSink m_sink;
+
+    QMutex m_mutex;
+
+    // Application side
+    std::set<SharedVoice, QRtAudioEngineVoiceCompare> m_voices;
+
+    // Rt memory
+    // Note: when the memory pool is exhausted, we fall back to the system allocator. Not great for
+    // real-time uses, but a simple fallback strategy
+    static constexpr size_t poolSize = 128 * 1024; // 128kb
+    std::unique_ptr<pmr::memory_resource> m_rtMemoryPool;
+
+    // Voice registry on the real-time thread:
+    // invariant: every voice in m_rtVoiceRegistry is also in m_voices
+    using VoiceRegistry = std::set<SharedVoice, QRtAudioEngineVoiceCompare,
+                                   pmr::polymorphic_allocator<SharedVoice>>;
+    VoiceRegistry m_rtVoiceRegistry{
+        m_rtMemoryPool.get(),
+    };
+
+    // rt/nrt communication
+    static constexpr size_t commandBuffersSize = 2048;
+    QtPrivate::QAudioRingBuffer<RtCommand> m_appToRt{ commandBuffersSize };
+    QtPrivate::QAudioRingBuffer<Notification> m_rtToApp{ commandBuffersSize };
+
+    QtPrivate::QAutoResetEvent m_notificationEvent;
+    std::vector<VoiceId> m_finishedVoices;
+};
+
+} // namespace QtMultimediaPrivate
+
+QT_END_NAMESPACE
+
+#endif // QRTAUDIOENGINE_P_H
