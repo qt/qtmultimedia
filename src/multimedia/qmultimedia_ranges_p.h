6b6b129869f747750d92f94c1b13511caf70d286
Tim Blechmann <tim.blechmann@qt.io>
2025-04-11 10:13:49 +0800

    Utilities: add some ranges algorithms to QtMultimediaPrivate
    Adding a new QtMultimediaPrivate::ranges namespace for some ranges. It
    is not feature-complete, but we can add functionality as needed
    
    Change-Id: Ifc3da46f70d08167ae4686f6f135c77691670b59
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
    Reviewed-by: Mikko Hallamaa <mikko.hallamaa@qt.io>
    (cherry picked from commit 222317256bb620a75312d35138238e78a311d660)
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>

---
+++ src/multimedia/qmultimedia_ranges_p.h
@@ -0,0 +1,47 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QMULTIMEDIA_RANGES_P_H
+#define QMULTIMEDIA_RANGES_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtCore/qtconfigmacros.h>
+
+#ifdef __cpp_lib_ranges
+#  include <ranges> // IWYU pragma: export
+#endif
+
+#include <algorithm>
+
+QT_BEGIN_NAMESPACE
+
+namespace QtMultimediaPrivate::ranges {
+
+#ifdef __cpp_lib_ranges
+using std::ranges::equal;
+#else
+
+// Caveat: best effort, not a 1-to-1 mapping to c++20 style ranges
+
+constexpr auto equal = [](const auto &lhs, const auto &rhs, auto &&predicate) {
+    return std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end(), predicate);
+};
+
+#endif
+
+} // namespace QtMultimediaPrivate::ranges
+
+QT_END_NAMESPACE
+
+#endif // QMULTIMEDIA_RANGES_P_H
+
