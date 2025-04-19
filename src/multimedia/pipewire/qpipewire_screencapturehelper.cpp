30416214f8de1310ed5b72a48410affff1dc587a
Tim Blechmann <tim.blechmann@qt.io>
2025-04-19 08:45:20 +0800

    Pipewire: silence unused-value warnings from pipewire macros
    Change-Id: I44e01a2f3154bf60119f865d9aceaf4a8012938b
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
    (cherry picked from commit 09b4c8082ab20c5870fed642d61176dad4a289ea)
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>

--- src/multimedia/pipewire/qpipewire_screencapturehelper.cpp
+++ src/multimedia/pipewire/qpipewire_screencapturehelper.cpp
@@ -36,6 +36,10 @@
 
 #include <fcntl.h>
 
+// pipewire's macros tend to emit unused value warnings
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_CLANG("-Wunused-value")
+
 QT_BEGIN_NAMESPACE
 
 using namespace Qt::StringLiterals;
@@ -932,3 +936,5 @@ spa_video_format QPipeWireCaptureHelper::toSpaVideoFormat(QVideoFrameFormat::Pix
 } // namespace QtPipeWire
 
 QT_END_NAMESPACE
+
+QT_WARNING_POP
