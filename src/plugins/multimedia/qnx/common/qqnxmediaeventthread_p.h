d5587d7c3926999016a5c73380e61faaf5c4494a
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-05-16 19:43:32 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the source files. Examples, tests, documentation,
    or build files are not updated.
    
    Modified licenseRule.json and REUSE.toml accordingly.
    Removed unsued license text files under LICENSES.
    
    Task-number: QTBUG-136484
    Change-Id: Ia278939b89945c7ecf09c6f72ca6462d91db6428
    
    Conflicts solved (these files were removed in
    a385d74a81a1dae622db7d2a2a6db53533a8f326):
    	src/multimedia/audio/qaudiostatemachine.cpp
    	src/multimedia/audio/qaudiostatemachine_p.h
    	src/multimedia/audio/qaudiostatemachineutils_p.h
    
    Change-Id: I09ee6bc5621fe8d62ab0da7b5d46039c3ab7b524
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- src/plugins/multimedia/qnx/common/qqnxmediaeventthread_p.h
+++ src/plugins/multimedia/qnx/common/qqnxmediaeventthread_p.h
@@ -1,6 +1,6 @@
 // Copyright (C) 2017 QNX Software Systems. All rights reserved.
 // Copyright (C) 2021 The Qt Company
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #ifndef QQNXMEDIAEVENTTHREAD_P_H
 #define QQNXMEDIAEVENTTHREAD_P_H
