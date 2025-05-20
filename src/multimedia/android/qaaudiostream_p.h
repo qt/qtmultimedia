65909534d5e5168ce21ae97f9e3456baf5454380
Mikko Hallamaa <mikko.hallamaa@qt.io>
2025-05-20 15:47:26 +0200

    Android: Add classes for handling AAudio streams and builders
    This patch adds classes QtAAudio::Stream and QtAAudio::StreamBuilder,
    which are RAII wrappers of AAudioStream and AAudioStreamBuilder
    respectively.
    
    QtAAudio::StreamBuilder can be set up to open a AAudio stream with
    specified parameters. QtAAudio::Stream is used to send requests to
    operate on the underlying AAudioStream resources.
    
    Task-number: QTBUG-132951
    Change-Id: I654c6830ff8ca9219926f533cefc99ebbcf1b1c1
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 6b2aa8af9f46e0b90f84dba5eea3e596da80a933)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/multimedia/android/qaaudiostream_p.h
@@ -0,0 +1,105 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QAAUDIOSTREAM_P_H
+#define QAAUDIOSTREAM_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <aaudio/AAudio.h>
+#include <QtMultimedia/qaudioformat.h>
+
+QT_BEGIN_NAMESPACE
+
+namespace QtAAudio {
+
+Q_DECLARE_LOGGING_CATEGORY(qLcAAudioStream)
+
+struct StreamBuilder;
+
+struct Stream
+{
+    explicit Stream(const StreamBuilder &builder);
+    ~Stream();
+
+    Q_DISABLE_COPY_MOVE(Stream)
+
+    bool start();
+    void stop();
+    void pause();
+    void flush();
+
+    bool isOpen();
+    bool areFormatParametersRespected();
+    bool areStreamParametersRespected();
+
+private:
+    void close();
+
+    aaudio_result_t waitForTargetState(aaudio_stream_state_t targetState);
+
+    template <typename Functor>
+    aaudio_result_t requestWithExpectedState(Functor &&request, aaudio_stream_state_t expected);
+
+    // stream members
+    AAudioStream *m_stream{ nullptr };
+    bool m_areFormatParametersRespected{ false };
+    bool m_areStreamParametersRespected{ false };
+};
+
+struct FormatParameterSet
+{
+    int32_t sampleRate{ AAUDIO_UNSPECIFIED };
+    int32_t channelCount{ AAUDIO_UNSPECIFIED };
+    aaudio_format_t sampleFormat{ AAUDIO_UNSPECIFIED };
+};
+
+struct StreamParameterSet
+{
+    aaudio_sharing_mode_t sharingMode = AAUDIO_SHARING_MODE_SHARED;
+    aaudio_performance_mode_t performanceMode = AAUDIO_PERFORMANCE_MODE_NONE;
+    aaudio_direction_t direction = AAUDIO_DIRECTION_OUTPUT;
+    aaudio_usage_t outputUsage = AAUDIO_USAGE_MEDIA;
+    aaudio_content_type_t outputContentType = AAUDIO_CONTENT_TYPE_MUSIC;
+    aaudio_input_preset_t inputPreset = AAUDIO_INPUT_PRESET_VOICE_RECOGNITION;
+};
+
+struct StreamBuilder
+{
+    friend Stream;
+
+    StreamBuilder(QAudioFormat format);
+    ~StreamBuilder();
+
+    Q_DISABLE_COPY_MOVE(StreamBuilder)
+
+    static FormatParameterSet parseFormat(const QAudioFormat &format);
+
+    QAudioFormat format;
+    AAudioStream_dataCallback callback;
+    AAudioStream_errorCallback errorCallback;
+    void *userData{ nullptr };
+    StreamParameterSet params;
+    int32_t deviceId{ AAUDIO_UNSPECIFIED };
+    int32_t bufferCapacity{ AAUDIO_UNSPECIFIED };
+
+    void setupBuilder();
+
+private:
+    AAudioStreamBuilder *m_builder{ nullptr };
+};
+
+} // namespace QtAAudio
+
+QT_END_NAMESPACE
+
+#endif
