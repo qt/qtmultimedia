5b1a129e91cd04a50f373c23a7bb2ee035dd0f32
Tim Blechmann <tim.blechmann@qt.io>
2025-05-30 15:25:21 +0800

    FFmpeg: set name of QFFmpegAudioInput thread
    This makes it a little easier to navigate threads in a debugger.
    
    Change-Id: I0538cb1866fe61fa931daf822a7a8cdef3387b7e
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 110d01fa88036fb2185844d97f1e44dcf18daf38)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 095e3b8882a2f4883627f135551b6c02386dbcd3)

--- src/plugins/multimedia/ffmpeg/qffmpegaudioinput.cpp
+++ src/plugins/multimedia/ffmpeg/qffmpegaudioinput.cpp
@@ -172,6 +172,7 @@ QFFmpegAudioInput::QFFmpegAudioInput(QAudioInput *qq)
     qRegisterMetaType<QAudioBuffer>();
 
     m_inputThread = std::make_unique<QThread>();
+    m_inputThread->setObjectName(QStringLiteral("QFFmpegAudioInputThread"));
     m_audioIO = new QFFmpeg::AudioSourceIO(this);
     m_audioIO->moveToThread(m_inputThread.get());
     m_inputThread->start();
