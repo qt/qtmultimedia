8bb8b91af074b24a827513ccf16efb1f6721cb2e
Tim Blechmann <tim.blechmann@qt.io>
2025-06-04 10:49:57 +0800

    FFmpeg: Muxer - fix memory leak
    `av_interleaved_write_frame` does not take ownership of the `AVPacket`,
    but of its `AVBufferRef *buf` member.
    
    Pick-to: 6.5
    Change-Id: Ic16ed2b146a05b723ab33115ec83a5a598167984
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit dc8e6d9e9082fd87357b8f53aa885321308a1808)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 117fe91ff2cb3d0df07070006c1f8b79412c3ce9)
    (cherry picked from commit b4c24377f5b04c17caac0f179e0fb35933d7839e)

--- src/plugins/multimedia/ffmpeg/recordingengine/qffmpegmuxer.cpp
+++ src/plugins/multimedia/ffmpeg/recordingengine/qffmpegmuxer.cpp
@@ -56,8 +56,22 @@ void Muxer::processOne()
     //   qCDebug(qLcFFmpegEncoder) << "writing packet to file" << packet->pts << packet->duration <<
     //   packet->stream_index;
 
-    // the function takes ownership for the packet
-    av_interleaved_write_frame(m_encoder->avFormatContext(), packet.release());
+    // from AVPacket Struct Reference:
+    // "The semantics of data ownership depends on the buf field. If it is set, the packet data is
+    // dynamically allocated and is valid indefinitely until a call to av_packet_unref() reduces the
+    // reference count to 0."
+    //
+    // from av_interleaved_write_frame:
+    // "If the packet is reference-counted, this function will take ownership of this reference and
+    // unreference it later when it sees fit. The caller must not access the data through this
+    // reference after this function returns. If the packet is not reference-counted, libavformat
+    // will make a copy."
+    //
+    // This means that av_interleaved_write_frame takes ownership of the AVBufferRef field, not the
+    // whole AVPacket.
+
+    av_interleaved_write_frame(m_encoder->avFormatContext(), packet.get());
+    Q_ASSERT(packet->buf == nullptr); // av_interleaved_write_frame took ownership of the buffer
 }
 
 } // namespace QFFmpeg
