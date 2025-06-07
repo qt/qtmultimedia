6a3658c78010062e35fc5d72473c20dd066bb66c
Tim Blechmann <tim.blechmann@qt.io>
2025-06-07 08:07:25 +0800

    PulseAudio: wait for suspend/resume operations
    We should ensure that suspend/resume are synchronous by blocking:
    otherwise audio data may still be processed after calling suspend()
    
    Pick-to: 6.8
    Change-Id: Icf70ac2e2c2eb56763ee61fbfe0a2c312c526766
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>
    (cherry picked from commit af081ed037b2fa2a725debc4320511653edd5b01)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/multimedia/pulseaudio/qpulseaudiosink.cpp
+++ src/multimedia/pulseaudio/qpulseaudiosink.cpp
@@ -167,7 +167,7 @@ void QPulseAudioSinkStream::suspend()
     QPulseAudioContextManager *pulseEngine = QPulseAudioContextManager::instance();
     std::unique_lock engineLock{ *pulseEngine };
 
-    pa_stream_cork(m_stream.get(), 1, nullptr, nullptr);
+    pulseEngine->waitForAsyncOperation(pa_stream_cork(m_stream.get(), 1, nullptr, nullptr));
 }
 
 void QPulseAudioSinkStream::resume()
@@ -175,7 +175,7 @@ void QPulseAudioSinkStream::resume()
     QPulseAudioContextManager *pulseEngine = QPulseAudioContextManager::instance();
     std::unique_lock engineLock{ *pulseEngine };
 
-    pa_stream_cork(m_stream.get(), 0, nullptr, nullptr);
+    pulseEngine->waitForAsyncOperation(pa_stream_cork(m_stream.get(), 0, nullptr, nullptr));
 }
 
 bool QPulseAudioSinkStream::open() const
