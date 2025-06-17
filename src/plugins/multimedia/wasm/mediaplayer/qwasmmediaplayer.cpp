qt/qtmultimedia

commit e71aec0fff4765d20050d52abea6040e84ea9aa7

wasm: fix audio controls on video player
When a QMediaPlayer is playing a video with audio output
specified, the underlying video element volume needs to be set
instead of the audio (element) volume.

Also fix audio/video selection when file as detected by Qt, is
application/octet-stream.

Fixes: QTBUG-120671
Pick-to: 6.7
Change-Id: I92b9677e5b0c962665974b0da7ca03b2348d82b0
Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

src/plugins/multimedia/wasm/mediaplayer/qwasmmediaplayer.cpp
@@ -209,12 +209,13 @@ void QWasmMediaPlayer::setMedia(const QUrl &mediaContent, QIODevice *stream)
     if (mediaContent.isEmpty()) {
         if (stream) {
             m_mediaStream = stream;
-            if (db.mimeTypeForData(stream).name().contains("video")) {
-                setVideoAvailable(true);
-                m_videoOutput->setSource(m_mediaStream);
-            }  else {
+            qCDebug(lcMediaPlayer) << db.mimeTypeForData(stream).name();
+            if (db.mimeTypeForData(stream).name().contains("audio")) {
                 setAudioAvailable(true);
                 m_audioOutput->setSource(m_mediaStream);
+            }  else { // treat octet-stream as video
+                setVideoAvailable(true);
+                m_videoOutput->setSource(m_mediaStream);
             }
         } else {
 
@@ -222,12 +223,13 @@ void QWasmMediaPlayer::setMedia(const QUrl &mediaContent, QIODevice *stream)
         }
     } else {
         QString sourceFile = mediaContent.toLocalFile();
-        if (db.mimeTypeForFile(QFileInfo(sourceFile)).name().contains("video")) {
-                setVideoAvailable(true);
-            m_videoOutput->setSource(mediaContent);
-        } else {
+        qCDebug(lcMediaPlayer) << db.mimeTypeForFile(QFileInfo(sourceFile)).name();
+        if (db.mimeTypeForFile(QFileInfo(sourceFile)).name().contains("audio")) {
             setAudioAvailable(true);
             m_audioOutput->setSource(mediaContent);
+        } else { // treat octet-stream as video
+            setVideoAvailable(true);
+            m_videoOutput->setSource(mediaContent);
         }
     }
 
@@ -246,6 +248,9 @@ void QWasmMediaPlayer::setVideoSink(QVideoSink *sink)
 
     initVideo();
     m_videoOutput->setSurface(sink);
+    setVideoAvailable(true);
+    if (isAudioAvailable() && m_audioOutput)
+        m_audioOutput->setVideoElement(m_videoOutput->currentVideoElement());
 }
 
 void QWasmMediaPlayer::setAudioOutput(QPlatformAudioOutput *output)
@@ -256,6 +261,7 @@ void QWasmMediaPlayer::setAudioOutput(QPlatformAudioOutput *output)
     if (m_audioOutput)
         m_audioOutput->q->disconnect(this);
     m_audioOutput = static_cast<QWasmAudioOutput *>(output);
+    setAudioAvailable(true);
 }
 
 void QWasmMediaPlayer::updateAudioDevice()

src/plugins/multimedia/wasm/common/qwasmaudiooutput.cpp
@@ -30,8 +30,23 @@ void QWasmAudioOutput::setAudioDevice(const QAudioDevice &audioDevice)
     device = audioDevice;
 }
 
+void QWasmAudioOutput::setVideoElement(emscripten::val videoElement)
+{
+    m_videoElement = videoElement;
+}
+
+emscripten::val QWasmAudioOutput::videoElement()
+{
+    return m_videoElement;
+}
+
 void QWasmAudioOutput::setMuted(bool muted)
 {
+    emscripten::val realElement = videoElement();
+    if (!realElement.isUndefined()) {
+        realElement.set("muted", muted);
+        return;
+    }
     if (m_audio.isUndefined() || m_audio.isNull()) {
         qCDebug(qWasmMediaAudioOutput) << "Error"
                                        << "Audio element could not be created";
@@ -44,14 +59,20 @@ void QWasmAudioOutput::setMuted(bool muted)
 
 void QWasmAudioOutput::setVolume(float volume)
 {
+    volume = qBound(qreal(0.0), volume, qreal(1.0));
+    emscripten::val realElement = videoElement();
+    if (!realElement.isUndefined()) {
+        realElement.set("volume", volume);
+        return;
+    }
     if (m_audio.isUndefined() || m_audio.isNull()) {
         qCDebug(qWasmMediaAudioOutput) << "Error"
-                                       << "Audio element could not be created";
+                                       << "Audio element not available";
         emit errorOccured(QMediaPlayer::ResourceError,
                           QStringLiteral("Media file could not be opened"));
         return;
     }
-    volume = qBound(qreal(0.0), volume, qreal(1.0));
+
     m_audio.set("volume", volume);
 }
 

src/plugins/multimedia/wasm/common/qwasmaudiooutput_p.h
@@ -51,6 +51,7 @@ public:
 
     void setSource(const QUrl &url);
     void setSource(QIODevice *stream);
+    void setVideoElement(emscripten::val videoElement);
 
 Q_SIGNALS:
     void readyChanged(bool);
@@ -66,6 +67,8 @@ Q_SIGNALS:
 private:
     void doElementCallbacks();
     void createAudioElement(const std::string &id);
+
+    emscripten::val videoElement();
 
     QScopedPointer<QWasmAudioSink> m_sink;
     QScopedPointer<qstdweb::EventCallback> m_playEvent;
@@ -84,6 +87,7 @@ private:
     QString m_source;
     QIODevice *m_audioIODevice = nullptr;
     emscripten::val m_audio = emscripten::val::undefined();
+    emscripten::val m_videoElement = emscripten::val::undefined();
     QMediaPlayer::MediaStatus m_currentMediaStatus;
     qreal m_currentBufferedValue;
 };

src/plugins/multimedia/wasm/common/qwasmvideooutput.cpp
@@ -661,7 +661,6 @@ void QWasmVideoOutput::doElementCallbacks()
     // emptied
     auto emptiedCallback = [=](emscripten::val event) {
         Q_UNUSED(event)
-        checkNetworkState();
         qCDebug(qWasmMediaVideoOutput) << "emptied";
         emit readyChanged(false);
         m_currentMediaStatus = QMediaPlayer::EndOfMedia;

src/plugins/multimedia/wasm/common/qwasmvideooutput_p.h
@@ -87,6 +87,8 @@ public:
     // mediacapturesession has the videosink
     QVideoSink *m_wasmSink = nullptr;
 
+    emscripten::val currentVideoElement() { return m_video; }
+
 Q_SIGNALS:
     void readyChanged(bool);
     void bufferingChanged(qint32 percent);

commit ff04902150d876de50795dda8dbf6b8fdc2be548

wasm: fix http streaming
This fixes a regression introduced by e71aec0 by restoring old code
for web urls.

Fixes: QTBUG-136921
Change-Id: Id98c51fc3b70822c364c04cd7d87f032d240eba3
Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

src/plugins/multimedia/wasm/mediaplayer/qwasmmediaplayer.cpp
@@ -227,14 +227,24 @@ void QWasmMediaPlayer::setMedia(const QUrl &mediaContent, QIODevice *stream)
             setMediaStatus(QMediaPlayer::NoMedia);
         }
     } else {
-        QString sourceFile = mediaContent.toLocalFile();
-        qCDebug(lcMediaPlayer) << db.mimeTypeForFile(QFileInfo(sourceFile)).name();
-        if (db.mimeTypeForFile(QFileInfo(sourceFile)).name().contains(u"audio"_s)) {
-            setAudioAvailable(true);
-            m_audioOutput->setSource(mediaContent);
-        } else { // treat octet-stream as video
-            setVideoAvailable(true);
-            m_videoOutput->setSource(mediaContent);
+        if (mediaContent.isLocalFile()) {
+            QString sourceFile = mediaContent.toLocalFile();
+            if (db.mimeTypeForFile(QFileInfo(sourceFile)).name().contains(u"audio"_s)) {
+                setAudioAvailable(true);
+                m_audioOutput->setSource(mediaContent);
+            } else { // treat octet-stream as video
+                setVideoAvailable(true);
+                m_videoOutput->setSource(mediaContent);
+            }
+        } else {
+            // web url
+            if (!m_videoOutput->currentVideoElement().isNull()) {
+                m_videoOutput->setSource(mediaContent);
+                setVideoAvailable(true);
+            } else {
+                m_audioOutput->setSource(mediaContent);
+                setAudioAvailable(true);
+            }
         }
     }
 