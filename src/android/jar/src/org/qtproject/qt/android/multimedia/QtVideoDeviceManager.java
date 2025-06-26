cc9a465d7f5b3b5c88d9847ad70f628ccbce46af
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2025-06-26 09:10:50 +0200

    Android, camera: Simplify isAfModeSupported
    The affected method used to be called from native code, and had the
    behavior that it should return true if the camera supported a specific
    auto-focus mode, and also had a working implementation for it.
    
    This method is no longer being used from the C++ thread, and we can
    simplify the rules to only consider if it's available, not whether
    it has a working implementation.
    
    Pick-to: 6.8
    Change-Id: Ieed0c56cebfea0c32227b8927f978e1af6a41386
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 6b9443a8b5c3f3e4334d11ad74f645d9801ae1ae)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 311e6cc9613638be3c0772afc51c4e82268cd9e5)

--- src/android/jar/src/org/qtproject/qt/android/multimedia/QtVideoDeviceManager.java
+++ src/android/jar/src/org/qtproject/qt/android/multimedia/QtVideoDeviceManager.java
@@ -239,27 +239,12 @@ class QtVideoDeviceManager {
         return characteristicsValue != null ? characteristicsValue : new int[0];
     }
 
-    // Returns true if the afMode is both available and we have a working implementation
-    // for it.
-    @UsedFromNativeCode
-    boolean isAfModeSupported(String cameraId, int afMode) {
+    boolean isAfModeAvailable(String cameraId, int afMode) {
         if (cameraId == null || cameraId.isEmpty())
             return false;
-
-        final boolean available = Arrays
+        return Arrays
             .stream(getAllAvailableAfModes(cameraId))
             .anyMatch(value -> value == afMode);
-
-        if (available) {
-            if (afMode == CameraCharacteristics.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
-                return true;
-            else if (afMode == CameraCharacteristics.CONTROL_AF_MODE_OFF
-                && isManualFocusDistanceSupported(cameraId))
-                return true;
-        }
-
-
-        return false;
     }
 
     // Returns supported QCamera::FocusModes as strings. I.e FocusModeAuto becomes "FocusModeAuto".
@@ -273,11 +258,11 @@ class QtVideoDeviceManager {
         ArrayList<String> outList = new ArrayList<String>();
 
         // FocusModeAuto maps to the CONTINUOUS_PICTURE mode.
-        if (isAfModeSupported(cameraId, CameraCharacteristics.CONTROL_AF_MODE_CONTINUOUS_PICTURE)) {
+        if (isAfModeAvailable(cameraId, CameraCharacteristics.CONTROL_AF_MODE_CONTINUOUS_PICTURE)) {
             outList.add("FocusModeAuto");
         }
 
-        if (isAfModeSupported(cameraId, CameraCharacteristics.CONTROL_AF_MODE_OFF)
+        if (isAfModeAvailable(cameraId, CameraCharacteristics.CONTROL_AF_MODE_OFF)
             && isManualFocusDistanceSupported(cameraId))
             outList.add("FocusModeManual");
 
