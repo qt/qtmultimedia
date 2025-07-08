ef406081ca9cc08ee649b09f9599f4a4c30aad40
Tim Blechmann <tim.blechmann@qt.io>
2025-07-08 12:15:22 +0800

    Tests: add unit test for windows resampler
    Pick-to: 6.8
    Change-Id: I1b99077000ae8a04d32df8ff5912abf0bd29f294
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit d26c1a826e781c01d7447c50dd44fb1955d2965f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d9aee9e4f0170f4f7f4bbb686b66d9a0360b670b)

---
+++ tests/auto/unit/multimedia/qwindowsresampler/tst_qwindowsresampler.cpp
@@ -0,0 +1,93 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include <QtTest/QtTest>
+
+#include <QtMultimedia/private/qwindowsresampler_p.h>
+#include <QtMultimedia/private/qmultimedia_ranges_p.h>
+
+namespace ranges = QtMultimediaPrivate::ranges;
+
+QAudioFormat makeAudioFormat(int sampleRate, int numberOfChannels, QAudioFormat::SampleFormat fmt)
+{
+    QAudioFormat ret;
+    ret.setSampleFormat(fmt);
+    ret.setChannelCount(numberOfChannels);
+    ret.setSampleRate(sampleRate);
+    ret.setChannelConfig(QAudioFormat::defaultChannelConfigForChannelCount(numberOfChannels));
+    return ret;
+}
+
+template <typename Container>
+auto asByteSpan(Container &c)
+{
+    return as_bytes(QSpan(c));
+}
+
+class tst_QWindowsResampler : public QObject
+{
+    Q_OBJECT
+
+public slots:
+    void initTestCase();
+
+private slots:
+    void resample_QByteArray_mono_to_stereo();
+    void resample_QByteArray_stereo_to_mono();
+
+private:
+    std::unique_ptr<QWindowsResampler> dut;
+};
+
+void tst_QWindowsResampler::initTestCase()
+{
+    dut = std::make_unique<QWindowsResampler>();
+}
+
+void tst_QWindowsResampler::resample_QByteArray_mono_to_stereo()
+{
+    auto formatMono = makeAudioFormat(44100, 1, QAudioFormat::Float);
+    auto formatStereo = makeAudioFormat(44100, 2, QAudioFormat::Float);
+
+    dut->setup(formatMono, formatStereo);
+
+    constexpr std::array monoData{
+        0.1f, 0.2f, 0.3f, 0.4f, 0.5f,
+    };
+    constexpr std::array stereoData{
+        0.1f, 0.1f, 0.2f, 0.2f, 0.3f, 0.3f, 0.4f, 0.4f, 0.5f, 0.5f,
+    };
+
+    auto result = dut->resample(as_bytes(QSpan(monoData)));
+
+    QCOMPARE(size_t(result.size()), size_t(formatStereo.bytesForFrames(qint32(monoData.size()))));
+    QCOMPARE(asByteSpan(result).size(), asByteSpan(stereoData).size());
+    QVERIFY(ranges::equal(asByteSpan(result), asByteSpan(stereoData), std::equal_to<>{}));
+}
+
+void tst_QWindowsResampler::resample_QByteArray_stereo_to_mono()
+{
+    auto formatMono = makeAudioFormat(44100, 1, QAudioFormat::Float);
+    auto formatStereo = makeAudioFormat(44100, 2, QAudioFormat::Float);
+
+    dut->setup(formatStereo, formatMono);
+
+    constexpr std::array monoData{
+        0.1f, 0.2f, 0.3f, 0.4f, 0.5f,
+    };
+    constexpr std::array stereoData{
+        0.1f, 0.1f, 0.2f, 0.2f, 0.3f, 0.3f, 0.4f, 0.4f, 0.5f, 0.5f,
+    };
+
+    auto result = dut->resample(as_bytes(QSpan(stereoData)));
+
+    QCOMPARE(size_t(result.size()),
+             size_t(formatMono.bytesForFrames(qint32(stereoData.size() / 2))));
+    QCOMPARE(asByteSpan(result).size(), asByteSpan(monoData).size());
+    QVERIFY(ranges::equal(asByteSpan(result), asByteSpan(monoData), std::equal_to<>{}));
+}
+
+QTEST_MAIN(tst_QWindowsResampler)
+
+#include "tst_qwindowsresampler.moc"
+
