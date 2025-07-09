a84870706e6cb7eb93a98857aed65bf8ddc01ba2
Tim Blechmann <tim.blechmann@qt.io>
2025-07-09 11:22:23 +0800

    Windows: introduce QWMF support functionality
    introduce withLockedBuffer as helper to acces IMFMediaBuffer with the
    appropriate locking.
    
    Pick-to: 6.8
    Change-Id: Iba0f8e3847de0d20cc38bc46bac91a19ef5bbbad
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 43d81f4e2862b824b0d18b8592f0c82964d85ae7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 9470cec7cbf2e14821cbe415d03e8d8c8cb3e2c6)
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>

--- src/multimedia/windows/qwindowsresampler.cpp
+++ src/multimedia/windows/qwindowsresampler.cpp
@@ -7,6 +7,7 @@
 #include <QtCore/private/qsystemerror_p.h>
 #include <QtMultimedia/private/qaudio_alignment_support_p.h>
 #include <QtMultimedia/private/qwindowsaudioutils_p.h>
+#include <QtMultimedia/private/qwmf_support_p.h>
 
 #include <wmcodecdsp.h>
 #include <mftransform.h>
@@ -72,18 +73,13 @@ HRESULT QWindowsResampler::processInput(const QByteArrayView &in)
     if (FAILED(hr))
         return hr;
 
-    BYTE *data = nullptr;
-    DWORD maxLen = 0;
-    DWORD currentLen = 0;
-    hr = buffer->Lock(&data, &maxLen, &currentLen);
-    if (FAILED(hr))
-        return hr;
+    auto result = QWMF::withLockedBuffer(buffer, [&](QSpan<BYTE> data, QSpan<BYTE> /*max*/) {
+        std::copy_n(in.data(), in.size(), data.data());
+        return true;
+    });
 
-    memcpy(data, in.data(), in.size());
-
-    hr = buffer->Unlock();
-    if (FAILED(hr))
-        return hr;
+    if (!result)
+        return result.error();
 
     hr = buffer->SetCurrentLength(in.size());
     if (FAILED(hr))
@@ -128,12 +124,14 @@ HRESULT QWindowsResampler::processOutput(QByteArray &out)
         if (SUCCEEDED(hr)) {
             ComPtr<IMFMediaBuffer> outputBuffer;
             outputDataBuffer.pSample->ConvertToContiguousBuffer(outputBuffer.GetAddressOf());
-            DWORD len = 0;
-            BYTE *data = nullptr;
-            hr = outputBuffer->Lock(&data, nullptr, &len);
-            if (SUCCEEDED(hr))
-                out.push_back(QByteArray(reinterpret_cast<char *>(data), len));
-            outputBuffer->Unlock();
+            auto result = QWMF::withLockedBuffer(outputBuffer,
+                                                 [&](QSpan<BYTE> data, QSpan<BYTE> /*max*/) {
+                out.append(reinterpret_cast<const char *>(data.data()), data.size());
+                return true;
+            });
+
+            if (!result)
+                hr = result.error();
         }
     } while (SUCCEEDED(hr));
 
@@ -184,13 +182,12 @@ QByteArray QWindowsResampler::resample(const ComPtr<IMFSample> &sample)
     if (m_inputFormat == m_outputFormat) {
         ComPtr<IMFMediaBuffer> outputBuffer;
         sample->ConvertToContiguousBuffer(outputBuffer.GetAddressOf());
-        DWORD len = 0;
-        BYTE *data = nullptr;
-        hr = outputBuffer->Lock(&data, nullptr, &len);
-        if (SUCCEEDED(hr))
-            out.push_back(QByteArray(reinterpret_cast<char *>(data), len));
-        outputBuffer->Unlock();
 
+        std::ignore =
+                QWMF::withLockedBuffer(outputBuffer, [&](QSpan<BYTE> data, QSpan<BYTE> /*max*/) {
+            out.push_back(data);
+            return true;
+        });
     } else {
         Q_ASSERT(m_resampler && m_wmf);
 
