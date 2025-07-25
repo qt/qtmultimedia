7a7a8b20ad08fc2ce66127fc5a413d3c2a18cec5
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2025-07-25 10:06:41 +0200

    Apple: Minor fix in include guard for QAVFCamera_p.h
    This patch renames the include guard for QAVFCamera_p.h
    
    Pick-to: 6.9 6.8
    Change-Id: I36ef5735f3a479a1d49a8c9f50b97f2b8e45d515
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 1e45a02946c06144e1666a20785e0db1d7530b08)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/multimedia/ffmpeg/darwin/qavfcamera_p.h
+++ src/plugins/multimedia/ffmpeg/darwin/qavfcamera_p.h
@@ -1,8 +1,8 @@
 // Copyright (C) 2022 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#ifndef QAVFCAMERA_H
-#define QAVFCAMERA_H
+#ifndef QAVFCAMERA_P_H
+#define QAVFCAMERA_P_H
 
 //
 //  W A R N I N G
@@ -100,5 +100,4 @@ private:
 QT_END_NAMESPACE
 
 
-#endif  // QFFMPEGCAMERA_H
-
+#endif // QAVFCAMERA_P_H
