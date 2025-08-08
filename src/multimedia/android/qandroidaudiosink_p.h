2c5164b4bbb398199f5feacbd1b5bfa6ee03c23d
Mikko Hallamaa <mikko.hallamaa@qt.io>
2025-08-08 14:08:48 +0200

    Android: Clean up includes
    This patch cleans up includes in multimedia/android.
    
    Change-Id: I29219aa103dbba012d25cc79f054a0a287157c18
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 698db40f62e1d40bc7ee464f21de3af9c591a2b2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/multimedia/android/qandroidaudiosink_p.h
+++ src/multimedia/android/qandroidaudiosink_p.h
@@ -15,11 +15,11 @@
 // We mean it.
 //
 
-#include <QtMultimedia/private/qaudio_platform_implementation_support_p.h>
-
-#include <memory>
+#include <private/qaudio_platform_implementation_support_p.h>
 
 #include "qaaudiostream_p.h"
+
+#include <memory>
 
 QT_BEGIN_NAMESPACE
 
