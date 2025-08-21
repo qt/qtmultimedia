a0ea47d86e2db706a51f3ca91496a9c648615263
Tim Blechmann <tim.blechmann@qt.io>
2025-08-21 10:23:23 +0800

    FFmpeg: QAVFScreenCapture::Grabber - move definition before use
    c++23 is a little stricter when it comes to template instantiations,
    i.e. the std::unique_ptr<Grabber> cannot be used synthesized before Grabber is defined.
    
    Moving the class definition up in the file fixes compile errors.
    
    Pick-to: 6.8
    Change-Id: I8c4cd23d326a61e1ee3619b85f495cb06e3c23a4
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Nils Petter Skålerud <nils.petter.skalerud@qt.io>
    (cherry picked from commit f903d054cac208b59065d023ebc22684f76c03fe)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2ad45af6677d9bab59853cf3341e03410fbdb83f)

--- src/plugins/multimedia/ffmpeg/darwin/qavfscreencapture.mm
+++ src/plugins/multimedia/ffmpeg/darwin/qavfscreencapture.mm
@@ -38,6 +38,68 @@ CGDirectDisplayID findDisplayByName(const QString &name)
 
 QT_BEGIN_NAMESPACE
 
+class QAVFScreenCapture::Grabber
+{
+public:
+    Grabber(QAVFScreenCapture &capture, QScreen *screen, CGDirectDisplayID screenID,
+            std::unique_ptr<QFFmpeg::HWAccel> hwAccel)
+    {
+        m_captureSession = [[AVCaptureSession alloc] init];
+
+        m_sampleBufferDelegate = [[QAVFSampleBufferDelegate alloc]
+                initWithFrameHandler:[&capture](const QVideoFrame &frame) {
+            capture.onNewFrame(frame);
+        }];
+
+        m_videoDataOutput = [[AVCaptureVideoDataOutput alloc] init];
+
+        NSDictionary *videoSettings = [NSDictionary
+                dictionaryWithObjectsAndKeys:[NSNumber numberWithUnsignedInt:DefaultCVPixelFormat],
+                                             kCVPixelBufferPixelFormatTypeKey, nil];
+
+        [m_videoDataOutput setVideoSettings:videoSettings];
+        [m_videoDataOutput setAlwaysDiscardsLateVideoFrames:true];
+
+        // Configure video output
+        m_dispatchQueue = dispatch_queue_create("vf_queue", nullptr);
+        [m_videoDataOutput setSampleBufferDelegate:m_sampleBufferDelegate queue:m_dispatchQueue];
+
+        [m_captureSession addOutput:m_videoDataOutput];
+
+        [m_sampleBufferDelegate setHWAccel:std::move(hwAccel)];
+
+        const auto frameRate = std::min(screen->refreshRate(), MaxScreenCaptureFrameRate);
+        [m_sampleBufferDelegate setVideoFormatFrameRate:frameRate];
+
+        m_screenInput = [[AVCaptureScreenInput alloc] initWithDisplayID:screenID];
+        [m_screenInput setMinFrameDuration:CMTimeMake(1, static_cast<int32_t>(frameRate))];
+        [m_captureSession addInput:m_screenInput];
+
+        [m_captureSession startRunning];
+    }
+
+    ~Grabber()
+    {
+        if (m_captureSession)
+            [m_captureSession stopRunning];
+
+        if (m_dispatchQueue)
+            dispatch_release(m_dispatchQueue);
+
+        [m_sampleBufferDelegate release];
+        [m_screenInput release];
+        [m_videoDataOutput release];
+        [m_captureSession release];
+    }
+
+private:
+    AVCaptureSession *m_captureSession = nullptr;
+    AVCaptureScreenInput *m_screenInput = nullptr;
+    AVCaptureVideoDataOutput *m_videoDataOutput = nullptr;
+    QAVFSampleBufferDelegate *m_sampleBufferDelegate = nullptr;
+    dispatch_queue_t m_dispatchQueue = nullptr;
+};
+
 QAVFScreenCapture::QAVFScreenCapture() : QPlatformSurfaceCapture(ScreenSource{})
 {
     CGRequestScreenCaptureAccess();
@@ -101,68 +163,6 @@ std::optional<int> QAVFScreenCapture::ffmpegHWPixelFormat() const
 {
     return AV_PIX_FMT_VIDEOTOOLBOX;
 }
-
-class QAVFScreenCapture::Grabber
-{
-public:
-    Grabber(QAVFScreenCapture &capture, QScreen *screen, CGDirectDisplayID screenID,
-            std::unique_ptr<QFFmpeg::HWAccel> hwAccel)
-    {
-        m_captureSession = [[AVCaptureSession alloc] init];
-
-        m_sampleBufferDelegate = [[QAVFSampleBufferDelegate alloc]
-                initWithFrameHandler:[&capture](const QVideoFrame &frame) {
-                    capture.onNewFrame(frame);
-                }];
-
-        m_videoDataOutput = [[AVCaptureVideoDataOutput alloc] init];
-
-        NSDictionary *videoSettings = [NSDictionary
-                dictionaryWithObjectsAndKeys:[NSNumber numberWithUnsignedInt:DefaultCVPixelFormat],
-                                             kCVPixelBufferPixelFormatTypeKey, nil];
-
-        [m_videoDataOutput setVideoSettings:videoSettings];
-        [m_videoDataOutput setAlwaysDiscardsLateVideoFrames:true];
-
-        // Configure video output
-        m_dispatchQueue = dispatch_queue_create("vf_queue", nullptr);
-        [m_videoDataOutput setSampleBufferDelegate:m_sampleBufferDelegate queue:m_dispatchQueue];
-
-        [m_captureSession addOutput:m_videoDataOutput];
-
-        [m_sampleBufferDelegate setHWAccel:std::move(hwAccel)];
-
-        const auto frameRate = std::min(screen->refreshRate(), MaxScreenCaptureFrameRate);
-        [m_sampleBufferDelegate setVideoFormatFrameRate:frameRate];
-
-        m_screenInput = [[AVCaptureScreenInput alloc] initWithDisplayID:screenID];
-        [m_screenInput setMinFrameDuration:CMTimeMake(1, static_cast<int32_t>(frameRate))];
-        [m_captureSession addInput:m_screenInput];
-
-        [m_captureSession startRunning];
-    }
-
-    ~Grabber()
-    {
-        if (m_captureSession)
-            [m_captureSession stopRunning];
-
-        if (m_dispatchQueue)
-            dispatch_release(m_dispatchQueue);
-
-        [m_sampleBufferDelegate release];
-        [m_screenInput release];
-        [m_videoDataOutput release];
-        [m_captureSession release];
-    }
-
-private:
-    AVCaptureSession *m_captureSession = nullptr;
-    AVCaptureScreenInput *m_screenInput = nullptr;
-    AVCaptureVideoDataOutput *m_videoDataOutput = nullptr;
-    QAVFSampleBufferDelegate *m_sampleBufferDelegate = nullptr;
-    dispatch_queue_t m_dispatchQueue = nullptr;
-};
 
 bool QAVFScreenCapture::initScreenCapture(QScreen *screen)
 {
