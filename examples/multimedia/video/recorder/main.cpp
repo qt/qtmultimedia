98c8fa2a5578f1eb9b49190dd46056775675ba60
Sze Howe Koh <szehowe.koh@gmail.com>
2025-09-22 10:31:59 +0800

    QML Video Recorder example: Port to modern QML APIs
    * qt_add_resources() -> qt_add_qml_module(), for automatic qmldir file
      generation (the manually-written one can be removed since there is
      no qmake project)
    * QQmlEngine::load() -> QQmlEngine::loadFromModule(), so that we don't
      need to know the files' exact URLs
    * QQmlEngine::objectCreated() -> QQmlEngine::objectCreationFailed(),
      for less verbose code
    
    Task-number: QTBUG-132922
    Task-number: QTBUG-140461
    Task-number: QTBUG-140464
    Change-Id: I1769d2c0985a9a2a455d487490c240d08db45911
    Pick-to: 6.10 6.8
    Reviewed-by: Nils Petter Skålerud <nils.petter.skalerud@qt.io>
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- examples/multimedia/video/recorder/main.cpp
+++ examples/multimedia/video/recorder/main.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 The Qt Company Ltd.
+// Copyright (C) 2025 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 #include <QGuiApplication>
@@ -13,38 +13,34 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(
-            &engine, &QQmlApplicationEngine::objectCreated, &app,
-            [url](QObject *obj, const QUrl &objUrl) {
-                if (!obj && url == objUrl)
-                    QCoreApplication::exit(-1);
+            &engine, &QQmlApplicationEngine::objectCreationFailed, &app, []() {
+                QCoreApplication::exit(-1);
             },
             Qt::QueuedConnection);
 
 #if QT_CONFIG(permissions)
     // If the permissions are not granted, display another main window, which
     // simply contains the error message.
-    const QUrl noPermissionsUrl(QStringLiteral("qrc:/main_no_permissions.qml"));
     QCameraPermission cameraPermission;
     qApp->requestPermission(cameraPermission, [&](const QPermission &permission) {
         if (permission.status() != Qt::PermissionStatus::Granted) {
             qWarning("Camera permission is not granted!");
-            engine.load(noPermissionsUrl);
+            engine.loadFromModule("RecorderExample", "Main_no_permissions");
             return;
         }
         QMicrophonePermission micPermission;
         qApp->requestPermission(micPermission, [&](const QPermission &permission) {
             if (permission.status() != Qt::PermissionStatus::Granted) {
                 qWarning("Microphone permission is not granted!");
-                engine.load(noPermissionsUrl);
+                engine.loadFromModule("RecorderExample", "Main_no_permissions");
             } else {
-                engine.load(url);
+                engine.loadFromModule("RecorderExample", "Main");
             }
         });
     });
 #else
-    engine.load(url);
+    engine.loadFromModule("RecorderExample", "Main");
 #endif
 
     return app.exec();
