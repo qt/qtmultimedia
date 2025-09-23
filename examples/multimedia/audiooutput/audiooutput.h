962f6880026101421420845a8fcd58361b3bc38d
Bror Wetlesen Vedeld <bror.vedeld@qt.io>
2025-09-23 17:38:07 +0200

    AudioOutput: Add feature for changing audio format
    Previously we had no way to change the audioformat in this example
    
    The audioformat is composed of three components: channel count,
    format and sample rate. This adds three dropdowns that will allow a
    user to set these independently, in case of an invalid format a
    pop-up will occur and the gui will resync based on actual values.
    
    Pick-to: 6.8 6.10
    Fixes: QTBUG-134263
    Change-Id: Ia0ee57ad347152aee3a840088849e810f58638af
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Nils Petter Skålerud <nils.petter.skalerud@qt.io>

--- examples/multimedia/audiooutput/audiooutput.h
+++ examples/multimedia/audiooutput/audiooutput.h
@@ -11,13 +11,11 @@
 #include <QLabel>
 #include <QMainWindow>
 #include <QMediaDevices>
+#include <QMessageBox>
 #include <QObject>
 #include <QPushButton>
 #include <QSlider>
 #include <QTimer>
-
-#include <math.h>
-#include <memory>
 
 class Generator : public QIODevice
 {
@@ -62,6 +60,8 @@ private:
     QTimer *m_pushTimer = nullptr;
 
     // Owned by layout
+    QComboBox *m_channelsBox = nullptr;
+    QComboBox *m_rateBox = nullptr;
     QComboBox *m_formatBox = nullptr;
     QPushButton *m_modeButton = nullptr;
     QPushButton *m_suspendResumeButton = nullptr;
@@ -76,6 +76,8 @@ private:
     void restartAudioStream();
 
 private slots:
+    void channelCountChanged(int index);
+    void sampleRateChanged(int index);
     void sampleFormatChanged(int index);
     void toggleMode();
     void toggleSuspendResume();
