qt/qtmultimedia

commit 80fd48180a6409db418843e77c61a6f43b6382d6

GStreamer: Check buffer memory type manually (dma-buf, glmemory)
Some gst elements, like v4l2h264dec, can provide Direct Memory Access
buffers (DMA-BUF) without specifying it in their caps, causing us to
handle the buffers as system/cpu memory.

This change fixes this by checking the memory format manually for each
video frame buffer before rendering.

Pick-to: 6.10 6.9 6.8
Change-Id: Ia28e0c680f1653c132332217b0e2e89bbade3c0b
Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

src/plugins/multimedia/gstreamer/common/qgstvideobuffer.cpp
@@ -6,6 +6,7 @@
 #include <private/qvideotexturehelper_p.h>
 #include <qpa/qplatformnativeinterface.h>
 #include <qguiapplication.h>
+#include <QtCore/qloggingcategory.h>
 
 #include <gst/video/video.h>
 #include <gst/video/video-frame.h>
@@ -36,6 +37,8 @@
 
 QT_BEGIN_NAMESPACE
 
+Q_STATIC_LOGGING_CATEGORY(qLcGstVideoBuffer, "qt.multimedia.gstreamer.videobuffer");
+
 // keep things building without drm_fourcc.h
 #define fourcc_code(a, b, c, d) ((uint32_t)(a) | ((uint32_t)(b) << 8) | \
                                  ((uint32_t)(c) << 16) | ((uint32_t)(d) << 24))
@@ -60,7 +63,7 @@ QGstVideoBuffer::QGstVideoBuffer(QGstBufferHandle buffer, const GstVideoInfo &in
                              ? QVideoFrame::RhiTextureHandle
                              : QVideoFrame::NoHandle,
                      sink ? sink->rhi() : nullptr),
-      memoryFormat(format),
+      m_memoryFormat(format),
       m_frameFormat(frameFormat),
       m_rhi(sink ? sink->rhi() : nullptr),
       m_videoInfo(info),
@@ -72,7 +75,7 @@ QGstVideoBuffer::QGstVideoBuffer(QGstBufferHandle buffer, const GstVideoInfo &in
         eglImageTargetTexture2D = sink->eglImageTargetTexture2D();
     }
 #endif
-    Q_UNUSED(memoryFormat);
+    Q_UNUSED(m_memoryFormat);
     Q_UNUSED(eglDisplay);
     Q_UNUSED(eglImageTargetTexture2D);
 }
@@ -256,6 +259,8 @@ private:
 static GlTextures mapFromGlTexture(const QGstBufferHandle &bufferHandle, GstVideoFrame &frame,
                                    GstVideoInfo &videoInfo)
 {
+    qCDebug(qLcGstVideoBuffer) << "mapFromGlTexture";
+
     GstBuffer *buffer = bufferHandle.get();
     auto *mem = GST_GL_BASE_MEMORY_CAST(gst_buffer_peek_memory(buffer, 0));
     if (!mem)
@@ -293,6 +298,8 @@ static GlTextures mapFromDmaBuffer(QRhi *rhi, const QGstBufferHandle &bufferHand
                                    GstVideoFrame &frame, GstVideoInfo &videoInfo,
                                    Qt::HANDLE eglDisplay, QFunctionPointer eglImageTargetTexture2D)
 {
+    qCDebug(qLcGstVideoBuffer) << "mapFromDmaBuffer";
+
     GstBuffer *buffer = bufferHandle.get();
 
     Q_ASSERT(gst_is_dmabuf_memory(gst_buffer_peek_memory(buffer, 0)));
@@ -372,11 +379,11 @@ QVideoFrameTexturesUPtr QGstVideoBuffer::mapTextures(QRhi &rhi, QVideoFrameTextu
 {
 #if QT_CONFIG(gstreamer_gl)
     GlTextures textures = {};
-    if (memoryFormat == QGstCaps::GLTexture)
+    if (m_memoryFormat == QGstCaps::GLTexture)
         textures = mapFromGlTexture(m_buffer, m_frame, m_videoInfo);
 
 #  if QT_CONFIG(gstreamer_gl_egl) && QT_CONFIG(linux_dmabuf)
-    else if (memoryFormat == QGstCaps::DMABuf)
+    else if (m_memoryFormat == QGstCaps::DMABuf)
         textures = mapFromDmaBuffer(m_rhi, m_buffer, m_frame, m_videoInfo, eglDisplay,
                                     eglImageTargetTexture2D);
 

src/plugins/multimedia/gstreamer/common/qgstvideobuffer_p.h
@@ -39,7 +39,7 @@ public:
     QVideoFrameTexturesUPtr mapTextures(QRhi &, QVideoFrameTexturesUPtr& /*oldTextures*/) override;
 
 private:
-    const QGstCaps::MemoryFormat memoryFormat = QGstCaps::CpuMemory;
+    const QGstCaps::MemoryFormat m_memoryFormat = QGstCaps::CpuMemory;
     const QVideoFrameFormat m_frameFormat;
     QRhi *m_rhi = nullptr;
     mutable GstVideoInfo m_videoInfo;

src/plugins/multimedia/gstreamer/common/qgstvideorenderersink.cpp
@@ -169,7 +169,7 @@ bool QGstVideoRenderer::start(const QGstCaps& caps)
         m_format = {};
         m_videoInfo = {};
     }
-    m_memoryFormat = caps.memoryFormat();
+    m_capsMemoryFormat = caps.memoryFormat();
 
     // NOTE: m_format will not be fully populated until GST_EVENT_TAG is processed
 
@@ -217,11 +217,22 @@ GstFlowReturn QGstVideoRenderer::render(GstBuffer *buffer)
         }
     }
 
+    // Some gst elements, like v4l2h264dec, can provide Direct Memory Access buffers (DMA-BUF)
+    // without specifying it in their caps. So we check the memory format manually:
+    QGstCaps::MemoryFormat bufferMemoryFormat = m_capsMemoryFormat;
+    if (m_capsMemoryFormat == QGstCaps::CpuMemory) {
+        GstMemory* mem = gst_buffer_peek_memory(buffer, 0);
+        if (gst_is_dmabuf_memory(mem))
+            bufferMemoryFormat = QGstCaps::DMABuf;
+        else if (gst_is_gl_memory(mem))
+            bufferMemoryFormat = QGstCaps::GLTexture;
+    }
+
     RenderBufferState state{
         QGstBufferHandle{ buffer, QGstBufferHandle::NeedsRef },
         m_format,
         m_videoInfo,
-        m_memoryFormat,
+        bufferMemoryFormat,
     };
 
     qCDebug(qLcGstVideoRenderer) << "    sending video frame";

src/plugins/multimedia/gstreamer/common/qgstvideorenderersink_p.h
@@ -125,7 +125,7 @@ private:
     const QGstCaps m_surfaceCaps;
     QVideoFrameFormat m_format;
     GstVideoInfo m_videoInfo{};
-    QGstCaps::MemoryFormat m_memoryFormat = QGstCaps::CpuMemory;
+    QGstCaps::MemoryFormat m_capsMemoryFormat = QGstCaps::CpuMemory;
 
     // --- only accessed from qt thread
     QVideoFrame m_currentPipelineFrame;

commit 7d252c0ee5747bf86f50606a882dad3f504e0fc8

GStreamer: Fix broken playback for unsupported dmabuf memory layouts
The function mapFromDmaBuffer will be refactored and hardened by
change 662143: Add dma-buf video rendering support with GStreamer
Until then, this adds a fallback to the system memory code path when
the number of planes differs from the number of memory blocks in
the video buffers, e.g. with NV12 pixel format.

This broken video playback is a regression introduced by
change 80fd48180a6409db418843e77c61a6f43b6382d6
It affects certain embedded boards like imx6 and imx8m plus

Fixes: QTBUG-140431
Pick-to: 6.10 6.10.0 6.9 6.9.3 6.8
Change-Id: I6c65ca31c4ed126968a6efe99944863ca976824f
Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>

src/plugins/multimedia/gstreamer/common/qgstvideobuffer.cpp
@@ -323,6 +323,12 @@ static GlTextures mapFromDmaBuffer(QRhi *rhi, const QGstBufferHandle &bufferHand
     textures.count = GST_VIDEO_FRAME_N_PLANES(&frame);
     //        int width = GST_VIDEO_FRAME_WIDTH(&frame);
     //        int height = GST_VIDEO_FRAME_HEIGHT(&frame);
+    if (textures.count != gst_buffer_n_memory(buffer)) {
+        qCDebug(qLcGstVideoBuffer) << "mapFromDmaBuffer: Unsupported memory layout, creating "
+                                      "textures from system memory instead. Will be fixed by "
+                                      "https://codereview.qt-project.org/c/qt/qtmultimedia/+/662143";
+        return {};
+    }
     Q_ASSERT(GST_VIDEO_FRAME_N_PLANES(&frame) == gst_buffer_n_memory(buffer));
 
     QOpenGLFunctions functions(glContext);