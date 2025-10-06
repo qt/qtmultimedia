467e9bfbcd3f00aeb1008b40e6c8a2a28e38253f
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2025-10-06 12:10:37 +0200

    Android: Clean up includes in qandroidscreencapture.cpp
    No functional changes.
    
    Pick-to: 6.10 6.8
    Change-Id: I7d767ce9cef28ec61a9ca1ca4feb9031e74029d1
    Reviewed-by: Bror Wetlesen Vedeld <bror.vedeld@qt.io>
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>

--- src/plugins/multimedia/ffmpeg/qandroidscreencapture.cpp
+++ src/plugins/multimedia/ffmpeg/qandroidscreencapture.cpp
@@ -1,12 +1,13 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#include "qandroidscreencapture_p.h"
+#include <QtFFmpegMediaPluginImpl/private/qandroidscreencapture_p.h>
 
 #include <QtCore/private/qjnihelpers_p.h>
-#include <QReadWriteLock>
-#include <qffmpegvideobuffer_p.h>
-#include <qandroidvideoframefactory_p.h>
+#include <QtCore/qreadwritelock.h>
+
+#include <QtFFmpegMediaPluginImpl/private/qandroidvideoframefactory_p.h>
+#include <QtFFmpegMediaPluginImpl/private/qffmpegvideobuffer_p.h>
 
 QT_BEGIN_NAMESPACE
 
