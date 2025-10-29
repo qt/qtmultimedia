1d1fabd8fc21e64de951978479d73a686d235403
Mikko Hallamaa <mikko.hallamaa@qt.io>
2025-10-29 16:02:53 +0100

    tst_qvideoframebackend: Test more backends when converting to image
    We are able to set the QRhi backend of the thread-local instance used
    in QVideoFrame to QImage conversion. This can be used in tests testing
    the conversion to increase our coverage.
    
    This patch expands the test running the conversion on a worker thread to
    test with all available QRhi backends, as this was a blind spot with
    likely failures.
    
    Pick-to: 6.8
    Task-number: QTBUG-135598
    Change-Id: I6290650fdadf4c8881b3ae51a141fcd804ca1020
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit ec166f8d17d4a0edcd9b8db79115a73fcaab7f1b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/integration/qvideoframebackend/tst_qvideoframebackend.cpp
+++ tests/auto/integration/qvideoframebackend/tst_qvideoframebackend.cpp
@@ -1,18 +1,20 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
+#include <QtCore/qdebug.h>
+#include <QtGui/rhi/qrhi.h>
+#include <QtMultimedia/qmediaplayer.h>
+#include <QtMultimedia/qvideoframe.h>
 #include <QtTest/qtest.h>
-#include <qmediaplayer.h>
-#include <qvideoframe.h>
-#include <qdebug.h>
 
-#include <private/mediafileselector_p.h>
 #include <private/mediabackendutils_p.h>
+#include <private/mediafileselector_p.h>
+#include <private/qthreadlocalrhi_p.h>
+#include <private/qvideotexturehelper_p.h>
+#include <private/qvideowindow_p.h>
 #include <private/testvideosink_p.h>
-#include "private/qvideotexturehelper_p.h"
-#include "private/qvideowindow_p.h"
-#include <thread>
 
+#include <thread>
 
 QT_USE_NAMESPACE
 
@@ -34,6 +36,7 @@ private slots:
     void toImage_rendersUpdatedFrame_afterMappingInWriteModeAndModifying_data();
     void toImage_rendersUpdatedFrame_afterMappingInWriteModeAndModifying();
 
+    void toImage_returnsImage_whenCalledFromSeparateThreadAndWhileRenderingToWindow_data();
     void toImage_returnsImage_whenCalledFromSeparateThreadAndWhileRenderingToWindow();
 
 private:
@@ -218,13 +221,34 @@ void tst_QVideoFrameBackend::toImage_rendersUpdatedFrame_afterMappingInWriteMode
     QCOMPARE(originalImage.pixel(1, 1), modifiedImage.pixel(1, 1));
 }
 
+void tst_QVideoFrameBackend::toImage_returnsImage_whenCalledFromSeparateThreadAndWhileRenderingToWindow_data()
+{
+    QTest::addColumn<QRhi::Implementation>("backend");
+
+#if QT_CONFIG(opengl)
+    QTest::newRow("OpenGL") << QRhi::OpenGLES2;
+#endif
+#if QT_CONFIG(metal)
+    QTest::newRow("Metal") << QRhi::Metal;
+#endif
+#if defined(Q_OS_WIN)
+    QTest::newRow("D3D11") << QRhi::D3D11;
+#endif
+}
+
 void tst_QVideoFrameBackend::toImage_returnsImage_whenCalledFromSeparateThreadAndWhileRenderingToWindow()
 {
+    QFETCH(QRhi::Implementation, backend);
+
     if (isCI()) {
 #ifdef Q_OS_MACOS
-        QSKIP("SKIP on macOS because of crash and error \"Failed to create QWindow::MetalSurface. Metal is not supported by any of the GPUs in this system.\"");
+        if (backend == QRhi::Metal)
+            QSKIP("SKIP on macOS because of crash and error \"Failed to create QWindow::MetalSurface. Metal is not supported by any of the GPUs in this system.\"");
 #elif defined(Q_OS_ANDROID)
         QSKIP("SKIP initTestCase on CI, because of QTBUG-118571");
+#elif defined(Q_OS_WIN)
+        if (backend == QRhi::OpenGLES2)
+            QSKIP("Skip on Windows OpenGL: QTBUG-135598");
 #endif
     }
     // Arrange
@@ -241,10 +265,14 @@ void tst_QVideoFrameBackend::toImage_returnsImage_whenCalledFromSeparateThreadAn
 
     // act
     connect(sink, &QVideoSink::videoFrameChanged, sink, [&](const QVideoFrame &frame) {
-
         // Run toImage on separate thread to exercise special code path
         QImage image;
-        auto t = std::thread([&] { image = frame.toImage(); });
+        auto t = std::thread([&] {
+            // Set thread-local QRhi implementation to use
+            qSetPreferredThreadLocalRhiBackend(backend);
+
+            image = frame.toImage();
+        });
         t.join();
 
         if (!image.isNull())
