29e09bbcada4b14e80021361ba543f5ecd481c0c
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2025-11-06 15:03:33 +0200

    X11 window capture: retry loop
    Capturing may fail if the window geometry changes between the invocation
    of update and XShmGetImage. Retrying the operation a few times makes
    such failures improbable.
    
    Change-Id: I6a46dc6e8463c41b5f8029a0e5de225e7fd76d90
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit de1be4503a1901c593086bfd60168f5539e570e0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/multimedia/ffmpeg/qx11surfacecapture.cpp
+++ src/plugins/multimedia/ffmpeg/qx11surfacecapture.cpp
@@ -289,16 +289,8 @@ private:
 protected:
     QVideoFrame grabFrame() override
     {
-        if (!update())
+        if (!grabXImage())
             return {};
-
-        if (!XShmGetImage(m_display.get(), m_xid, m_xImage.get(), m_xOffset, m_yOffset,
-                          AllPlanes)) {
-            updateError(QPlatformSurfaceCapture::CaptureFailed,
-                        QStringLiteral(
-                                "Cannot get ximage; the window geometry may be undergoing change"));
-            return {};
-        }
 
         QByteArray data(m_xImage->bytes_per_line * m_xImage->height, Qt::Uninitialized);
 
@@ -315,6 +307,22 @@ protected:
     }
 
 private:
+    bool grabXImage()
+    {
+        for (int i = 0; i < 10; i++) {
+            if (!update())
+                return false;
+            if (XShmGetImage(m_display.get(), m_xid, m_xImage.get(),
+                             m_xOffset, m_yOffset, AllPlanes))
+                return true;
+        }
+
+        updateError(QPlatformSurfaceCapture::CaptureFailed,
+                    QStringLiteral(
+                        "Cannot get ximage; the window geometry may be undergoing change"));
+        return false;
+    }
+
     std::optional<QPlatformSurfaceCapture::Error> m_prevGrabberError;
     XID m_xid = None;
     int m_xOffset = 0;
