e5ebccebd177119baae5867372f44b77e49c3d7e
Artem Dyomin <artem.dyomin@qt.io>
2026-02-12 17:00:09 +0100

    Remove redundant #elif check
    QT_MULTIMEDIA_REMOVED_SINCE is enabled if
    QT_MULTIMEDIA_BUILD_REMOVED_API is defined, so the removed condition
    is always true.
    
    Change-Id: Icfaec2519431b229d206fe284f2381a384bd781f
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit df32fcf71f77e99299e48c58e8254e781f53b9df)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/multimedia/audio/qaudio.h
+++ src/multimedia/audio/qaudio.h
@@ -83,7 +83,7 @@ namespace QAudio
 {
 #if QT_MULTIMEDIA_REMOVED_SINCE(6, 10)
 Q_MULTIMEDIA_EXPORT float convertVolume(float volume, VolumeScale from, VolumeScale to);
-#elif !defined(QT_MULTIMEDIA_BUILD_REMOVED_API)
+#else
 using QtAudio::convertVolume;
 #endif
 } // namespace QAudio
