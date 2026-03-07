qt/qtmultimedia

commit 2f74916641ef3d6d1a5949f564a332d95f35366f

wasm: Add placeholder devices when no permissions are given
This means user can play audio (which doesn't require permissions)
without being prompted for permissions.

If they need camera or mic input, this will cause the javascript
permission prompt during getUserMedia regardless, but use the
system 'default' devices or one they select.

Pick-to: 6.11 6.10
Change-Id: I008a5036e2bf9a128dd55f1cf0498580dd4f0d52
Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

src/plugins/multimedia/wasm/mediacapture/qwasmmediarecorder.cpp
@@ -188,7 +188,8 @@ void QWasmMediaRecorder::initUserMedia()
                 return;
             }
         }
-    } else {
+    }
+    if (m_session->hasAudio()) {
         qCDebug(qWasmMediaRecorder) << Q_FUNC_INFO << "has audio";
         stream = static_cast<QWasmAudioInput *>(m_session->audioInput())->mediaStream();
 
@@ -196,7 +197,7 @@ void QWasmMediaRecorder::initUserMedia()
             qCDebug(qWasmMediaRecorder) << Q_FUNC_INFO << "Audio input stream not found";
             return;
         }
-        m_jsMediaRecorderDevice->setNeedsAudio(true);
+        m_jsMediaRecorderDevice->setNeedsAudio(m_session->hasAudio());
     }
     if (stream.isNull() || stream.isUndefined()) {
          qCDebug(qWasmMediaRecorder) << Q_FUNC_INFO << "No input stream found";

src/multimedia/wasm/qwasmjs.cpp
@@ -365,6 +365,10 @@ void JsMediaInputStream::setStreamDevice(const std::string &id)
         return;
     }
 
+    std::string deviceIdString = id;
+    if (deviceIdString.find("System") != std::string::npos)
+        deviceIdString.clear(); // no id is default/any device
+
     qstdweb::PromiseCallbacks getUserMediaCallback{
         // default
         .thenFunc =

src/multimedia/wasm/qwasmmediadevices.cpp
@@ -134,13 +134,17 @@ void QWasmMediaDevices::parseDevices(emscripten::val devices)
         emscripten::val mediaDevice = devices[i];
 
         const std::string deviceKind = mediaDevice["kind"].as<std::string>();
-        const std::string label = mediaDevice["label"].as<std::string>();
-        const std::string deviceId = mediaDevice["deviceId"].as<std::string>();
+        std::string label = mediaDevice["label"].as<std::string>();
+        std::string deviceId = mediaDevice["deviceId"].as<std::string>();
 
         qCDebug(qWasmMediaDevices) << QString::fromStdString(deviceKind)
                                    << QString::fromStdString(deviceId)
                                    << QString::fromStdString(label);
 
+        if (deviceId.empty()) { // no permissions we'll use System;
+            label = "System " + deviceKind;
+            deviceId = label;
+        }
         if (deviceKind.empty())
             continue;
         bool isDefault = false;

src/plugins/multimedia/wasm/common/qwasmaudiooutput.cpp
@@ -215,6 +215,17 @@ void QWasmAudioOutput::createAudioElement(const std::string &id)
     emscripten::val document = emscripten::val::global("document");
     m_audio = document.call<emscripten::val>("createElement", std::string("audio"));
 
+    if (id == "System output") { // defaul output, no perms needed
+        emscripten::val audioContext = emscripten::val::global("window")["AudioContext"].new_();
+        emscripten::val sourceNode = audioContext.call<emscripten::val>("createMediaElementSource", m_audio);
+
+       sourceNode.call<void>("connect", audioContext["destination"]);
+// or control volume // is this needed?
+        // emscripten::val volumeNode = audioContext.call<emscripten::val>("createGain");
+        // sourceNode.call<void>("connect", volumeNode);
+        // volumeNode.call<void>("connect", audioContext["destination"]);
+    }
+
     // only works in chrome and firefox.
     // Firefox this feature is behind media.setsinkid.enabled preferences
     // allows user to choose audio output device
@@ -224,7 +235,7 @@ void QWasmAudioOutput::createAudioElement(const std::string &id)
     }
 
     std::string usableId = id;
-    if (usableId.empty())
+    if (usableId.empty() || usableId == "System output")
         usableId = QMediaDevices::defaultAudioOutput().id();
 
     qstdweb::PromiseCallbacks sinkIdCallbacks{

commit c4812feb24dbb59476bf8411f3d79166825cc55b

wasm: fix media recorder
This fixes what 2f74916641ef3d6d1a5949f564a332d95f35366f broke -
selecting a MediaSteam when recording.

The fix is because there is only ever one MediaStream,
they may contain video and/or audio, if we find one for
video, we found the correct one.

Pick-to: 6.10 6.11
Change-Id: I0ab13e5b3fcff337ef527ef31ba02be709354565
Reviewed-by: Even Oscar Andersen <even.oscar.andersen@qt.io>
Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

src/plugins/multimedia/wasm/mediacapture/qwasmmediarecorder.cpp
@@ -188,8 +188,7 @@ void QWasmMediaRecorder::initUserMedia()
                 return;
             }
         }
-    }
-    if (m_session->hasAudio()) {
+    } else if (m_session->hasAudio()) {
         qCDebug(qWasmMediaRecorder) << Q_FUNC_INFO << "has audio";
         stream = static_cast<QWasmAudioInput *>(m_session->audioInput())->mediaStream();
 