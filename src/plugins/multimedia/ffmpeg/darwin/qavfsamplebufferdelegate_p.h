83545142418aeacd1e47e0077974f5a267062e97
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2026-03-18 16:02:34 +0100

    FFmpeg: Mangle QAVFSampleBufferDelegate Obj-C class
    Obj-C classes are not namespaced. To avoid symbol-clashes, we mangle
    the typename correctly and add a namespaced C++ alias for it.
    
    Pick-to: 6.11
    Change-Id: I2210ab2e9475c89671087654a94a5a20c71be536
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- src/plugins/multimedia/ffmpeg/darwin/qavfsamplebufferdelegate_p.h
+++ src/plugins/multimedia/ffmpeg/darwin/qavfsamplebufferdelegate_p.h
@@ -46,7 +46,7 @@ using QAVFSampleBufferDelegateTransformProvider = std::function<QAVFSampleBuffer
 QT_END_NAMESPACE
 
 // This type is used by screencapture and camera-capture.
-@interface QAVFSampleBufferDelegate : NSObject <AVCaptureVideoDataOutputSampleBufferDelegate>
+@interface QT_MANGLE_NAMESPACE(QAVFSampleBufferDelegate) : NSObject <AVCaptureVideoDataOutputSampleBufferDelegate>
 
 // These parameters are called during the captureOutput callback.
 //
@@ -74,5 +74,15 @@ QT_END_NAMESPACE
 - (void)setVideoFormatFrameRate:(qreal)frameRate;
 
 @end
+
+QT_BEGIN_NAMESPACE
+
+namespace QFFmpeg {
+
+using QAVFSampleBufferDelegate = QT_MANGLE_NAMESPACE(QAVFSampleBufferDelegate);
+
+} // namespace QFFmpeg
+
+QT_END_NAMESPACE
 
 #endif
