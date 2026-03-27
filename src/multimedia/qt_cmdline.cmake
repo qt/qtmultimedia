160f180569886277b92ac178d569cc67a42992f3
Artem Dyomin <artem.dyomin@qt.io>
2026-03-27 16:16:45 +0100

    Move FFmpeg configure options to qt_cmdline.cmake in QtMM
    It allows to run qt-configure-module for multimedia with these
    options.
    
    Change-Id: I6c2111fde4dfa5586aba15d965e4cbe0897f0668
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>
    Reviewed-by: Mikko Hallamaa <mikko.hallamaa@qt.io>

--- src/multimedia/qt_cmdline.cmake
+++ src/multimedia/qt_cmdline.cmake
@@ -4,3 +4,6 @@
 qt_commandline_option(alsa TYPE boolean)
 qt_commandline_option(gstreamer TYPE optionalString VALUES no yes)
 qt_commandline_option(pulseaudio TYPE boolean)
+
+qt_commandline_option(ffmpeg-dir TYPE path CMAKE_VARIABLE FFMPEG_DIR)
+qt_commandline_option(ffmpeg-deploy TYPE boolean CMAKE_VARIABLE QT_DEPLOY_FFMPEG)
