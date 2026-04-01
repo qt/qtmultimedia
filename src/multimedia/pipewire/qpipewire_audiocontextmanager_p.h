4dd90f81f7408f1f0368ac5341012df3e833cadc
Tim Blechmann <tim.blechmann@qt.io>
2026-04-01 09:51:26 +0800

    Pipewire: stop listeners before terminating pw event loop
    There have been a few rare assertion failures during shutdown when we
    still get notifications from pipewire.
    We better stop the listeners before stopping the event loop.
    
    Pick-to: 6.11
    Change-Id: I8f7733b72b2002a18cd597971ab7b627efc1feb4
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- src/multimedia/pipewire/qpipewire_audiocontextmanager_p.h
+++ src/multimedia/pipewire/qpipewire_audiocontextmanager_p.h
@@ -92,6 +92,7 @@ private:
     std::unique_ptr<QAudioDeviceMonitor> m_deviceMonitor;
 
     void startDeviceMonitor();
+    void stopDeviceMonitor();
     static void objectAddedCb(void *data, uint32_t id, uint32_t permissions, const char *type,
                               uint32_t version, const struct spa_dict *props);
     static void objectRemovedCb(void *data, uint32_t id);
@@ -101,6 +102,7 @@ private:
 
     // default metadata object
     void startListenDefaultMetadataObject(ObjectId, uint32_t version);
+    void stopListenDefaultMetadataObject();
     struct MetadataRecord
     {
         const char *key;
