52967b9f25114896f558e18223f397f150fa6837
Tim Blechmann <tim.blechmann@qt.io>
2026-04-05 19:08:08 +0800

    QMediaPlayer: do not clear url on error
    setting the QUrl on the platform-specific layer can cause the state to
    transition to NoMedia, which is not what we want to do when handling
    errors.
    
    Change-Id: I3999350a8ce2e6f00e8c420dc2bad7d8b754c36e
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    Reviewed-by: Vladimir Belyavsky <belyavskyv@gmail.com>
    Reviewed-by: Mikko Hallamaa <mikko.hallamaa@qt.io>
    (cherry picked from commit 4467a62b526a0313f0a306b2c7ed2812cc16ed28)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/multimedia/playback/qmediaplayer.cpp
+++ src/multimedia/playback/qmediaplayer.cpp
@@ -137,7 +137,6 @@ void QMediaPlayerPrivate::setMedia(const QUrl &media, QIODevice *stream)
         QMediaPlayer::Error err,
         const QString &errString)
     {
-        control->setMedia(QUrl(), nullptr);
         control->mediaStatusChanged(status);
         control->error(err, errString);
     };
