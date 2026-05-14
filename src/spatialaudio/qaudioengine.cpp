1c354e2c01b2dbdfd906832c1bb181b14d91b27b
Tim Blechmann <tim.blechmann@qt.io>
2026-05-14 09:11:28 +0800

    QAudioEngine: fix setting of listener position
    When refactoring the listener position handling, the SetHeadPosition
    call was lost.
    Amends 1d6c6e67ca7a9232763319d6cdbb081ed60abee1
    
    Fixes: QTBUG-146764
    Pick-to: 6.8
    Change-Id: Ib37b2c5c5a625714ebe3f5a42aa190e071293192
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 4316a35073979fed7c957efcbfbe15d5f36c728a)
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>

--- src/spatialaudio/qaudioengine.cpp
+++ src/spatialaudio/qaudioengine.cpp
@@ -54,6 +54,9 @@ float QAudioEnginePrivate::masterVolume() const
 void QAudioEnginePrivate::setListenerPosition(std::optional<QVector3D> pos)
 {
     m_position = pos;
+
+    QVector3D posValue = pos.value_or(QVector3D{});
+    resonanceAudio->api->SetHeadPosition(posValue.x(), posValue.y(), posValue.z());
 }
 
 void QAudioEnginePrivate::setListenerRotation(const QQuaternion &rotation)
