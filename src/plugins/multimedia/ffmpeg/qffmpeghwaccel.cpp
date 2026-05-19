1286efbc09fb2f24a813ec87f02014afd195b947
Tim Blechmann <tim.blechmann@qt.io>
2026-05-19 09:02:11 +0800

    FFmpeg: remove unused header
    libavutil/version.h is not used in qffmpeghwaccel.cpp
    
    Pick-to: 6.11
    Change-Id: Ic09e4d582d06b1096220f64af35c9cca9735a0cd
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- src/plugins/multimedia/ffmpeg/qffmpeghwaccel.cpp
+++ src/plugins/multimedia/ffmpeg/qffmpeghwaccel.cpp
@@ -1,8 +1,6 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#include "libavutil/version.h"
-
 #include "qffmpeghwaccel_p.h"
 
 #ifdef Q_OS_WINDOWS
