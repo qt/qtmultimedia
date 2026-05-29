6d7db2222d8687725e93c967bf5bb56685217660
Tim Blechmann <tim.blechmann@qt.io>
2026-05-29 20:56:51 +0800

    pulseaudio: wait for stream state in start()
    We need to wait for the stream state as otherwise later "suspend" calls
    may not actually suspend the stream
    
    Fixes: QTBUG-147146
    Change-Id: I0b0e50f4bdb33872380bd4438a703034fcf2c42d
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 0658b6197a1ffd0c26839d727a567164e30cf4ed)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2bfdb9842d9a3e9efc6cfa76502408a8f40db0aa)

--- src/multimedia/pulseaudio/qpulseaudiosource.cpp
+++ src/multimedia/pulseaudio/qpulseaudiosource.cpp
@@ -6,6 +6,7 @@
 #include <QtMultimedia/private/qaudiosystem_platform_stream_support_p.h>
 #include <QtMultimedia/private/qpulseaudio_contextmanager_p.h>
 #include <QtMultimedia/private/qpulsehelpers_p.h>
+#include <QtCore/qsemaphore.h>
 
 #include <mutex> // for std::lock_guard
 #include <unistd.h>
@@ -154,8 +155,30 @@ bool QPulseAudioSourceStream::startStream(StreamType streamType)
     constexpr pa_stream_flags flags =
             pa_stream_flags(PA_STREAM_AUTO_TIMING_UPDATE | PA_STREAM_ADJUST_LATENCY);
 
-    std::lock_guard engineLock{ *pulseEngine };
+    std::unique_lock engineLock{ *pulseEngine };
     installCallbacks(streamType);
+
+    struct state_t
+    {
+        pa_stream_state state{};
+        QSemaphore sem;
+    };
+
+    state_t state;
+
+    pa_stream_set_state_callback(m_stream.get(), [](pa_stream *stream, void *data) {
+        auto *state = reinterpret_cast<state_t *>(data);
+        switch (pa_stream_get_state(stream)) {
+        case PA_STREAM_READY:
+        case PA_STREAM_FAILED:
+        case PA_STREAM_TERMINATED:
+            state->state = pa_stream_get_state(stream);
+            state->sem.release();
+            break;
+        default:
+            break;
+        }
+    }, &state);
 
     const auto id = m_audioDevice.id();
     int status = pa_stream_connect_record(m_stream.get(), id.data(), &attr, flags);
@@ -164,6 +187,18 @@ bool QPulseAudioSourceStream::startStream(StreamType streamType)
         m_stream = {};
         return false;
     }
+
+    engineLock.unlock();
+    state.sem.acquire();
+    if (state.state != PA_STREAM_READY) {
+        qCWarning(qLcPulseAudioOut) << "Stream failed to connect:" << state.state;
+        m_stream = {};
+        return false;
+    }
+
+    engineLock.lock();
+    pa_stream_set_state_callback(m_stream.get(), nullptr, nullptr);
+
     return true;
 }
 
