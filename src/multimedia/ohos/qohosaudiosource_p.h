7e5aa3f48b5a5d66e8ac947a9fba9aa2da3748da
Andy Nichols <andy.nichols@qt.io>
2026-06-11 12:46:14 +0200

    ohos: use Q_DECL_NONBLOCKING_FUNCTION
    The QT_MM_NONBLOCKING macro was dropped in favor of
    Q_DECL_NONBLOCKING_FUNCTION from qtbase, but the rename missed the ohos
    audio backend files since they only compile for HarmonyOS and were not
    caught by CI. Update the remaining uses so the HarmonyOS build compiles
    again.
    
    Change-Id: I2c4471e7c66fe2003c155543c929070f9f8fbc5f
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>
    (cherry picked from commit 24555b5bf6c7e21241d7bbf7321a75318e68d748)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/multimedia/ohos/qohosaudiosource_p.h
+++ src/multimedia/ohos/qohosaudiosource_p.h
@@ -59,10 +59,10 @@ private:
     void updateStreamIdle(bool idle) override;
 
     QSpan<const std::byte> getHostSpan(void *audioData,
-                                       int32_t numBytes) const noexcept QT_MM_NONBLOCKING;
+                                       int32_t numBytes) const noexcept Q_DECL_NONBLOCKING_FUNCTION;
     int32_t processRingbuffer(QSpan<const std::byte> audioSpan,
-                              int32_t numBytes) noexcept QT_MM_NONBLOCKING;
-    int32_t processCallback(QSpan<const std::byte> audioSpan) noexcept QT_MM_NONBLOCKING;
+                              int32_t numBytes) noexcept Q_DECL_NONBLOCKING_FUNCTION;
+    int32_t processCallback(QSpan<const std::byte> audioSpan) noexcept Q_DECL_NONBLOCKING_FUNCTION;
 
     QOhosAudioSource *m_parent;
     std::optional<AudioCallback> m_audioCallback;
