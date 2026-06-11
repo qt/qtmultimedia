032cf872782b8f14e4a52a210a8a36bef9cfba3f
Tim Blechmann <tim.blechmann@qt.io>
2026-06-11 15:39:30 +0800

    cleanup: avoid extra cmake include paths
    We can remove some of the unneeded include directories, some do not even
    seem to exist anymore.
    
    Pick-to: 6.12
    Change-Id: Id523745f73d74d9baaac345e8ee37694a644cc97
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- src/multimedia/recording/qcapturablewindow.cpp
+++ src/multimedia/recording/qcapturablewindow.cpp
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qcapturablewindow.h"
-#include "qcapturablewindow_p.h"
-#include "qplatformmediaintegration_p.h"
+
+#include <QtMultimedia/private/qcapturablewindow_p.h>
+#include <QtMultimedia/private/qplatformmediaintegration_p.h>
 
 QT_BEGIN_NAMESPACE
 
