6d6aa773c2256c45e4abd05a75dbb3ac97a5404a
Tim Blechmann <tim.blechmann@qt.io>
2026-07-02 01:48:28 +0000

    Revert "Remove setting default /usr/local FFMPEG_DIR"
    This reverts commit 317fdd3a3aa86a70fc7f539b3abbe0a8b69824ba.
    
    Reason for revert: It breaks builds with homebrew installed FFmpeg:
    FFmpeg seems to be detected, but the targets are not properly generated:
    
    ```
    CMake Error at qtbase/cmake/QtTargetHelpers.cmake:194 (target_link_libraries):
      Target "FFmpegMediaPluginImplPrivate" links to:
    
        FFmpeg::avformat
    
      but the target was not found.  Possible reasons include:
    
        * There is a typo in the target name.
        * A find_package call is missing for an IMPORTED target.
        * An ALIAS target is missing.
    
    Call Stack (most recent call first):
      qtmultimedia/src/plugins/multimedia/ffmpeg/CMakeLists.txt:308 (qt_internal_extend_target)
    ```
    
    Change-Id: I8b6538fa905233d53f269d295fc402b393d0e048
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- cmake/FindFFmpeg.cmake
+++ cmake/FindFFmpeg.cmake
@@ -3,15 +3,6 @@
 
 include(FindPackageHandleStandardArgs)
 
-find_package(PkgConfig QUIET)
-if (NOT PKG_CONFIG_FOUND AND NOT FFMPEG_DIR)
-    return()
-elseif (FFMPEG_DIR)
-    if(NOT IS_DIRECTORY "${FFMPEG_DIR}")
-        message(FATAL_ERROR "Explicitly set FFmpeg directory '${FFMPEG_DIR}' is invalid")
-    endif()
-endif()
-
 if (NOT FFmpeg_FIND_COMPONENTS)
     set(FFmpeg_FIND_COMPONENTS AVCODEC AVFORMAT AVUTIL SWSCALE SWRESAMPLE)
 endif ()
@@ -65,6 +56,15 @@ macro(set_component_found _component)
     endif ()
 endmacro()
 
+find_package(PkgConfig QUIET)
+if (FFMPEG_DIR)
+    if(NOT EXISTS "${FFMPEG_DIR}" OR NOT IS_DIRECTORY "${FFMPEG_DIR}")
+        message(FATAL_ERROR "Explicitly set FFmpeg directory '${FFMPEG_DIR}' is invalid")
+    endif()
+endif()
+if (NOT PKG_CONFIG_FOUND AND NOT FFMPEG_DIR)
+    set(FFMPEG_DIR "/usr/local")
+endif()
 #
 ### Macro: find_component
 #
