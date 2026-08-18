c78942898ef2f5ac290b24ad1a6a0f8e3746f5d2
Tim Blechmann <tim.blechmann@qt.io>
2026-08-18 15:16:12 +0800

    ffmpeg: link the symbol stubs into the ffmpeg unit tests
    A stubbed FFmpeg build carries unresolved references into OpenSSL and
    VAAPI. FindFFmpeg.cmake deliberately keeps the stub libraries out of
    the FFmpeg::av* link interface, so nothing provides those symbols to
    consumers.
    
    * explicitly link the stubs in qt_internal_multimedia_link_ffmpeg_stubs
    when LINK_FFMPEG_STUBS is given
    * link with --no-as-needed to force linking the symbols stubs into the
    binaries.
    
    Fixes: QTBUG-133914
    Pick-to: 6.12
    Change-Id: I43890a1890b8d61fe929e02a7fe7058b27913d0b
    Reviewed-by: Nils Petter Skålerud <nils.petter.skalerud@qt.io>

--- src/multimedia/Qt6MultimediaBuildInternals.cmake
+++ src/multimedia/Qt6MultimediaBuildInternals.cmake
@@ -1,11 +1,71 @@
 # Copyright (C) 2026 The Qt Company Ltd.
 # SPDX-License-Identifier: BSD-3-Clause
 
+
+function(qt_internal_multimedia_get_ffmpeg_stub_libraries out_var)
+    set(result "")
+
+    set(stub_prefix "${QT_CMAKE_EXPORT_NAMESPACE}FFmpegStub-")
+    string(TOUPPER "${QT_CMAKE_EXPORT_NAMESPACE}" install_paths_prefix)
+
+    set(searched_dirs "")
+    if(${install_paths_prefix}_INSTALL_PREFIX AND ${install_paths_prefix}_INSTALL_LIBS)
+        list(APPEND searched_dirs
+            "${${install_paths_prefix}_INSTALL_PREFIX}/${${install_paths_prefix}_INSTALL_LIBS}")
+    endif()
+
+    if(CMAKE_CURRENT_FUNCTION_LIST_DIR)
+        get_filename_component(config_libdir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../.." ABSOLUTE)
+        if(NOT config_libdir IN_LIST searched_dirs)
+            list(APPEND searched_dirs "${config_libdir}")
+        endif()
+    endif()
+
+    if(QT_FEATURE_ffmpeg_stubs)
+        foreach(stub IN ITEMS ssl crypto va va-drm va-x11)
+            if(TARGET ${stub_prefix}${stub})
+                list(APPEND result ${stub_prefix}${stub})
+            endif()
+        endforeach()
+
+        if(NOT result)
+            foreach(dir IN LISTS searched_dirs)
+                file(GLOB stub_files "${dir}/lib${stub_prefix}*${CMAKE_SHARED_LIBRARY_SUFFIX}*")
+
+                foreach(stub_file IN LISTS stub_files)
+                    get_filename_component(stub_file "${stub_file}" REALPATH)
+                    if(NOT stub_file IN_LIST result)
+                        list(APPEND result "${stub_file}")
+                    endif()
+                endforeach()
+            endforeach()
+        endif()
+    endif()
+
+    set(${out_var} "${result}" PARENT_SCOPE)
+endfunction()
+
+function(qt_internal_multimedia_link_ffmpeg_stubs target)
+    qt_internal_multimedia_get_ffmpeg_stub_libraries(stub_libraries)
+
+    if(stub_libraries)
+        target_link_options(${target} PRIVATE "LINKER:--no-as-needed")
+        target_link_libraries(${target} PRIVATE ${stub_libraries})
+    elseif(QT_FEATURE_ffmpeg_stubs AND BUILD_SHARED_LIBS)
+        message(FATAL_ERROR
+            "FFmpeg is used with symbol stubs, but no stub library was found for "
+            "target ${target}. Linking it would fail with undefined references "
+            "into OpenSSL or VAAPI. Reconfigure with "
+            "-DQT_MULTIMEDIA_DEBUG_FFMPEG_STUBS=ON for details.")
+    endif()
+endfunction()
+
 function(qt_internal_multimedia_add_test name)
     set(options
         MACOS_BUNDLE          # Add a macOS bundle target, requires Info.plist
         NEEDS_MEDIA_BACKEND_PLUGINS # Add dependency to qt_plugins_qtmultimedia target
                               # IOS: Add ffmpeg libraries to the target
+        LINK_FFMPEG_STUBS     # Required for executables linking Qt::FFmpegMediaPluginImplPrivate
     )
     set(oneValueArgs)
     set(multiValueArgs)
@@ -32,5 +92,9 @@ function(qt_internal_multimedia_add_test name)
         if(TARGET qt_plugins_qtmultimedia)
             add_dependencies(${name} qt_plugins_qtmultimedia)
         endif()
+    endif()
+
+    if(args_LINK_FFMPEG_STUBS)
+        qt_internal_multimedia_link_ffmpeg_stubs(${name})
     endif()
 endfunction()
