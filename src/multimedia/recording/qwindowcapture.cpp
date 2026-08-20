2b1968e145d7c17869c4c350451bc9beb6452405
Nils Petter Skålerud <nils.petter.skalerud@qt.io>
2026-08-20 11:11:33 +0200

    Move QWindowCapturePrivate into separate header
    Future patches will have the integration tests access some private APIs
    in QWindowCapture. This patch moves the QWindowCapturePrivate class into
    a separate header so that it can be included in the integration tests.
    
    Pick-to: 6.12
    Change-Id: Ic4be83d733875c5c073e59f24e3e7e471561877b
    Reviewed-by: Tim Blechmann <tim.blechmann@qt.io>

--- src/multimedia/recording/qwindowcapture.cpp
+++ src/multimedia/recording/qwindowcapture.cpp
@@ -1,13 +1,12 @@
 // Copyright (C) 2023 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#include <QtMultimedia/qwindowcapture.h>
-
-#include <QtCore/private/qobject_p.h>
+#include "qwindowcapture.h"
 
 #include <QtMultimedia/qmediacapturesession.h>
 #include <QtMultimedia/private/qplatformmediaintegration_p.h>
 #include <QtMultimedia/private/qplatformsurfacecapture_p.h>
+#include <QtMultimedia/private/qwindowcapture_p.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -15,13 +14,6 @@ static QWindowCapture::Error toWindowCaptureError(QPlatformSurfaceCapture::Error
 {
     return static_cast<QWindowCapture::Error>(error);
 }
-
-class QWindowCapturePrivate : public QObjectPrivate
-{
-public:
-    QMediaCaptureSession *captureSession = nullptr;
-    std::unique_ptr<QPlatformSurfaceCapture> platformWindowCapture;
-};
 
 /*!
     \class QWindowCapture
